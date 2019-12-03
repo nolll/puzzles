d6fe138763047b75dc54e4d9564e19db476527e3
Henrik Severinson <henriks@gmail.com>
2019-12-03 21:47:15 +0100

    Day 3, part 1 solved

--- Core/IntersectionFinder/IntersectionFinder.cs
+++ Core/IntersectionFinder/IntersectionFinder.cs
@@ -15,24 +15,10 @@ namespace Core.IntersectionFinder
             var pointsA = new Plotter(commandSequenceA).GetPoints();
             var pointsB = new Plotter(commandSequenceB).GetPoints();
 
-            var intersections = FindIntersections(pointsA, pointsB);
+            var intersections = pointsA.Intersect(pointsB).Where(o => o.Distance > 0);
             ClosestIntersection = intersections.OrderBy(o => o.Distance).First();
         }
-
-        private IList<Point> FindIntersections(IList<Point> pointsA, IList<Point> pointsB)
-        {
-            var intersections = new List<Point>();
-            foreach (var a in pointsA)
-            {
-                foreach (var b in pointsB)
-                {
-                    if (a.X == b.X && a.Y == b.Y)
-                        intersections.Add(a);
-                }
-            }
-            return intersections.Where(o => o.Distance > 0).ToList();
-        }
-
+        
         private IList<Command> GetCommands(string path)
         {
             return path.Split(',').Select(CommandFactory.Create).ToList();
--- Core/IntersectionFinder/Point.cs
+++ Core/IntersectionFinder/Point.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Core.IntersectionFinder
 {
-    public class Point
+    public class Point : IEquatable<Point>
     {
         public int X { get; }
         public int Y { get; }
@@ -14,5 +14,28 @@ namespace Core.IntersectionFinder
         }
 
         public int Distance => Math.Abs(X) + Math.Abs(Y);
+
+        public bool Equals(Point other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
     }
 }

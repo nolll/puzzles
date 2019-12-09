ffaaa0d2cd6c2d335d6c4f70d255a0b24af3367f
Henrik Severinson <henriks@gmail.com>
2019-12-09 23:41:44 +0100

    Day 5 cleanup

--- ConsoleApp/Days/Day05.cs
+++ ConsoleApp/Days/Day05.cs
@@ -1,18 +1,39 @@
-﻿using ConsoleApp.Inputs;
+﻿using System;
+using ConsoleApp.Inputs;
 using Core.Computer;
 
 namespace ConsoleApp.Days
 {
     public class Day05 : Day
     {
+        private long _output;
+
         public Day05() : base(5)
         {
+            _output = 0;
         }
 
         protected override void RunDay()
         {
-            var computer = new ConsoleComputer(InputData.ComputerProgramDay5);
-            computer.Start();
+            WritePartTitle();
+            var ci1 = new ComputerInterface(InputData.ComputerProgramDay5, ReadInputPart1, WriteOutput);
+            ci1.Start();
+
+            Console.WriteLine($"Diagnostic code for ID 1: {_output}");
+
+            WritePartTitle();
+            var ci2 = new ComputerInterface(InputData.ComputerProgramDay5, ReadInputPart2, WriteOutput);
+            ci2.Start();
+
+            Console.WriteLine($"Diagnostic code for ID 5: {_output}");
+        }
+
+        private long ReadInputPart1() => 1;
+        private long ReadInputPart2() => 5;
+
+        private void WriteOutput(long output)
+        {
+            _output = output;
         }
     }
 }

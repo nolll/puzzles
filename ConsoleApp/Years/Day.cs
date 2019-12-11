1b4b5b13e0937118b99ca715d3c49ef54a96a48b
Henrik Severinson <henriks@gmail.com>
2019-12-11 01:36:25 +0100

    Timer

--- ConsoleApp/Years/Day.cs
+++ ConsoleApp/Years/Day.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApp.Years
 {
     public abstract class Day
     {
+        private const string Divider = "--------------------------------------------------";
+
         private readonly int _day;
         private int _part;
         protected abstract void RunDay();
+        private readonly Timer _timer;
 
         protected Day(int day)
         {
             _day = day;
+            _timer = new Timer();
             _part = 1;
         }
 
         public void Run()
         {
+            _timer.Start();
             WriteDayTitle();
             RunDay();
+            WriteDayEnd();
         }
 
         protected void WritePartTitle()
@@ -31,7 +38,29 @@ namespace ConsoleApp.Years
         {
             Console.WriteLine();
             Console.WriteLine($"Day {_day}:");
-            Console.WriteLine("-------------------------");
+            WriteDivider();
+        }
+
+        private void WriteDayEnd()
+        {
+            WriteDivider();
+            WriteTimer();
+        }
+
+        private void WriteDivider()
+        {
+            Console.WriteLine(Divider);
+        }
+
+        private void WriteTimer()
+        {
+            var timeTaken = _timer.FromStart;
+            var seconds = timeTaken.TotalMilliseconds / 1000;
+            var decimalSeconds = Convert.ToDecimal(seconds);
+            var roundedSeconds = Math.Round(decimalSeconds, 2);
+            var secondsString = roundedSeconds.ToString(CultureInfo.InvariantCulture);
+            var timeString = $"{secondsString}s";
+            Console.WriteLine(timeString.PadLeft(Divider.Length));
         }
     }
 }

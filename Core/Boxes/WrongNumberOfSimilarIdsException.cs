fade71646c344070cc0b32b76387ccf2580b6f05
Henrik Severinson <henriks@gmail.com>
2019-12-12 01:23:30 +0100

    Moved tests

---
+++ Core/Boxes/WrongNumberOfSimilarIdsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Boxes
+{
+    public class WrongNumberOfSimilarIdsException : Exception
+    {
+        public WrongNumberOfSimilarIdsException(List<string> ids) : base($"Wrong number of similar ids. Should be two, was {ids.Count}.")
+        {
+        }
+    }
+}

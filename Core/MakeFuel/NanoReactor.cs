71418c28da8ee07ec0050e22b97bdd0ca2f0ef23
Henrik Severinson <henriks@gmail.com>
2019-12-15 15:38:27 +0100

    Day 14, part 1 tests passing

--- Core/MakeFuel/NanoReactor.cs
+++ Core/MakeFuel/NanoReactor.cs
@@ -5,20 +5,82 @@ namespace Core.MakeFuel
 {
     public class NanoReactor
     {
-        private readonly IList<Reaction> _reactions;.
+        private readonly IList<Reaction> _reactions;
         private readonly IDictionary<string, ChemicalQuantity> _waste;
+        private readonly IDictionary<string, int> _storage;
+        private readonly IDictionary<string, Reaction> _recipes;
+        private int _oreCount;
 
         public NanoReactor(string input)
         {
+            _oreCount = 0;
             _reactions = new ReactionParser().Parse(input);
             _waste = new Dictionary<string, ChemicalQuantity>();
+            _storage = new Dictionary<string, int>();
+            _recipes = new Dictionary<string, Reaction>();
+            Init();
+        }
+
+        private void Init()
+        {
+            _storage.Add("ORE", 0);
+            foreach (var reaction in _reactions)
+            {
+                _recipes.Add(reaction.Output.Name, reaction);
+                _storage.Add(reaction.Output.Name, 0);
+            }
         }
 
         public int GetRequiredOre()
         {
-            var fuelReaction = FindReactionByOutputName("FUEL");
-            var inputOres = GetNextLevelInputs(fuelReaction.Inputs, 1);
-            return inputOres.Sum(o => o.Quantity);
+            var fuel = Get("FUEL", 1).Quantity;
+            return _oreCount;
+            //var fuelReaction = FindReactionByOutputName("FUEL");
+            //var inputOres = GetNextLevelInputs(fuelReaction.Inputs, 1);
+            //return inputOres.Sum(o => o.Quantity);
+        }
+
+        private ChemicalQuantity Get(string name, int quantity)
+        {
+            var inStorage = _storage[name];
+            while (inStorage < quantity)
+            {
+                Make(name);
+                inStorage = _storage[name];
+            }
+
+            RemoveFromStorage(name, quantity);
+            return new ChemicalQuantity(name, quantity);
+        }
+
+        private void Make(string name)
+        {
+            if (name == "ORE")
+            {
+                _oreCount += 1;
+                AddToStorage(name, 1);
+            }
+            else
+            {
+                var recipe = _recipes[name];
+                foreach (var input in recipe.Inputs)
+                {
+                    var cq = Get(input.Name, input.Quantity);
+                }
+                AddToStorage(name, recipe.Output.Quantity);
+            }
+        }
+
+        private void AddToStorage(string name, int quantity)
+        {
+            var inStorage = _storage[name];
+            _storage[name] = inStorage + quantity;
+        }
+
+        private void RemoveFromStorage(string name, int quantity)
+        {
+            var inStorage = _storage[name];
+            _storage[name] = inStorage - quantity;
         }
 
         private IEnumerable<ChemicalQuantity> GetNextLevelInputs(IEnumerable<ChemicalQuantity> inputs, int multiplier)
--- Tests/FuelTests.cs
+++ Tests/FuelTests.cs
@@ -6,7 +6,7 @@ namespace Tests
     public class FuelTests
     {
         [Test]
-        public void Parser()
+        public void NumberOfOresIs31()
         {
             const string input = @"
 10 ORE => 10 A
@@ -21,5 +21,26 @@ namespace Tests
 
             Assert.That(result, Is.EqualTo(31));
         }
+
+        [Test]
+        public void NumberOfOresIs165()
+        {
+            const string input = @"
+9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL";
+
+            var reactor = new NanoReactor(input);
+            var result = reactor.GetRequiredOre();
+
+            Assert.That(result, Is.EqualTo(165));
+        }
+
     }
+
+
 }

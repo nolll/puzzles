70e4a31d5b332fab9864996ad9670734116f629d
Henrik <henriks@gmail.com>
2020-01-15 01:56:24 +0100

    Day 10 2015, part 2 solved

--- ConsoleApp/Years/Year2015/Day10.cs
+++ ConsoleApp/Years/Year2015/Day10.cs
@@ -13,8 +13,11 @@ namespace ConsoleApp.Years.Year2015
         {
             WritePartTitle();
             var game = new LookAndSayGame(Input, 40);
-            Console.WriteLine($"Length of string: {game.Result.Length}");
+            Console.WriteLine($"Length of string 1: {game.Result.Length}");
 
+            WritePartTitle();
+            var game2 = new LookAndSayGame(Input, 50);
+            Console.WriteLine($"Length of string 2: {game2.Result.Length}");
         }
 
         private const string Input = "1113222113";

e3047292b66537691a2a56eeed1bc7f73834807c
Henrik <henriks@gmail.com>
2020-01-17 21:35:27 +0100

    Day 11 2015, part 1 solved

---
+++ ConsoleApp/Years/Year2015/Day11.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core.CorporatePolicy;
+
+namespace ConsoleApp.Years.Year2015
+{
+    public class Day11 : Day
+    {
+        public Day11() : base(11)
+        {
+        }
+
+        protected override void RunDay()
+        {
+            WritePartTitle();
+            var validator = new CorporatePasswordValidator();
+            var pwd = validator.FindNextPassword(Input);
+            Console.WriteLine($"Next password: {pwd}");
+        }
+
+        private const string Input = "hxbxwxba";
+    }
+}

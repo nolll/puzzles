65c1b9124a6864a68c5fc7e2f317333a76acde61
Henrik <henriks@gmail.com>
2020-02-13 21:45:26 +0100

    Day 16 2016, part 1 solved

---
+++ ConsoleApp/Years/Year2016/Day16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core.DragonChecksum;
+
+namespace ConsoleApp.Years.Year2016
+{
+    public class Day16 : Day
+    {
+        public Day16() : base(16)
+        {
+        }
+
+        protected override void RunDay()
+        {
+            WritePartTitle();
+            var dragonCurve = new DragonCurve();
+            var checksum = dragonCurve.Run(Input, 272);
+            Console.WriteLine($"Checksum: {checksum}");
+        }
+
+        private const string Input = "01000100010010111";
+    }
+}

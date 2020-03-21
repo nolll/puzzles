9557d9c0e270f9d017a09615a2dfe99af66a0e2d
Henrik Severinson <henriks@gmail.com>
2020-03-21 01:09:33 +0100

    Day 21 2016, part 1 solved

---
+++ Tests/ScrambledLettersTests.cs
@@ -0,0 +1,29 @@
+using Core.ScrambledLetters;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class ScrambledLettersTests
+    {
+        [Test]
+        public void CorrectScramble()
+        {
+            const string input = @"
+swap position 4 with position 0
+swap letter d with letter b
+reverse positions 0 through 4
+rotate left 1 step
+move position 1 to position 4
+move position 3 to position 0
+rotate based on position of letter b
+rotate based on position of letter d";
+
+            const string pwd = "abcde";
+
+            var scrambler = new StringScrambler(input);
+            var result = scrambler.Scramble(pwd);
+
+            Assert.That(result, Is.EqualTo("decab"));
+        }
+    }
+}

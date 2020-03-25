ab94745c038818e01e0c4fa76d9abd9b284416f5
Henrik <henriks@gmail.com>
2020-03-25 02:28:04 +0100

    solved

--- ConsoleApp/Years/Year2019/Event2019.cs
+++ ConsoleApp/Years/Year2019/Event2019.cs
@@ -30,6 +30,7 @@ namespace ConsoleApp.Years.Year2019
             new Day18(),
             new Day19(),
             new Day20(),
+            new Day21(),
             new Day22(),
             new Day24(),
             new Day25()

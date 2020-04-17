5f4f56d300ebf9df8e04ae2171da24487443a758
Henrik Severinson <henriks@gmail.com>
2020-04-17 09:11:12 +0200

    Day 24 2015, part 1 solved (quite slow)

---
+++ Core/BalancedPresents/PresentBalancer.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using Core.Tools;
+
+namespace Core.BalancedPresents
+{
+    public class PresentBalancer
+    {
+        private readonly int _partitionSum;
+        public long QuantumEntanglementOfFirstGroup { get; }
+
+        public PresentBalancer(string input)
+        {
+            var presents = PuzzleInputReader.Read(input).Select(int.Parse).ToList();
+            presents.Reverse();
+            var sum = presents.Sum();
+            _partitionSum = sum / 3;
+            
+            var groups = FindGroupsRecursive(new PresentGroup(), presents, 0).ToList();
+            groups = groups.OrderBy(o => o.Count).ThenBy(o => o.QuantumEntanglement).ToList();
+
+            QuantumEntanglementOfFirstGroup = groups.First().QuantumEntanglement;
+        }
+
+        private IEnumerable<PresentGroup> FindGroupsRecursive(PresentGroup group, List<int> remainingPresents, int level)
+        {
+            if (level < 6)
+            {
+                foreach (var present in remainingPresents)
+                {
+                    var currentSum = group.Sum;
+                    var newSum = currentSum + present;
+                    if (newSum <= _partitionSum)
+                    {
+                        var newGroup = group.Clone();
+                        var newRemainingPresents = remainingPresents.Where(o => o != present).ToList();
+                        newGroup.Add(present);
+
+                        if (newSum == _partitionSum)
+                        {
+                            yield return newGroup;
+                        }
+
+                        if (newSum < _partitionSum)
+                        {
+                            var subResults = FindGroupsRecursive(newGroup, newRemainingPresents, level + 1);
+                            foreach (var result in subResults)
+                            {
+                                yield return result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tests/BalancedPresentsTests.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using Core.BalancedPresents;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class BalancedPresentsTests
+    {
+        [Test]
+        public void QuantumEntanglementOfFirstGroupIsCorrect()
+        {
+            const string input = @"
+1
+2
+3
+4
+5
+7
+8
+9
+10
+11";
+
+            var balancer = new PresentBalancer(input);
+
+            Assert.That(balancer.QuantumEntanglementOfFirstGroup, Is.EqualTo(99));
+        }
+    }
+}

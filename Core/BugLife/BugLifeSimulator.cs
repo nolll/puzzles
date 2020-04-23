67a21cd39f2f9e4e3513c15c18e3e467e182c12b
nolll <henriks@gmail.com>
2020-04-23 11:38:08 +0200

    started creating classes

--- Core/BugLife/BugLifeSimulator.cs
+++ Core/BugLife/BugLifeSimulator.cs
@@ -4,6 +4,24 @@ using Core.Tools;
 
 namespace Core.BugLife
 {
+    public class BugLifeArea
+    {
+        public BugLifeArea()
+        {
+            var cells = new BugLifeCell[5, 5];
+        }
+    }
+
+    public class BugLifeCell
+    {
+        public bool HasBug { get; }
+
+        public BugLifeCell(bool hasBug)
+        {
+            HasBug = hasBug;
+        }
+    }
+
     public class BugLifeSimulator
     {
         private Matrix<char> _matrix;

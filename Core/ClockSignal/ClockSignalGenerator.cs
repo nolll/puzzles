8633ec9f3314357da4618db47c1276e370038b65
nolll <henriks@gmail.com>
2020-04-26 20:22:19 +0200

    refactoring

--- Core/ClockSignal/ClockSignalGenerator.cs
+++ Core/ClockSignal/ClockSignalGenerator.cs
@@ -8,58 +8,43 @@ namespace Core.ClockSignal
         {
             var index = 1;
             var output = "";
+            var targetOutputLength = 10;
 
             while (output != "0101010101")
             {
                 output = "";
                 var a = index;
-                var b = 0;
+                var d = a + 633 * 4;
 
-                var d = a;
-                var c = 4;
-                
-                do
+                while (output.Length < targetOutputLength)
                 {
-                    b = 633;
-
+                    a = d;
                     do
                     {
-                        d++;
-                        b--;
-                    } while (b != 0);
-                    
-                    c--;
-                } while (c != 0);
+                        var b = a;
+                        var c = 2;
+                        a = 0;
+                        while (b != 0)
+                        {
+                            b--;
+                            c--;
+                            if (c != 0)
+                                continue;
+                            a++;
+                            c = 2;
+                        }
+
+                        b = 2;
+                        while (c != 0)
+                        {
+                            b--;
+                            c--;
+                        }
+
+                        output += b;
+                    } while (output.Length < targetOutputLength && a != 0);
+                }
                 
-                row9: a = d;
-                row10:
-                b = a;
-                a = 0;
-                row13: c = 2;
-                row14: if (b != 0) 
-                    goto row16;
-                goto row21;
-                row16: b--;
-                c--;
-                if (c != 0)
-                    goto row14;
-                a++;
-                goto row13;
-                row21: b = 2;
-                row22: if (c != 0) 
-                    goto row24;
-                goto row27;
-                row24: b--;
-                c--;
-                goto row22;
-                row27:
-                output += b;
-                if (output.Length == 10)
-                    goto row31;
-                if (a != 0) 
-                    goto row10;
-                goto row9;
-                row31:
                 LowestA = index;
                 index++;
             }

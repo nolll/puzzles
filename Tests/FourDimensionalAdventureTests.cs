3d7b3aa3884b71fb17ee165715a7a1af4be33ceb
nolll <henriks@gmail.com>
2020-04-27 08:37:16 +0200

    solved

--- Tests/FourDimensionalAdventureTests.cs
+++ Tests/FourDimensionalAdventureTests.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Core.Tools;
+using Core.FourDimensionalAdventure;
 using NUnit.Framework;
 
 namespace Tests
@@ -23,51 +20,71 @@ namespace Tests
 
             var finder = new ConstellationFinder(input);
             var constellationCount = finder.Find();
-        }
-    }
 
-    public class ConstellationFinder
-    {
-        public ConstellationFinder(string input)
+            Assert.That(constellationCount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void FindsConstellations2()
         {
-            var points = ParsePoints(input);
+            const string input = @"
+-1,2,2,0
+0,0,2,-2
+0,0,0,-2
+-1,2,0,0
+-2,-2,-2,2
+3,0,2,-1
+-1,3,2,2
+-1,0,-1,0
+0,2,1,-2
+3,0,0,0";
+
+            var finder = new ConstellationFinder(input);
+            var constellationCount = finder.Find();
+
+            Assert.That(constellationCount, Is.EqualTo(4));
         }
 
-        private IList<Point4d> ParsePoints(string input)
+        [Test]
+        public void FindsConstellations3()
         {
-            var rows = PuzzleInputReader.Read(input);
-            return rows.Select(ParsePoint).ToList();
+            const string input = @"
+1,-1,0,1
+2,0,-1,0
+3,2,-1,0
+0,0,3,1
+0,0,-1,-1
+2,3,-2,0
+-2,2,0,0
+2,-2,0,-1
+1,-1,0,-1
+3,2,0,2";
+
+            var finder = new ConstellationFinder(input);
+            var constellationCount = finder.Find();
+
+            Assert.That(constellationCount, Is.EqualTo(3));
         }
 
-        private Point4d ParsePoint(string s)
+        [Test]
+        public void FindsConstellations4()
         {
-            var coords = s.Trim().Split(',').Select(int.Parse).ToList();
-            return new Point4d(coords[0], coords[1], coords[2], coords[3]);
+            const string input = @"
+1,-1,-1,-2
+-2,-2,0,1
+0,2,1,3
+-2,3,-2,1
+0,2,3,-2
+-1,-1,1,-2
+0,-2,-1,0
+-2,2,3,-1
+1,2,2,0
+-1,-2,0,-2";
+
+            var finder = new ConstellationFinder(input);
+            var constellationCount = finder.Find();
+
+            Assert.That(constellationCount, Is.EqualTo(8));
         }
-
-        public int Find()
-        {
-            return 0;
-        }
-    }
-
-    public class Point4d
-    {
-        public int X { get; }
-        public int Y { get; }
-        public int Z { get; }
-        public int W { get; }
-
-        public Point4d(int x, int y, int z, int w)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-            W = w;
-        }
-
-        public int ManhattanDistanceTo(Point4d other) => ManhattanDistanceTo(other.X, other.Y, other.Z, other.W);
-        private int ManhattanDistanceTo(int x, int y, int z, int w) => GetDistance(X, x) + GetDistance(Y, y) + GetDistance(Z, z) + GetDistance(W, w);
-        private int GetDistance(int a, int b) => Math.Max(a, b) - Math.Min(a, b);
     }
 }

83d43f2f2cd20b805b1a4aebf025aed98eee77c6
Henrik Severinson <henriks@gmail.com>
2020-04-27 08:39:11 +0200

    Day 25 2018 solved

---
+++ Core/FourDimensionalAdventure/Constellation.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Core.FourDimensionalAdventure
+{
+    public class Constellation
+    {
+        private readonly IList<Point4d> _points;
+        public IReadOnlyList<Point4d> Points => _points.ToImmutableList();
+
+        public Constellation()
+        {
+            _points = new List<Point4d>();
+        }
+
+        public bool IsClose(Point4d point)
+        {
+            return Points.Any(o => o.ManhattanDistanceTo(point) < 4);
+        }
+
+        public bool IsClose(Constellation otherConstellation)
+        {
+            foreach (var point in otherConstellation.Points)
+            {
+                if (IsClose(point))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Add(Point4d point)
+        {
+            _points.Add(point);
+        }
+
+        public void Add(Constellation constellation)
+        {
+            foreach (var point in constellation.Points)
+            {
+                _points.Add(point);
+            }
+        }
+    }
+}

1048687b8430d31fdd64cb9adc3119ee28f821fb
Henrik <henriks@gmail.com>
2020-12-15 01:10:58 +0100

    repaired day 11 2019. Screen output

--- ConsoleApp/Years/Year2019/Puzzles/Day11.cs
+++ ConsoleApp/Years/Year2019/Puzzles/Day11.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Core.ShipPainting;
 
 namespace ConsoleApp.Years.Year2019.Puzzles
@@ -21,10 +23,37 @@ namespace ConsoleApp.Years.Year2019.Puzzles
         {
             var robot2 = new PaintRobot(FileInput);
             var result2 = robot2.Paint(true);
+            var printout = CleanPrintout(result2.Printout);
 
-            var printout = result2.Printout.Replace('0', ' ').Replace('1', 'X');
-
-            return new PuzzleResult(printout, "ABCLFUHJ");
+            return new PuzzleResult(printout, CorrectAnswer);
         }
+
+        private string CleanPrintout(string s)
+        {
+            var rows = s.Split("\r\n").ToList();
+            var rowsWithOutput = new List<string>();
+
+            foreach(var row in rows)
+            {
+                var chars = row.Trim().ToCharArray();
+                if(!chars.All(o => o == '0'))
+                {
+                    rowsWithOutput.Add(row);
+                }
+            }
+
+            var firstCharPos = rowsWithOutput.Min(o => o.IndexOf('1'));
+            var lastCharPos = rowsWithOutput.Max(o => o.LastIndexOf('1'));
+            var length = lastCharPos - firstCharPos + 1;
+            var printoutRows = rowsWithOutput.Select(o => o.Substring(firstCharPos, length));
+            return string.Join("\r\n", printoutRows).Replace('0', ' ').Replace('1', 'X'); ;
+        }
+
+        private const string CorrectAnswer = @" XX  XXX   XX  X    XXXX X  X X  X   XX
+X  X X  X X  X X    X    X  X X  X    X
+X  X XXX  X    X    XXX  X  X XXXX    X
+XXXX X  X X    X    X    X  X X  X    X
+X  X X  X X  X X    X    X  X X  X X  X
+X  X XXX   XX  XXXX X     XX  X  X  XX ";
     }
 }

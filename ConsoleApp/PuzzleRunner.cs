4e82182a350e68c695a066fc148a19fd7e525681
Henrik <henriks@gmail.com>
2021-01-07 13:17:04 +0100

    Only use timeout for days marked as slow

--- ConsoleApp/PuzzleRunner.cs
+++ ConsoleApp/PuzzleRunner.cs
@@ -38,28 +38,28 @@ namespace ConsoleApp
 
         private DayResult RunDay(Day day)
         {
-            var p1 = RunPuzzleWithTimer(day.RunPart1);
-            var p2 = RunPuzzleWithTimer(day.RunPart2);
+            var p1 = RunPuzzleWithTimer(day.RunPart1, day.IsSlow);
+            var p2 = RunPuzzleWithTimer(day.RunPart2, day.IsSlow);
 
             return new DayResult(day, p1, p2, day.Comment);
         }
 
-        private TimedPuzzleResult RunPuzzleWithTimer(Func<PuzzleResult> func)
+        private TimedPuzzleResult RunPuzzleWithTimer(Func<PuzzleResult> func, bool isSlow)
         {
             var timer = new Timer();
-            var result = RunPuzzle(func);
+            var result = RunPuzzle(func, isSlow);
             return new TimedPuzzleResult(result, timer.FromStart);
         }
 
-        private PuzzleResult RunPuzzle(Func<PuzzleResult> func)
+        private PuzzleResult RunPuzzle(Func<PuzzleResult> func, bool isSlow)
         {
             PuzzleResult result = null;
             try
             {
-                if (_timeout != null)
+                if (isSlow && _timeout != null)
                 {
-                    var task1 = Task.Run(() => result = func());
-                    if (!task1.Wait(TimeSpan.FromSeconds(_timeout.Value)))
+                    var task = Task.Run(() => result = func());
+                    if (!task.Wait(TimeSpan.FromSeconds(_timeout.Value)))
                         return new TimeoutPuzzleResult($"Puzzle failed to finish within {_timeout.Value} seconds");
                 }
                 else

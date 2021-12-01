ca38143e49aaa3812a2b06135b9e8e1c164c8c18
Henrik <henriks@gmail.com>
2021-12-01 06:33:49 +0100

    Day 1 2021

---
+++ ConsoleApp/Years/Year2021/Puzzles/Day01.cs
@@ -0,0 +1,27 @@
+﻿using Core.SeaDepth;
+
+namespace ConsoleApp.Years.Year2021.Puzzles
+{
+    public class Day01 : Day2021
+    {
+        public Day01() : base(1)
+        {
+        }
+
+        public override PuzzleResult RunPart1()
+        {
+            var calculator = new DepthMeasurement();
+            var result = calculator.GetNumberOfIncreasingMeasurements(FileInput, false);
+            
+            return new PuzzleResult(result, 1477);
+        }
+
+        public override PuzzleResult RunPart2()
+        {
+            var calculator = new DepthMeasurement();
+            var result = calculator.GetNumberOfIncreasingMeasurements(FileInput, true);
+
+            return new PuzzleResult(result, 1523);
+        }
+    }
+}

284d0605321ac7fd90897e4d85534effb2cb8216
Henrik <henriks@gmail.com>
2021-12-07 08:59:01 +0100

    correct result but needs refactoring

--- Core/Puzzles/Year2021/Day07/Year2021Day07.cs
+++ Core/Puzzles/Year2021/Day07/Year2021Day07.cs
@@ -1,4 +1,6 @@
-﻿using Core.Platform;
+﻿using System;
+using System.Linq;
+using Core.Platform;
 
 namespace Core.Puzzles.Year2021.Day07
 {
@@ -8,13 +10,76 @@ namespace Core.Puzzles.Year2021.Day07
 
         public override PuzzleResult RunPart1()
         {
-            return new PuzzleResult(0);
+            var result = GetFuel1(FileInput);
+            return new PuzzleResult(result);
         }
 
         public override PuzzleResult RunPart2()
         {
-            return new PuzzleResult(0);
+            var result = GetFuel2(FileInput);
+            return new PuzzleResult(result);
         }
-        
+
+        public int GetFuel1(string input)
+        {
+            var minDiff = int.MaxValue;
+            var minDiffPos = 0;
+            var positions = input.Split(',').Select(int.Parse).ToArray();
+            for (int i = 0; i < positions.Length; i++)
+            {
+                var diff = 0;
+                for (int j = 0; j < positions.Length; j++)
+                {
+                    var large = Math.Max(positions[i], positions[j]);
+                    var small = Math.Min(positions[i], positions[j]);
+                    diff += large - small;
+                }
+
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                }
+            }
+
+            return minDiff;
+        }
+
+        public int GetFuel2(string input)
+        {
+            var minCost = int.MaxValue;
+            var minCostPos = 0;
+            var positions = input.Split(',').Select(int.Parse).ToArray();
+            var maxPos = positions.Max();
+            for (int i = 0; i < maxPos; i++)
+            {
+                var cost = 0;
+                for (int j = 0; j < positions.Length; j++)
+                {
+                    cost += GetCost(i, positions[j]);
+                }
+
+                if (cost < minCost)
+                {
+                    minCost = cost;
+                    //minCostPos = positions[i];
+                }
+            }
+
+            return minCost;
+        }
+
+        public int GetCost(int a, int b)
+        {
+            var large = Math.Max(a, b);
+            var small = Math.Min(a, b);
+            var diff = large - small;
+            var d = 0;
+            for (int k = 1; k <= diff; k++)
+            {
+                d += k;
+            }
+
+            return d;
+        }
     }
 }
--- Tests/PuzzleTests/Year2021Tests/Year2021Day07Tests.cs
+++ Tests/PuzzleTests/Year2021Tests/Year2021Day07Tests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using Core.Common.Strings;
 using Core.Puzzles.Year2021.Day07;
 using NUnit.Framework;
 
@@ -9,21 +12,40 @@ namespace Tests.PuzzleTests.Year2021Tests
         public void Part1()
         {
             var puzzle = new Year2021Day07();
-            var result = 0;
+            var result = puzzle.GetFuel1(Input);
 
-            Assert.That(0, Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo(37));
         }
 
         [Test]
         public void Part2()
         {
             var puzzle = new Year2021Day07();
-            var result = 0;
+            var result = puzzle.GetFuel2(Input);
 
-            Assert.That(0, Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo(168));
         }
 
-        private const string Input = @"
-";
+        [TestCase(16, 5, 66)]
+        [TestCase(1, 5, 10)]
+        [TestCase(2, 5, 6)]
+        [TestCase(0, 5, 15)]
+        [TestCase(4, 5, 1)]
+        [TestCase(2, 5, 6)]
+        [TestCase(7, 5, 3)]
+        [TestCase(1, 5, 10)]
+        [TestCase(2, 5, 6)]
+        [TestCase(14, 5, 45)]
+        [TestCase(5, 16, 66)]
+        [TestCase(5, 5, 0)]
+        public void Cost(int a, int b, int expected)
+        {
+            var puzzle = new Year2021Day07();
+            var result = puzzle.GetCost(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        private const string Input = "16,1,2,0,4,2,7,1,2,14";
     }
 }

766df1e6cd132d3931d7e0890e06557c6d179d32
Henrik <henriks@gmail.com>
2021-12-08 09:40:09 +0100

    wip

--- Core/Puzzles/Year2021/Day08/Year2021Day08.cs
+++ Core/Puzzles/Year2021/Day08/Year2021Day08.cs
@@ -1,19 +1,143 @@
-﻿using Core.Platform;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Strings;
+using Core.Platform;
+using NUnit.Framework.Constraints;
 
 namespace Core.Puzzles.Year2021.Day08
 {
     public class Year2021Day08 : Year2021Day
     {
+        
         public override int Day => 8;
 
         public override PuzzleResult RunPart1()
         {
-            return new PuzzleResult(0);
+            var decoder = new SevenSegmentDisplayDecoder(FileInput);
+            var result = decoder.GetEasyNumbers();
+            return new PuzzleResult(result, 278);
         }
 
         public override PuzzleResult RunPart2()
         {
-            return new PuzzleResult(0);
+            var decoder = new SevenSegmentDisplayDecoder(FileInput);
+            var result = decoder.GetDecodedSum();
+            return new PuzzleResult(result);
+        }
+    }
+
+    public class SevenSegmentDisplayDecoder
+    {
+        private readonly List<DigitDecoder> _decoders;
+
+        public SevenSegmentDisplayDecoder(string input)
+        {
+            var lines = PuzzleInputReader.ReadLines(input);
+            _decoders = new List<DigitDecoder>();
+            foreach (var line in lines)
+            {
+                _decoders.Add(new DigitDecoder(line));
+            }
+        }
+
+        public int GetDecodedSum()
+        {
+            return 0;
+        }
+
+        public int GetEasyNumbers()
+        {
+            return _decoders.Sum(o => o.EasyNumberCount);
+        }
+    }
+
+    public class DigitDecoder
+    {
+        private readonly List<string> _signals;
+        private readonly List<string> _output;
+
+        public DigitDecoder(string input)
+        {
+            var parts = input.Split('|');
+            _signals = parts[0].Trim().Split(' ').Select(o => string.Concat(o.OrderBy(c => c))).Distinct().ToList();
+            _output = parts[1].Trim().Split(' ').Select(o => string.Concat(o.OrderBy(c => c))).ToList();
+        }
+
+        public int EasyNumberCount
+        {
+            get
+            {
+                var c = 0;
+                foreach (var i in _output)
+                {
+                    var length = i.Length;
+                    if (length == 2 || length == 3 || length == 4 || length == 7)
+                        c++;
+                }
+
+                return c;
+            }
+        }
+
+        public int DecodedNumber
+        {
+            get
+            {
+                var segmentCount = new Dictionary<int, int>
+                {
+                    {0, 6},
+                    {1, 2},
+                    {2, 5},
+                    {3, 5},
+                    {4, 4},
+                    {5, 5},
+                    {6, 6},
+                    {7, 3},
+                    {8, 7},
+                    {9, 6}
+                };
+
+                var s = new Dictionary<int, string>
+                {
+                    {1, _signals.First(o => o.Length == segmentCount[1])},
+                    {4, _signals.First(o => o.Length == segmentCount[4])},
+                    {7, _signals.First(o => o.Length == segmentCount[7])},
+                    {8, _signals.First(o => o.Length == segmentCount[8])},
+                };
+
+                var zerosSixesOrNines = _signals.First(o => o.Length == 6);
+                var twosThreesOrFives = _signals.First(o => o.Length == 5);
+
+                var segmentChars = new Dictionary<int, string>
+                {
+                    {1, null},
+                    {2, null},
+                    {3, null},
+                    {4, null},
+                    {5, null},
+                    {6, null},
+                    {7, null}
+                };
+
+                if (s[1] != null && s[7] != null)
+                {
+                    var diff = s[7].Replace(s[1], "");
+                    segmentChars[1] = diff;
+                }
+                
+                
+// Segements
+//  1111 
+// 2    3
+// 2    3
+//  4444 
+// 5    6
+// 5    6
+//  7777 
+//
+
+                return 0;
+            }
         }
     }
 }
--- Core/Puzzles/Year2021/Day08/Year2021Day08Tests.cs
+++ Core/Puzzles/Year2021/Day08/Year2021Day08Tests.cs
@@ -7,22 +7,39 @@ namespace Core.Puzzles.Year2021.Day08
         [Test]
         public void Part1()
         {
-            var puzzle = new Year2021Day08();
-            var result = 0;
+            var puzzle = new SevenSegmentDisplayDecoder(Input);
+            var result = puzzle.GetEasyNumbers();
 
-            Assert.That(result, Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo(26));
         }
 
         [Test]
+        public void Part2Single()
+        {
+            const string input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+            var decoder = new DigitDecoder(input);
+            Assert.That(decoder.DecodedNumber, Is.EqualTo(5353));
+        }
+        
+        [Test]
         public void Part2()
         {
-            var puzzle = new Year2021Day08();
-            var result = 0;
+            var puzzle = new SevenSegmentDisplayDecoder(Input);
+            var result = puzzle.GetDecodedSum();
 
             Assert.That(result, Is.EqualTo(0));
         }
 
         private const string Input = @"
-";
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
     }
 }

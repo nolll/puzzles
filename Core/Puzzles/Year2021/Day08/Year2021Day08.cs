6d822e8315b0b180274930fe0493c9bc0bda4599
Henrik Severinson <henriks@gmail.com>
2021-12-08 17:17:45 +0100

    Day 8 2021 solved

--- Core/Puzzles/Year2021/Day08/Year2021Day08.cs
+++ Core/Puzzles/Year2021/Day08/Year2021Day08.cs
@@ -1,19 +1,26 @@
-﻿using Core.Platform;
+﻿using System;
+using Core.Platform;
+using NUnit.Framework.Constraints;
 
 namespace Core.Puzzles.Year2021.Day08
 {
     public class Year2021Day08 : Year2021Day
     {
+        
         public override int Day => 8;
 
         public override PuzzleResult RunPart1()
         {
-            return new PuzzleResult(0);
+            var decoder = new SevenSegmentDisplayDecoder(FileInput);
+            var result = decoder.GetEasyNumbers();
+            return new PuzzleResult(result, 278);
         }
 
         public override PuzzleResult RunPart2()
         {
-            return new PuzzleResult(0);
+            var decoder = new SevenSegmentDisplayDecoder(FileInput);
+            var result = decoder.GetDecodedSum();
+            return new PuzzleResult(result, 986179);
         }
     }
 }

dd20728b7b2fef57652e504b2423b63714fd9305
Henrik <henriks@gmail.com>
2021-12-24 10:02:47 +0100

    just started

--- App/Puzzles/Year2021/Day24/Year2021Day24Tests.cs
+++ App/Puzzles/Year2021/Day24/Year2021Day24Tests.cs
@@ -1,3 +1,4 @@
+using App.Common.Strings;
 using NUnit.Framework;
 
 namespace App.Puzzles.Year2021.Day24;
@@ -5,11 +6,16 @@ namespace App.Puzzles.Year2021.Day24;
 public class Year2021Day24Tests
 {
     [Test]
-    public void Part1()
+    public void TestAlu()
     {
-        var result = 0;
+        const string input = @"
+inp x
+mul x -1";
 
-        Assert.That(result, Is.EqualTo(0));
+        var alu = new Alu(input);
+        var result = alu.Process();
+
+        Assert.That(result.x, Is.EqualTo(0));
     }
 
     [Test]
@@ -22,4 +28,17 @@ public class Year2021Day24Tests
 
     private const string Input = @"
 ";
+}
+
+public class Alu
+{
+    public Alu(string input)
+    {
+        var lines = PuzzleInputReader.ReadLines(input);
+    }
+
+    public (int w, int x, int y, int z) Process()
+    {
+        return (0, 0, 0, 0);
+    }
 }

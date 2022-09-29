f185e57918205c6c87e3f3239c77e58f7222742d
Henrik <henriks@gmail.com>
2022-09-30 00:46:07 +0200

    Optimizing day 15 2018. Cached paths. Need to evaluate empty matrix invalidate based on obstacles

--- core/Common/CoordinateSystems/PathFinder.cs
+++ core/Common/CoordinateSystems/PathFinder.cs
@@ -62,6 +62,56 @@ public static class PathFinder
 
         return queue;
     }
+
+    public static IList<MatrixAddress> ShortestPathTo(StaticMatrix<char> matrix, MatrixAddress from, MatrixAddress to)
+    {
+        var coordCounts = GetCoordCounts(matrix, from, to);
+        var pathMatrix = new Matrix<int>(matrix.Width, matrix.Height, -1);
+        foreach (var coordCount in coordCounts)
+        {
+            pathMatrix.MoveTo(coordCount.X, coordCount.Y);
+            pathMatrix.WriteValue(coordCount.Count);
+        }
+
+        var path = new List<MatrixAddress>();
+        var currentAddress = from;
+        while (!currentAddress.Equals(to))
+        {
+            pathMatrix.MoveTo(currentAddress);
+            var adjacentCoords = pathMatrix.PerpendicularAdjacentCoords
+                .Where(o => pathMatrix.ReadValueAt(o) > -1)
+                .OrderBy(o => pathMatrix.ReadValueAt(o))
+                .ThenBy(o => o.Y)
+                .ThenBy(o => o.X)
+                .ToList();
+            var bestAddress = adjacentCoords.FirstOrDefault();
+            if (bestAddress == null)
+                break;
+            currentAddress = new MatrixAddress(bestAddress.X, bestAddress.Y);
+            path.Add(currentAddress);
+        }
+
+        return path;
+    }
+
+    private static IList<CoordCount> GetCoordCounts(StaticMatrix<char> matrix, MatrixAddress from, MatrixAddress to)
+    {
+        var queue = new List<CoordCount> { new CoordCount(to.X, to.Y, 0) };
+        var index = 0;
+        while (index < queue.Count && !queue.Any(o => o.X == from.X && o.Y == from.Y))
+        {
+            var next = queue[index];
+            matrix.MoveTo(next.X, next.Y);
+            var adjacentCoords = matrix.PerpendicularAdjacentCoords
+                .Where(o => matrix.ReadValueAt(o) == '.' && !queue.Any(q => q.X == o.X && q.Y == o.Y))
+                .ToList();
+            var newCoordCounts = adjacentCoords.Select(o => new CoordCount(o.X, o.Y, next.Count + 1));
+            queue.AddRange(newCoordCounts);
+            index++;
+        }
+
+        return queue;
+    }
 }
 
 [DebuggerDisplay("{X},{Y},{Count}")]

cc7801728c86dbe8e94a8b71ca4ff839bb57abdf
Henrik <henriks@gmail.com>
2022-11-30 13:24:04 +0100

    skeleton for day 1 2022

---
+++ core/Puzzles/Year2022/Day01/Year2022Day01.cs
@@ -0,0 +1,13 @@
+﻿using Core.Platform;
+
+namespace Core.Puzzles.Year2022.Day01;
+
+public class Year2022Day01 : Puzzle
+{
+    public override PuzzleResult RunPart1()
+    {
+        var result = 0;
+
+        return new PuzzleResult(result);
+    }
+}

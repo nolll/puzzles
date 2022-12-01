6b601d30bec26787e742b1b85cb07157d19abd27
Henrik Severinson <henriks@gmail.com>
2022-12-01 06:50:50 +0100

    Day 1 2022 solved

---
+++ core/Puzzles/Year2022/Day01/Year2021Day01Tests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+namespace Core.Puzzles.Year2022.Day01;
+
+public class Year2022Day01Tests
+{
+    [Test]
+    public void Part1()
+    {
+        var calorieCounts = new CalorieCounts(Input);
+        var result = calorieCounts.TopSum;
+
+        Assert.That(result, Is.EqualTo(24000));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var calorieCounts = new CalorieCounts(Input);
+        var result = calorieCounts.Top3Sum;
+
+        Assert.That(result, Is.EqualTo(45000));
+    }
+
+    private const string Input = @"
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+}

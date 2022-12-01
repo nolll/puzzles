ee453e94cdff1e61e6aef3fcc1d19b00e0132d1f
Henrik <henriks@gmail.com>
2022-12-01 06:49:49 +0100

    Day 1 2022 solved

--- core/Puzzles/Year2022/Day01/Year2022Day01.cs
+++ core/Puzzles/Year2022/Day01/Year2022Day01.cs
@@ -1,13 +1,18 @@
 ﻿using Core.Platform;
 
-namespace Core.Puzzles.Year2022.Day01;
+namespace Core.Puzzles.Year2022.Day01;  
 
 public class Year2022Day01 : Puzzle
 {
     public override PuzzleResult RunPart1()
     {
-        var result = 0;
+        var calorieCounts = new CalorieCounts(FileInput);
+        return new PuzzleResult(calorieCounts.TopSum, 71023);
+    }
 
-        return new PuzzleResult(result);
+    public override PuzzleResult RunPart2()
+    {
+        var calorieCounts = new CalorieCounts(FileInput);
+        return new PuzzleResult(calorieCounts.Top3Sum, 206289);
     }
 }

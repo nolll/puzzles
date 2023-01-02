48517175724682ea64084bbeacae27f81b7b6930
Henrik <henriks@gmail.com>
2023-01-02 17:52:24 +0100

    Changed to quick matrix for day 24 2019

--- core/Puzzles/Year2019/Day24/RecursiveBugLifeSimulator.cs
+++ core/Puzzles/Year2019/Day24/RecursiveBugLifeSimulator.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using Core.Common.CoordinateSystems;
 using Core.Common.CoordinateSystems.CoordinateSystem2D;
 
 namespace Core.Puzzles.Year2019.Day24;
@@ -14,7 +13,7 @@ public class RecursiveBugLifeSimulator
     private IDictionary<int, IMatrix<char>> _matrixes;
     private readonly IDictionary<MatrixAddress, IList<RelativeLevelAddress>> _relativeAddresses;
     private readonly Dictionary<int, MatrixAddress> _cells;
-    private readonly IMatrix<char> _emptyMatrix = new DynamicMatrix<char>(Size, Size, '.');
+    private readonly IMatrix<char> _emptyMatrix = new QuickMatrix<char>(Size, Size, '.');
 
     public int BugCount => _matrixes.Values.Sum(o => o.Values.Count(m => m == '#'));
 
@@ -104,9 +103,9 @@ public class RecursiveBugLifeSimulator
         return currentValue;
     }
 
-    private DynamicMatrix<char> BuildMatrix(string map)
+    private static IMatrix<char> BuildMatrix(string map)
     {
-        var matrix = new DynamicMatrix<char>(1, 1);
+        var matrix = new QuickMatrix<char>(1, 1);
         var rows = map.Trim().Split('\n');
         var y = 0;
         foreach (var row in rows)
@@ -126,34 +125,34 @@ public class RecursiveBugLifeSimulator
         return matrix;
     }
 
-    private Dictionary<int, MatrixAddress> BuildCells()
+    private static Dictionary<int, MatrixAddress> BuildCells()
     {
         return new Dictionary<int, MatrixAddress>
         {
-            [1] = new MatrixAddress(0, 0),
-            [2] = new MatrixAddress(1, 0),
-            [3] = new MatrixAddress(2, 0),
-            [4] = new MatrixAddress(3, 0),
-            [5] = new MatrixAddress(4, 0),
-            [6] = new MatrixAddress(0, 1),
-            [7] = new MatrixAddress(1, 1),
-            [8] = new MatrixAddress(2, 1),
-            [9] = new MatrixAddress(3, 1),
-            [10] = new MatrixAddress(4, 1),
-            [11] = new MatrixAddress(0, 2),
-            [12] = new MatrixAddress(1, 2),
-            [14] = new MatrixAddress(3, 2),
-            [15] = new MatrixAddress(4, 2),
-            [16] = new MatrixAddress(0, 3),
-            [17] = new MatrixAddress(1, 3),
-            [18] = new MatrixAddress(2, 3),
-            [19] = new MatrixAddress(3, 3),
-            [20] = new MatrixAddress(4, 3),
-            [21] = new MatrixAddress(0, 4),
-            [22] = new MatrixAddress(1, 4),
-            [23] = new MatrixAddress(2, 4),
-            [24] = new MatrixAddress(3, 4),
-            [25] = new MatrixAddress(4, 4)
+            [1] = new(0, 0),
+            [2] = new(1, 0),
+            [3] = new(2, 0),
+            [4] = new(3, 0),
+            [5] = new(4, 0),
+            [6] = new(0, 1),
+            [7] = new(1, 1),
+            [8] = new(2, 1),
+            [9] = new(3, 1),
+            [10] = new(4, 1),
+            [11] = new(0, 2),
+            [12] = new(1, 2),
+            [14] = new(3, 2),
+            [15] = new(4, 2),
+            [16] = new(0, 3),
+            [17] = new(1, 3),
+            [18] = new(2, 3),
+            [19] = new(3, 3),
+            [20] = new(4, 3),
+            [21] = new(0, 4),
+            [22] = new(1, 4),
+            [23] = new(2, 4),
+            [24] = new(3, 4),
+            [25] = new(4, 4)
         };
     }
 
@@ -163,187 +162,187 @@ public class RecursiveBugLifeSimulator
         {
             [_cells[1]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(OuterLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[2]),
-                new RelativeLevelAddress(SameLevel, _cells[6]),
-                new RelativeLevelAddress(OuterLevel, _cells[12])
+                new(OuterLevel, _cells[8]),
+                new(SameLevel, _cells[2]),
+                new(SameLevel, _cells[6]),
+                new(OuterLevel, _cells[12])
             },
             [_cells[2]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(OuterLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[3]),
-                new RelativeLevelAddress(SameLevel, _cells[7]),
-                new RelativeLevelAddress(SameLevel, _cells[1])
+                new(OuterLevel, _cells[8]),
+                new(SameLevel, _cells[3]),
+                new(SameLevel, _cells[7]),
+                new(SameLevel, _cells[1])
             },
             [_cells[3]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(OuterLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[4]),
-                new RelativeLevelAddress(SameLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[2])
+                new(OuterLevel, _cells[8]),
+                new(SameLevel, _cells[4]),
+                new(SameLevel, _cells[8]),
+                new(SameLevel, _cells[2])
             },
             [_cells[4]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(OuterLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[5]),
-                new RelativeLevelAddress(SameLevel, _cells[9]),
-                new RelativeLevelAddress(SameLevel, _cells[3])
+                new(OuterLevel, _cells[8]),
+                new(SameLevel, _cells[5]),
+                new(SameLevel, _cells[9]),
+                new(SameLevel, _cells[3])
             },
             [_cells[5]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(OuterLevel, _cells[8]),
-                new RelativeLevelAddress(OuterLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[10]),
-                new RelativeLevelAddress(SameLevel, _cells[4])
+                new(OuterLevel, _cells[8]),
+                new(OuterLevel, _cells[14]),
+                new(SameLevel, _cells[10]),
+                new(SameLevel, _cells[4])
             },
             [_cells[6]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[1]),
-                new RelativeLevelAddress(SameLevel, _cells[7]),
-                new RelativeLevelAddress(SameLevel, _cells[11]),
-                new RelativeLevelAddress(OuterLevel, _cells[12])
+                new(SameLevel, _cells[1]),
+                new(SameLevel, _cells[7]),
+                new(SameLevel, _cells[11]),
+                new(OuterLevel, _cells[12])
             },
             [_cells[7]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[2]),
-                new RelativeLevelAddress(SameLevel, _cells[8]),
-                new RelativeLevelAddress(SameLevel, _cells[12]),
-                new RelativeLevelAddress(SameLevel, _cells[6])
+                new(SameLevel, _cells[2]),
+                new(SameLevel, _cells[8]),
+                new(SameLevel, _cells[12]),
+                new(SameLevel, _cells[6])
             },
             [_cells[8]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[3]),
-                new RelativeLevelAddress(SameLevel, _cells[9]),
-                new RelativeLevelAddress(InnerLevel, _cells[1]),
-                new RelativeLevelAddress(InnerLevel, _cells[2]),
-                new RelativeLevelAddress(InnerLevel, _cells[3]),
-                new RelativeLevelAddress(InnerLevel, _cells[4]),
-                new RelativeLevelAddress(InnerLevel, _cells[5]),
-                new RelativeLevelAddress(SameLevel, _cells[7])
+                new(SameLevel, _cells[3]),
+                new(SameLevel, _cells[9]),
+                new(InnerLevel, _cells[1]),
+                new(InnerLevel, _cells[2]),
+                new(InnerLevel, _cells[3]),
+                new(InnerLevel, _cells[4]),
+                new(InnerLevel, _cells[5]),
+                new(SameLevel, _cells[7])
             },
             [_cells[9]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[4]),
-                new RelativeLevelAddress(SameLevel, _cells[10]),
-                new RelativeLevelAddress(SameLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[8])
+                new(SameLevel, _cells[4]),
+                new(SameLevel, _cells[10]),
+                new(SameLevel, _cells[14]),
+                new(SameLevel, _cells[8])
             },
             [_cells[10]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[5]),
-                new RelativeLevelAddress(OuterLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[15]),
-                new RelativeLevelAddress(SameLevel, _cells[9])
+                new(SameLevel, _cells[5]),
+                new(OuterLevel, _cells[14]),
+                new(SameLevel, _cells[15]),
+                new(SameLevel, _cells[9])
             },
             [_cells[11]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[6]),
-                new RelativeLevelAddress(SameLevel, _cells[12]),
-                new RelativeLevelAddress(SameLevel, _cells[16]),
-                new RelativeLevelAddress(OuterLevel, _cells[12])
+                new(SameLevel, _cells[6]),
+                new(SameLevel, _cells[12]),
+                new(SameLevel, _cells[16]),
+                new(OuterLevel, _cells[12])
             },
             [_cells[12]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[7]),
-                new RelativeLevelAddress(InnerLevel, _cells[1]),
-                new RelativeLevelAddress(InnerLevel, _cells[6]),
-                new RelativeLevelAddress(InnerLevel, _cells[11]),
-                new RelativeLevelAddress(InnerLevel, _cells[16]),
-                new RelativeLevelAddress(InnerLevel, _cells[21]),
-                new RelativeLevelAddress(SameLevel, _cells[17]),
-                new RelativeLevelAddress(SameLevel, _cells[11])
+                new(SameLevel, _cells[7]),
+                new(InnerLevel, _cells[1]),
+                new(InnerLevel, _cells[6]),
+                new(InnerLevel, _cells[11]),
+                new(InnerLevel, _cells[16]),
+                new(InnerLevel, _cells[21]),
+                new(SameLevel, _cells[17]),
+                new(SameLevel, _cells[11])
             },
             [_cells[14]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[9]),
-                new RelativeLevelAddress(SameLevel, _cells[15]),
-                new RelativeLevelAddress(SameLevel, _cells[19]),
-                new RelativeLevelAddress(InnerLevel, _cells[5]),
-                new RelativeLevelAddress(InnerLevel, _cells[10]),
-                new RelativeLevelAddress(InnerLevel, _cells[15]),
-                new RelativeLevelAddress(InnerLevel, _cells[20]),
-                new RelativeLevelAddress(InnerLevel, _cells[25])
+                new(SameLevel, _cells[9]),
+                new(SameLevel, _cells[15]),
+                new(SameLevel, _cells[19]),
+                new(InnerLevel, _cells[5]),
+                new(InnerLevel, _cells[10]),
+                new(InnerLevel, _cells[15]),
+                new(InnerLevel, _cells[20]),
+                new(InnerLevel, _cells[25])
             },
             [_cells[15]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[10]),
-                new RelativeLevelAddress(OuterLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[20]),
-                new RelativeLevelAddress(SameLevel, _cells[14])
+                new(SameLevel, _cells[10]),
+                new(OuterLevel, _cells[14]),
+                new(SameLevel, _cells[20]),
+                new(SameLevel, _cells[14])
             },
             [_cells[16]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[11]),
-                new RelativeLevelAddress(SameLevel, _cells[17]),
-                new RelativeLevelAddress(SameLevel, _cells[21]),
-                new RelativeLevelAddress(OuterLevel, _cells[12])
+                new(SameLevel, _cells[11]),
+                new(SameLevel, _cells[17]),
+                new(SameLevel, _cells[21]),
+                new(OuterLevel, _cells[12])
             },
             [_cells[17]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[12]),
-                new RelativeLevelAddress(SameLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[22]),
-                new RelativeLevelAddress(SameLevel, _cells[16])
+                new(SameLevel, _cells[12]),
+                new(SameLevel, _cells[18]),
+                new(SameLevel, _cells[22]),
+                new(SameLevel, _cells[16])
             },
             [_cells[18]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(InnerLevel, _cells[21]),
-                new RelativeLevelAddress(InnerLevel, _cells[22]),
-                new RelativeLevelAddress(InnerLevel, _cells[23]),
-                new RelativeLevelAddress(InnerLevel, _cells[24]),
-                new RelativeLevelAddress(InnerLevel, _cells[25]),
-                new RelativeLevelAddress(SameLevel, _cells[19]),
-                new RelativeLevelAddress(SameLevel, _cells[23]),
-                new RelativeLevelAddress(SameLevel, _cells[17])
+                new(InnerLevel, _cells[21]),
+                new(InnerLevel, _cells[22]),
+                new(InnerLevel, _cells[23]),
+                new(InnerLevel, _cells[24]),
+                new(InnerLevel, _cells[25]),
+                new(SameLevel, _cells[19]),
+                new(SameLevel, _cells[23]),
+                new(SameLevel, _cells[17])
             },
             [_cells[19]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[20]),
-                new RelativeLevelAddress(SameLevel, _cells[24]),
-                new RelativeLevelAddress(SameLevel, _cells[18])
+                new(SameLevel, _cells[14]),
+                new(SameLevel, _cells[20]),
+                new(SameLevel, _cells[24]),
+                new(SameLevel, _cells[18])
             },
             [_cells[20]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[15]),
-                new RelativeLevelAddress(OuterLevel, _cells[14]),
-                new RelativeLevelAddress(SameLevel, _cells[25]),
-                new RelativeLevelAddress(SameLevel, _cells[19])
+                new(SameLevel, _cells[15]),
+                new(OuterLevel, _cells[14]),
+                new(SameLevel, _cells[25]),
+                new(SameLevel, _cells[19])
             },
             [_cells[21]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[16]),
-                new RelativeLevelAddress(SameLevel, _cells[22]),
-                new RelativeLevelAddress(OuterLevel, _cells[18]),
-                new RelativeLevelAddress(OuterLevel, _cells[12])
+                new(SameLevel, _cells[16]),
+                new(SameLevel, _cells[22]),
+                new(OuterLevel, _cells[18]),
+                new(OuterLevel, _cells[12])
             },
             [_cells[22]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[17]),
-                new RelativeLevelAddress(SameLevel, _cells[23]),
-                new RelativeLevelAddress(OuterLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[21])
+                new(SameLevel, _cells[17]),
+                new(SameLevel, _cells[23]),
+                new(OuterLevel, _cells[18]),
+                new(SameLevel, _cells[21])
             },
             [_cells[23]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[24]),
-                new RelativeLevelAddress(OuterLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[22])
+                new(SameLevel, _cells[18]),
+                new(SameLevel, _cells[24]),
+                new(OuterLevel, _cells[18]),
+                new(SameLevel, _cells[22])
             },
             [_cells[24]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[19]),
-                new RelativeLevelAddress(SameLevel, _cells[25]),
-                new RelativeLevelAddress(OuterLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[23])
+                new(SameLevel, _cells[19]),
+                new(SameLevel, _cells[25]),
+                new(OuterLevel, _cells[18]),
+                new(SameLevel, _cells[23])
             },
             [_cells[25]] = new List<RelativeLevelAddress>
             {
-                new RelativeLevelAddress(SameLevel, _cells[20]),
-                new RelativeLevelAddress(OuterLevel, _cells[14]),
-                new RelativeLevelAddress(OuterLevel, _cells[18]),
-                new RelativeLevelAddress(SameLevel, _cells[24])
+                new(SameLevel, _cells[20]),
+                new(OuterLevel, _cells[14]),
+                new(OuterLevel, _cells[18]),
+                new(SameLevel, _cells[24])
             }
         };
 

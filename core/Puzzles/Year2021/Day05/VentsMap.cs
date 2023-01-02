e2daa572911b26bbbfd7040c9777135a02605cf1
Henrik <henriks@gmail.com>
2023-01-02 18:30:30 +0100

    Changed to quick matrix for day 5 2021

--- core/Puzzles/Year2021/Day05/VentsMap.cs
+++ core/Puzzles/Year2021/Day05/VentsMap.cs
@@ -17,7 +17,7 @@ public class VentsMap
             lines = lines.Where(o => o.IsPerpendicular).ToList();
         var width = lines.Max(o => Math.Max(o.Start.X, o.End.X));
         var height = lines.Max(o => Math.Max(o.Start.Y, o.End.Y));
-        var matrix = new DynamicMatrix<int>(width, height);
+        var matrix = new QuickMatrix<int>(width, height);
 
         matrix = MapLines(matrix, lines);
         var c = matrix.Values.Count(o => o >= 2);
@@ -25,34 +25,31 @@ public class VentsMap
         return c;
     }
 
-    private DynamicMatrix<int> MapLines(DynamicMatrix<int> matrix, List<Line2d> lines)
+    private static QuickMatrix<int> MapLines(QuickMatrix<int> matrix, List<Line2d> lines)
     {
         foreach (var line in lines)
         {
             foreach (var pos in line.Positions)
             {
-                matrix.MoveTo(pos.X, pos.Y);
-                var v = matrix.ReadValue();
-                v += 1;
-                matrix.WriteValue(v);
+                matrix.WriteValueAt(pos.X, pos.Y, matrix.ReadValueAt(pos.X, pos.Y) + 1);
             }
         }
 
         return matrix;
     }
 
-    private List<Line2d> ParseLines(IList<string> rows)
+    private static List<Line2d> ParseLines(IEnumerable<string> rows)
     {
         return rows.Select(ParseLine).ToList();
     }
 
-    private Line2d ParseLine(string s)
+    private static Line2d ParseLine(string s)
     {
         var positions = s.Split(" -> ").Select(ParsePosition).ToList();
         return new Line2d(positions[0], positions[1]);
     }
 
-    private Position2d ParsePosition(string s)
+    private static Position2d ParsePosition(string s)
     {
         var parts = s.Split(',').Select(int.Parse).ToArray();
         return new Position2d(parts[0], parts[1]);

5122901f77c0f987893bde47c6b4aea30b2ddb77
Henrik <henriks@gmail.com>
2023-01-06 17:08:09 +0100

    Removed superclasses and moved everything to QuickMatrix

--- core/Common/CoordinateSystems/CoordinateSystem2D/QuickMatrix.cs
+++ core/Common/CoordinateSystems/CoordinateSystem2D/QuickMatrix.cs
@@ -1,39 +1,44 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 
 namespace Core.Common.CoordinateSystems.CoordinateSystem2D;
 
-public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
+public class QuickMatrix<T> : BaseMatrix, IMatrix<T>
 {
+    private readonly T _defaultValue;
     private readonly IDictionary<(int x, int y), T> _matrix;
     private int _minx;
     private int _maxx;
     private int _miny;
     private int _maxy;
 
-    public override IEnumerable<T> Values
+    public int Width => _maxx - _minx + 1;
+    public int Height => _maxy - _miny + 1;
+    public int XMin => _minx;
+    public int XMax => _maxx;
+    public int YMin => _miny;
+    public int YMax => _maxy;
+    public MatrixDirection Direction { get; private set; }
+    public MatrixAddress Address { get; private set; }
+    public MatrixAddress StartAddress { get; }
+    public bool IsAtTop => Address.Y == 0;
+    public bool IsAtRightEdge => Address.X == Width - 1;
+    public bool IsAtBottom => Address.Y == Height - 1;
+    public bool IsAtLeftEdge => Address.X == 0;
+    public MatrixAddress Center => new(Width / 2, Height / 2);
+
+    private QuickMatrix(T defaultValue)
     {
-        get
-        {
-            foreach (var coord in Coords)
-            {
-                if (_matrix.TryGetValue((coord.X, coord.Y), out var v))
-                    yield return v;
-                else
-                    yield return DefaultValue;
-            }
-        }
+        _defaultValue = defaultValue;
+        Address = new MatrixAddress(0, 0);
+        StartAddress = new MatrixAddress(0, 0);
+        Direction = MatrixDirection.Up;
     }
 
-    public override int Width => _maxx - _minx + 1;
-    public override int Height => _maxy - _miny + 1;
-    public override int XMin => _minx;
-    public override int XMax => _maxx;
-    public override int YMin => _miny;
-    public override int YMax => _maxy;
-
     public QuickMatrix(int width = 1, int height = 1, T defaultValue = default)
-        : base(defaultValue)
+        : this(defaultValue)
     {
         _minx = 0;
         _maxx = width - 1;
@@ -43,7 +48,7 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
     }
 
     private QuickMatrix(MatrixAddress min, MatrixAddress max, IDictionary<(int x, int y), T> values, T defaultValue = default)
-        : base(defaultValue)
+        : this(defaultValue)
     {
         _matrix = values;
         _minx = min.X;
@@ -52,14 +57,42 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
         _maxy = max.Y;
     }
 
-    public override T ReadValueAt(int x, int y)
+    public IEnumerable<T> Values
+    {
+        get
+        {
+            foreach (var coord in Coords)
+            {
+                if (_matrix.TryGetValue((coord.X, coord.Y), out var v))
+                    yield return v;
+                else
+                    yield return _defaultValue;
+            }
+        }
+    }
+
+    public IEnumerable<MatrixAddress> Coords
+    {
+        get
+        {
+            for (var y = YMin; y <= YMax; y++)
+            {
+                for (var x = XMin; x <= XMax; x++)
+                {
+                    yield return new MatrixAddress(x, y);
+                }
+            }
+        }
+    }
+
+    public T ReadValueAt(int x, int y)
     {
         return _matrix.TryGetValue((x, y), out var v)
             ? v
-            : DefaultValue;
+            : _defaultValue;
     }
 
-    public override void WriteValueAt(int x, int y, T value)
+    public void WriteValueAt(int x, int y, T value)
     {
         if (x < _minx)
             _minx = x;
@@ -74,12 +107,7 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
         _matrix[(x, y)] = value;
     }
     
-    protected override IMatrix<T> Create(int width, int height, T defaultValue = default)
-    {
-        return new QuickMatrix<T>(width, height, DefaultValue);
-    }
-
-    protected override void HandleExtend(MatrixAddress address)
+    private void HandleExtend(MatrixAddress address)
     {
         ExtendMatrix(address);
     }
@@ -144,7 +172,7 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
             _maxx += numberOfCols;
     }
 
-    public override void ExtendAllDirections(int steps = 1)
+    public void ExtendAllDirections(int steps = 1)
     {
         ExtendUp(steps);
         ExtendRight(steps);
@@ -152,53 +180,53 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
         ExtendLeft(steps);
     }
 
-    public override void ExtendUp(int steps = 1)
+    public void ExtendUp(int steps = 1)
     {
         AddRows(steps, MatrixAddMode.Prepend);
     }
 
-    public override void ExtendRight(int steps = 1)
+    public void ExtendRight(int steps = 1)
     {
         AddCols(steps, MatrixAddMode.Append);
     }
 
-    public override void ExtendDown(int steps = 1)
+    public void ExtendDown(int steps = 1)
     {
         AddRows(steps, MatrixAddMode.Append);
     }
 
-    public override void ExtendLeft(int steps = 1)
+    public void ExtendLeft(int steps = 1)
     {
         AddCols(steps, MatrixAddMode.Prepend);
     }
 
-    public override IMatrix<T> Copy()
+    public IMatrix<T> Copy()
     {
         var values = _matrix.ToDictionary(item => (item.Key.x, item.Key.y), item => item.Value);
         var min = new MatrixAddress(XMin, YMin);
         var max = new MatrixAddress(XMax, YMax);
-        return new QuickMatrix<T>(min, max, values, DefaultValue);
+        return new QuickMatrix<T>(min, max, values, _defaultValue);
     }
 
-    public override IMatrix<T> RotateLeft()
+    public IMatrix<T> RotateLeft()
     {
         var values = _matrix.ToDictionary(item => (item.Key.y, YMax - item.Key.x), item => item.Value);
         var min = new MatrixAddress(YMin, YMin);
         var max = new MatrixAddress(XMax, YMax);
-        var newMatrix = new QuickMatrix<T>(min, max, values, DefaultValue);
+        var newMatrix = new QuickMatrix<T>(min, max, values, _defaultValue);
         return newMatrix;
     }
 
-    public override IMatrix<T> RotateRight()
+    public IMatrix<T> RotateRight()
     {
         var values = _matrix.ToDictionary(item => (XMax - item.Key.y, item.Key.x), item => item.Value);
         var min = new MatrixAddress(YMin, YMin);
         var max = new MatrixAddress(XMax, YMax);
-        var newMatrix = new QuickMatrix<T>(min, max, values, DefaultValue);
+        var newMatrix = new QuickMatrix<T>(min, max, values, _defaultValue);
         return newMatrix;
     }
 
-    public override IMatrix<T> Slice(MatrixAddress from = null, MatrixAddress to = null)
+    public IMatrix<T> Slice(MatrixAddress from = null, MatrixAddress to = null)
     {
         from ??= new MatrixAddress(XMin, YMin);
         to ??= new MatrixAddress(XMax, YMax);
@@ -209,29 +237,284 @@ public class QuickMatrix<T> : Base2DMatrix<T>, IMatrix<T>
             .ToDictionary(item => (item.Key.x - dx, item.Key.y - dy), item => item.Value);
         var slicedFrom = new MatrixAddress(from.X - dx, from.Y - dy);
         var slicedTo = new MatrixAddress(to.X - dx, to.Y - dy);
-        var newMatrix = new QuickMatrix<T>(slicedFrom, slicedTo, values, DefaultValue);
+        var newMatrix = new QuickMatrix<T>(slicedFrom, slicedTo, values, _defaultValue);
         return newMatrix;
     }
 
-    public override IMatrix<T> Slice(MatrixAddress from, int width, int height)
+    public IMatrix<T> Slice(MatrixAddress from, int width, int height)
     {
         var to = new MatrixAddress(from.X + width, from.Y + height);
         return Slice(from, to);
     }
     
-    public override IMatrix<T> FlipVertical()
+    public IMatrix<T> FlipVertical()
     {
         var values = _matrix.ToDictionary(item => (item.Key.x, YMax - item.Key.y), item => item.Value);
         var min = new MatrixAddress(YMin, YMin);
         var max = new MatrixAddress(XMax, YMax);
-        return new QuickMatrix<T>(min, max, values, DefaultValue);
+        return new QuickMatrix<T>(min, max, values, _defaultValue);
     }
 
-    public override IMatrix<T> FlipHorizontal()
+    public IMatrix<T> FlipHorizontal()
     {
         var values = _matrix.ToDictionary(item => (XMax - item.Key.x, item.Key.y), item => item.Value);
         var min = new MatrixAddress(YMin, YMin);
         var max = new MatrixAddress(XMax, YMax);
-        return new QuickMatrix<T>(min, max, values, DefaultValue);
+        return new QuickMatrix<T>(min, max, values, _defaultValue);
+    }
+
+    public bool TryMoveTo(MatrixAddress address)
+    {
+        return MoveTo(address, false);
+    }
+
+    public bool MoveTo(MatrixAddress address)
+    {
+        return MoveTo(address, true);
+    }
+
+    private bool MoveTo(MatrixAddress address, bool extend)
+    {
+        if (IsOutOfRange(address))
+        {
+            if (extend)
+                HandleExtend(address);
+            else
+                return false;
+        }
+
+        var x = address.X > XMin ? address.X : XMin;
+        var y = address.Y > YMin ? address.Y : YMin;
+        Address = new MatrixAddress(x, y);
+        return true;
+    }
+
+    public bool TryMoveTo(int x, int y)
+    {
+        return MoveTo(new MatrixAddress(x, y), false);
+    }
+
+    public bool MoveTo(int x, int y)
+    {
+        return MoveTo(new MatrixAddress(x, y), true);
+    }
+
+    public bool TryMoveForward()
+    {
+        return MoveForward(false);
+    }
+
+    public bool MoveForward()
+    {
+        return MoveForward(true);
+    }
+
+    private bool MoveForward(bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X + Direction.X, Address.Y + Direction.Y), extend);
+    }
+
+    public bool TryMoveBackward()
+    {
+        return MoveBackward(false);
+    }
+
+    public bool MoveBackward()
+    {
+        return MoveBackward(true);
+    }
+
+    private bool MoveBackward(bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X - Direction.X, Address.Y - Direction.Y), extend);
+    }
+
+    public bool TryMoveUp(int steps = 1)
+    {
+        return MoveUp(steps, false);
+    }
+
+    public bool MoveUp(int steps = 1)
+    {
+        return MoveUp(steps, true);
+    }
+
+    private bool MoveUp(int steps, bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X, Address.Y - steps), extend);
+    }
+
+    public bool TryMoveRight(int steps = 1)
+    {
+        return MoveRight(steps, false);
+    }
+
+    public bool MoveRight(int steps = 1)
+    {
+        return MoveRight(steps, true);
+    }
+
+    private bool MoveRight(int steps, bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X + steps, Address.Y), extend);
+    }
+
+    public bool TryMoveDown(int steps = 1)
+    {
+        return MoveDown(steps, false);
+    }
+
+    public bool MoveDown(int steps = 1)
+    {
+        return MoveDown(steps, true);
+    }
+
+    private bool MoveDown(int steps, bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X, Address.Y + steps), extend);
+    }
+
+    public bool TryMoveLeft(int steps = 1)
+    {
+        return MoveLeft(steps, false);
+    }
+
+    public bool MoveLeft(int steps = 1)
+    {
+        return MoveLeft(steps, true);
+    }
+
+    private bool MoveLeft(int steps, bool extend)
+    {
+        return MoveTo(new MatrixAddress(Address.X - steps, Address.Y), extend);
+    }
+
+    public MatrixDirection TurnLeft()
+    {
+        if (Direction.Equals(MatrixDirection.Up))
+            return TurnTo(MatrixDirection.Left);
+        if (Direction.Equals(MatrixDirection.Right))
+            return TurnTo(MatrixDirection.Up);
+        if (Direction.Equals(MatrixDirection.Down))
+            return TurnTo(MatrixDirection.Right);
+        return TurnTo(MatrixDirection.Down);
+    }
+
+    public MatrixDirection TurnRight()
+    {
+        if (Direction.Equals(MatrixDirection.Up))
+            return TurnTo(MatrixDirection.Right);
+        if (Direction.Equals(MatrixDirection.Right))
+            return TurnTo(MatrixDirection.Down);
+        if (Direction.Equals(MatrixDirection.Down))
+            return TurnTo(MatrixDirection.Left);
+        return TurnTo(MatrixDirection.Up);
+    }
+
+    public MatrixDirection TurnTo(MatrixDirection direction)
+    {
+        Direction = direction;
+        return direction;
+    }
+
+    public string Print(bool markCurrentAddress = false, bool markStartAddress = false, T currentAddressMarker = default, T startAddressMarker = default, bool spacing = false)
+    {
+        var sb = new StringBuilder();
+
+        for (var y = YMin; y <= YMax; y++)
+        {
+            for (var x = XMin; x <= XMax; x++)
+            {
+                if (markCurrentAddress && x == Address.X && y == Address.Y)
+                    sb.Append('D');
+                else if (markStartAddress && x == StartAddress.X && y == StartAddress.Y)
+                    sb.Append('S');
+                else
+                    sb.Append(ReadValueAt(x, y));
+
+                if (spacing)
+                    sb.Append('.');
+            }
+
+            sb.AppendLine();
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public T ReadValue()
+    {
+        return ReadValueAt(Address.X, Address.Y);
+    }
+
+    public T ReadValueAt(MatrixAddress address)
+    {
+        return ReadValueAt(address.X, address.Y);
+    }
+
+    public void WriteValue(T value)
+    {
+        WriteValueAt(Address.X, Address.Y, value);
+    }
+
+    public void WriteValueAt(MatrixAddress address, T value)
+    {
+        WriteValueAt(address.X, address.Y, value);
+    }
+
+    public IList<MatrixAddress> FindAddresses(T value)
+    {
+        var addresses = new List<MatrixAddress>();
+        for (var y = YMin; y <= YMax; y++)
+        {
+            for (var x = XMin; x <= XMax; x++)
+            {
+                var address = new MatrixAddress(x, y);
+                var val = ReadValueAt(address);
+                if (val.Equals(value))
+                    addresses.Add(address);
+            }
+        }
+
+        return addresses;
+    }
+
+    public bool IsOutOfRange(MatrixAddress address)
+    {
+        return address.Y > YMax ||
+               address.Y < YMin ||
+               address.X > XMax ||
+               address.X < XMin;
+    }
+
+    public IList<T> PerpendicularAdjacentValues => PerpendicularAdjacentCoords.Select(ReadValueAt).ToList();
+    public IList<T> PerpendicularAdjacentValuesTo(MatrixAddress address) => PerpendicularAdjacentCoordsTo(address).Select(ReadValueAt).ToList();
+    public IList<MatrixAddress> PerpendicularAdjacentCoords => PerpendicularAdjacentCoordsTo(Address);
+    public IList<MatrixAddress> PerpendicularAdjacentCoordsTo(MatrixAddress address) => PossiblePerpendicularAdjacentCoordsTo(address).Where(o => !IsOutOfRange(o)).ToList();
+
+    private IEnumerable<MatrixAddress> PossiblePerpendicularAdjacentCoordsTo(MatrixAddress address) => new List<MatrixAddress>
+    {
+        new(address.X, address.Y - 1),
+        new(address.X + 1, address.Y),
+        new(address.X, address.Y + 1),
+        new(address.X - 1, address.Y)
+    };
+
+    public IList<T> AllAdjacentValues => AllAdjacentCoordsTo(Address).Select(ReadValueAt).ToList();
+    public IList<T> AllAdjacentValuesTo(MatrixAddress address) => AllAdjacentCoordsTo(address).Select(ReadValueAt).ToList();
+    public IList<MatrixAddress> AllAdjacentCoords => AllAdjacentCoordsTo(Address);
+    public IList<MatrixAddress> AllAdjacentCoordsTo(MatrixAddress address) => AllPossibleAdjacentCoordsTo(address).Where(o => !IsOutOfRange(o)).ToList();
+
+    private IEnumerable<MatrixAddress> AllPossibleAdjacentCoordsTo(MatrixAddress address)
+    {
+        foreach (var dy in AdjacentDeltas)
+        {
+            foreach (var dx in AdjacentDeltas)
+            {
+                var coord = new MatrixAddress(address.X + dx, address.Y + dy);
+                if (!coord.Equals(address))
+                    yield return coord;
+            }
+        }
     }
 }

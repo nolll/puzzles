da9e3899b2546d1ec7d6bd170060f58213310c4c
Henrik <henriks@gmail.com>
2023-09-30 00:48:17 +0200

    wip. need to remove extra tokens before measure

---
+++ aquaq/Puzzles/Aquaq13/Aquaq13.cs
@@ -0,0 +1,56 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq13;
+
+public class Aquaq13 : AquaqPuzzle
+{
+    public override string Name => "O RLE?";
+
+    protected override PuzzleResult Run()
+    {
+        var lines = InputFile.Split(Environment.NewLine);
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            sum += FindMaxRepeats(line);
+        }
+
+        return new PuzzleResult(sum);
+    }
+
+    public static int FindMaxRepeats(string s)
+    {
+        var tokens = FindTokens(s);
+        var maxCount = 0;
+        var maxToken = "";
+
+        foreach (var token in tokens)
+        {
+            var length = s.Split(token).Length;
+            if (length > maxCount)
+            {
+                maxCount = length;
+                maxToken = token;
+            }
+        }
+
+        var otherTokens = s.Replace(maxToken, "|").Split('|').Where(o => o != "");
+        foreach (var otherToken in otherTokens)
+        {
+            s = s.Replace(otherToken, "");
+        }
+
+        return s.Split(maxToken).Length - 1;
+    }
+
+    private static IEnumerable<string> FindTokens(string s)
+    {
+        for (var i = 0; i < s.Length; i++)
+        {
+            for (var j = i; j < s.Length - i; j++)
+            {
+                yield return s.Substring(i, j);
+            }
+        }
+    }
+}
---
+++ aquaq/Puzzles/Aquaq13/Aquaq13Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq13;
+
+public class Aquaq13Tests
+{
+    [TestCase("ABCABCABCABCABC", 5)]
+    [TestCase("AAAAAAB", 6)]
+    [TestCase("AAAAAABAAAAAAB", 12)]
+    [TestCase("buhtbuhtbuhtbuhtbuhtbuhtbuhtkbzowxgrfyvmltjp", 8)]
+    public void FindMaxRepeats(string input, int expected)
+    {
+        var result = Aquaq13.FindMaxRepeats(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}

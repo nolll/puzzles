0f8bfd88fbf4e1009a986a6e0a0382e8003c3531
Henrik <henriks@gmail.com>
2023-10-01 01:01:01 +0200

    wip aquaq 14

---
+++ aquaq/Puzzles/Aquaq14/Aquaq14.cs
@@ -0,0 +1,22 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq14;
+
+public class Aquaq14 : AquaqPuzzle
+{
+    private const string Board =
+"""
+6  17 34 50 68
+10 21 45 53 66
+5  25 36 52 69
+14 30 33 54 63
+15 23 41 51 62
+""";
+
+    public override string Name => "That's a bingo";
+
+    protected override PuzzleResult Run()
+    {
+        
+    }
+}

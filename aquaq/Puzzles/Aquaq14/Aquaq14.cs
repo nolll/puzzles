9536fbaef221b52a1c2d56f9a6f217071ec28d8f
Henrik <henriks@gmail.com>
2023-10-01 16:59:34 +0200

    AquaQ Challenge 14 solved

--- aquaq/Puzzles/Aquaq14/Aquaq14.cs
+++ aquaq/Puzzles/Aquaq14/Aquaq14.cs
@@ -1,11 +1,14 @@
-﻿using Common.Puzzles;
+﻿using Common.CoordinateSystems.CoordinateSystem2D;
+using Common.Puzzles;
+using System.Linq;
 
 namespace Aquaq.Puzzles.Aquaq14;
 
 public class Aquaq14 : AquaqPuzzle
 {
-    private const string Board =
-"""
+    private const int BoardSize = 5;
+    private const string BoardInput =
+        """
 6  17 34 50 68
 10 21 45 53 66
 5  25 36 52 69
@@ -13,10 +16,84 @@ public class Aquaq14 : AquaqPuzzle
 15 23 41 51 62
 """;
 
+    private static readonly int[] Range = Enumerable.Range(0, BoardSize).ToArray();
+    private static readonly Dictionary<int, MatrixAddress> Board = BuildBoard();
+    private static readonly IEnumerable<MatrixAddress> Diagonal1 = BuildDiagonal1();
+    private static readonly IEnumerable<MatrixAddress> Diagonal2 = BuildDiagonal2();
+
     public override string Name => "That's a bingo";
 
     protected override PuzzleResult Run()
     {
-        
+        var result = PlayBingo(InputFile);
+        return new PuzzleResult(result, 4327);
     }
+
+    public static int PlayBingo(string input)
+    {
+        var games = input.Split(Environment.NewLine).Select(o => o.Split(' ').Select(int.Parse));
+
+        var totalSum = 0;
+        foreach (var numbers in games)
+        {
+            var numberCount = 0;
+            var game = new List<MatrixAddress>();
+            foreach (var number in numbers)
+            {
+                numberCount++;
+                if(Board.TryGetValue(number, out var address))
+                {
+                    game.Add(address);
+
+                    if (IsBingo(game))
+                        break;
+                }
+            }
+
+            totalSum += numberCount;
+        }
+
+        return totalSum;
+    }
+
+    private static Dictionary<int, MatrixAddress> BuildBoard()
+    {
+        var boardLines = BoardInput.Replace("  ", " ").Split(Environment.NewLine);
+        var board = new Dictionary<int, MatrixAddress>();
+
+        var y = 0;
+        foreach (var boardLine in boardLines)
+        {
+            var lineNumbers = boardLine.Split(' ').Select(int.Parse);
+            var x = 0;
+            foreach (var lineNumber in lineNumbers)
+            {
+                var address = new MatrixAddress(x, y);
+                board.Add(lineNumber, address);
+                x += 1;
+            }
+
+            y += 1;
+        }
+
+        return board;
+    }
+
+    private static bool IsBingo(IReadOnlyCollection<MatrixAddress> game) 
+        => IsHorizontalBingo(game) || IsVerticalBingo(game) || IsDiagonalBingo(game);
+
+    private static bool IsHorizontalBingo(IEnumerable<MatrixAddress> game)
+        => game.GroupBy(o => o.X).Any(o => o.Count() == 5);
+
+    private static bool IsVerticalBingo(IEnumerable<MatrixAddress> game)
+        => game.GroupBy(o => o.Y).Any(o => o.Count() == 5);
+
+    private static bool IsDiagonalBingo(IReadOnlyCollection<MatrixAddress> game) 
+        => Diagonal1.All(game.Contains) || Diagonal2.All(game.Contains);
+
+    private static IEnumerable<MatrixAddress> BuildDiagonal1() 
+        => Range.Select(o => new MatrixAddress(o, o));
+
+    private static IEnumerable<MatrixAddress> BuildDiagonal2()
+        => Range.Select(o => new MatrixAddress(BoardSize - o - 1, o));
 }
---
+++ aquaq/Puzzles/Aquaq14/Aquaq14Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq14;
+
+public class Aquaq14Tests
+{
+    [TestCase("10 5 21 45 53 70 66 4", 7)]
+    [TestCase("400 500 17 21 25 30 23 600 700", 7)]
+    [TestCase("400 500 6 21 36 54 62 600 700", 7)]
+    [TestCase("400 500 15 30 36 53 68 600 700", 7)]
+    public void Bingo(string input, int expected)
+    {
+        var result = Aquaq14.PlayBingo(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}

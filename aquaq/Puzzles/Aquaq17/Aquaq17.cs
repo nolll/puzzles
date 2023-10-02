02ec00a10bee160cb2c859f5c767bf5c1f4f0cb2
Henrik <henriks@gmail.com>
2023-10-02 23:30:30 +0200

    wip aquaq17

---
+++ aquaq/Puzzles/Aquaq17/Aquaq17.cs
@@ -0,0 +1,20 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq17;
+
+public class Aquaq17 : AquaqPuzzle
+{
+    public override string Name => "The Beautiful Shame";
+
+    protected override PuzzleResult Run()
+    {
+        return new PuzzleResult(Run(InputFile));
+    }
+
+    public static string Run(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+
+        return "";
+    }
+}
---
+++ aquaq/Puzzles/Aquaq17/Aquaq17Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq17;
+
+public class Aquaq17Tests
+{
+    private const string Input =
+"""
+date,home_team,away_team,home_score,away_score,tournament,city,country,neutral
+1900-01-01,Somaliland,Other1,1,0,_,_,_,_
+1900-01-01,Formosa,Other2,0,1,_,_,_,_
+1900-01-01,Ganoa,Other3,1,0,_,_,_,_
+1900-01-02,Genoa,Other4,0,1,_,_,_,_
+1900-01-03,Somaliland,Other5,0,1,_,_,_,_
+1900-01-03,Genoa,Other6,0,1,_,_,_,_
+1900-01-06,Genoa,Other7,0,1,_,_,_,_
+1901-01-21,Other8,Genoa,0,1,_,_,_,_
+1902-01-01,Other9,Somaliland,0,1,_,_,_,_
+""";
+
+    [Test]
+    public void FindShame()
+    {
+        var result = Aquaq17.Run(Input);
+
+        Assert.That(result, Is.EqualTo("Somaliland 19000103 19020101"));
+    }
+}

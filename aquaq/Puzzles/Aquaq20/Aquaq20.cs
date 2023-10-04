1a14c0e0924155ce86796b2b5c224440d6c698c4
Henrik <henriks@gmail.com>
2023-10-04 20:29:40 +0200

    AquaQ Challenge 20 solved

---
+++ aquaq/Puzzles/Aquaq20/Aquaq20.cs
@@ -0,0 +1,52 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq20;
+
+public class Aquaq20 : AquaqPuzzle
+{
+    public override string Name => "Blackjack";
+
+    protected override PuzzleResult Run()
+    {
+        return new PuzzleResult(PlayBlackjack(InputFile), 256);
+    }
+
+    public static int PlayBlackjack(string input)
+    {
+        var deck = input.Split(' ').ToArray();
+        var winCount = 0;
+
+        var currentGame = new List<int>();
+        foreach (var card in deck)
+        {
+            var value = GetCardValue(card);
+            currentGame.Add(value);
+            var aceCount = currentGame.Count(o => o == 11);
+            var maxSum = currentGame.Sum();
+            var sums = Enumerable.Range(0, aceCount + 1).Select(o => maxSum - 10 * o).ToList();
+            if (sums.Any(o => o == 21))
+            {
+                winCount++;
+                currentGame.Clear();
+                continue;
+            }
+
+            if (sums.Any(o => o < 21))
+                continue;
+
+            currentGame.Clear();
+        }
+
+        return winCount;
+    }
+
+    private static int GetCardValue(string card)
+    {
+        return card switch
+        {
+            "A" => 11,
+            "K" or "Q" or "J" or "10" => 10,
+            _ => int.Parse(card)
+        };
+    }
+}
---
+++ aquaq/Puzzles/Aquaq20/Aquaq20Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq20;
+
+public class Aquaq20Tests
+{
+    [TestCase("3 A K 9 A 7 4 9", 1)]
+    [TestCase("K Q 2 9 4 8 A A A K A 7", 2)]
+    public void PlayBlackjack(string deck, int expected)
+    {
+        var result = Aquaq20.PlayBlackjack(deck);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}

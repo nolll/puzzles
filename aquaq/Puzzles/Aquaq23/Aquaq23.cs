84eba598e42dd44d8e501fbd4132f3d5a072f513
Henrik <henriks@gmail.com>
2023-10-07 00:37:57 +0200

    generate cipher board

---
+++ aquaq/Puzzles/Aquaq23/Aquaq23.cs
@@ -0,0 +1,35 @@
+﻿using Common.CoordinateSystems.CoordinateSystem2D;
+using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq23;
+
+public class Aquaq23 : AquaqPuzzle
+{
+    private static string Alphabet => "abcdefghijklmnopqrstuvwxyz";
+    private static char[] _cipherAlphabet = Alphabet.ToCharArray().Where(o => o != 'j').ToArray();
+
+    public override string Name => "Fair Play";
+
+    protected override PuzzleResult Run()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Matrix<char> CreateCipherBoard(string keyword)
+    {
+        var distinct = keyword.ToCharArray().Distinct();
+        var otherChars = _cipherAlphabet.Where(o => !distinct.Contains(o) && o != 'j');
+        var combined = distinct.Concat(otherChars).ToArray();
+        var size = 5;
+        var matrix = new Matrix<char>(size, size);
+
+        for (var i = 0; i < combined.Length; i++)
+        {
+            var x = i % 5;
+            var y = i / 5;
+            matrix.WriteValueAt(x, y, combined[i]);
+        }
+
+        return matrix;
+    }
+}
---
+++ aquaq/Puzzles/Aquaq23/Aquaq23Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq23;
+
+public class Aquaq23Tests
+{
+    [Test]
+    public void CreateBoard()
+    {
+        var expected =
+"""
+playf
+irbcd
+eghkm
+noqst
+uvwxz
+""";
+
+        var result = Aquaq23.CreateCipherBoard("playfair");
+
+        Assert.That(result.Print().Trim(), Is.EqualTo(expected));
+    }
+}

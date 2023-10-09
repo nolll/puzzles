db5fd81f83da4efe2b7ff5b3b1035d9ee8d1c8e4
Henrik <henriks@gmail.com>
2023-10-10 00:37:42 +0200

    started working on aquaq26. Permutations are acting up

---
+++ aquaq/Puzzles/Aquaq26/Aquaq26.cs
@@ -0,0 +1,39 @@
+﻿using Common.Combinatorics;
+using Common.Puzzles;
+using NUnit.Framework.Constraints;
+
+namespace Aquaq.Puzzles.Aquaq26;
+
+public class Aquaq26 : AquaqPuzzle
+{
+    public override string Name => "Typo Theft";
+
+    protected override PuzzleResult Run()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static long FindFirstLargerNumber(long input)
+    {
+        var digits = input.ToString().ToCharArray().Select(o => int.Parse(o.ToString())).ToList();
+        var permutations = PermutationGenerator.GetPermutations(digits, digits.Count);
+        var minDiff = long.MaxValue;
+        var minDiffNum = input;
+
+        foreach (var permutation in permutations)
+        {
+            var num = int.Parse(string.Join("", permutation));
+            var diff = num - input;
+            if (diff > 0 && diff < minDiff)
+            {
+                minDiff = diff;
+                minDiffNum = num;
+            }
+        }
+
+        if (minDiff == long.MaxValue)
+            minDiffNum = input;
+
+        return minDiffNum;
+    }
+}
---
+++ aquaq/Puzzles/Aquaq26/Aquaq26Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq26;
+
+public class Aquaq26Tests
+{
+    [TestCase(1423, 1432)]
+    [TestCase(121, 211)]
+    [TestCase(10290, 10902)]
+    [TestCase(4321, 4321)]
+    public void FindFirstLargestNumber(long input, long expected)
+    {
+        var result = Aquaq26.FindFirstLargerNumber(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}

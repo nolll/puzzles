501c9e589d22ce27414a009deb2685abb71cfb60
Henrik <henriks@gmail.com>
2023-10-09 20:33:41 +0200

    AquaQ Challenge 25 solved

---
+++ aquaq/Puzzles/Aquaq25/Aquaq25.cs
@@ -0,0 +1,142 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq25;
+
+public class Aquaq25 : AquaqPuzzle
+{
+    private static readonly Dictionary<char, string> CharToMorse = new()
+    {
+        { 'a', ".-" },
+        { 'b', "-..." },
+        { 'c', "-.-." },
+        { 'd', "-.." },
+        { 'e', "." },
+        { 'f', "..-." },
+        { 'g', "--." },
+        { 'h', "...." },
+        { 'i', ".." },
+        { 'j', ".---" },
+        { 'k', "-.-" },
+        { 'l', ".-.." },
+        { 'm', "--" },
+        { 'n', "-." },
+        { 'o', "---" },
+        { 'p', ".--." },
+        { 'q', "--.-" },
+        { 'r', ".-." },
+        { 's', "..." },
+        { 't', "-" },
+        { 'u', "..-" },
+        { 'v', "...-" },
+        { 'w', ".--" },
+        { 'x', "-..-" },
+        { 'y', "-.--" },
+        { 'z', "--.." }
+    };
+
+    private static readonly Dictionary<string, char> MorseToChar = CharToMorse.ToDictionary(k => k.Value, v => v.Key);
+
+    public override string Name => "S'morse";
+
+    protected override PuzzleResult Run()
+    {
+        var morse = ClicksToMorse(InputFile);
+        var result = DecodeMorse(morse);
+        var resultParts = result.Split(' ');
+
+        var parsedResult = new[]
+        {
+            resultParts[7],
+            resultParts[32],
+            resultParts[25],
+            resultParts[36],
+            ((char)(resultParts[49].First() + 1)).ToString()
+        };
+
+        var joinedResult = string.Join("", parsedResult);
+
+        return new PuzzleResult(joinedResult, "paris");
+    }
+
+    public static string EncodeMorse(string input)
+    {
+        var words = input.Split(' ');
+        var encodedWords = words.Select(EncodeMorseWord);
+        return string.Join("   ", encodedWords);
+    }
+
+    private static string EncodeMorseWord(string input)
+    {
+        var encodedLetters = input.Select(o => CharToMorse[o]);
+        return string.Join(" ", encodedLetters);
+    }
+
+    public static string DecodeMorse(string input)
+    {
+        var words = input.Split("   ");
+        var decodedWords = words.Select(DecodeMorseWord);
+        return string.Join(" ", decodedWords);
+    }
+
+    private static string DecodeMorseWord(string input)
+    {
+        var decodedLetters = input.Split(' ').Select(GetChar);
+        return string.Join("", decodedLetters);
+    }
+
+    private static string GetChar(string morse)
+    {
+        if (MorseToChar.TryGetValue(morse, out var c))
+            return c.ToString();
+        return "";
+    }
+
+    public static string ClicksToMorse(string input)
+    {
+        var timeStamps = input.Split(Environment.NewLine)
+            .Select(o => o.Trim())
+            .Where(o => o.Length > 0)
+            .Select(o => DateTime.Parse($"2020-02-02 {o}"))
+            .ToArray();
+
+        return TimeStampsToMorse(timeStamps);
+    }
+
+    private static string TimeStampsToMorse(IReadOnlyList<DateTime> timeStamps)
+    {
+        var timeSpans = new List<TimeSpan>();
+        for (var i = 1; i < timeStamps.Count; i++)
+        {
+            timeSpans.Add(timeStamps[i] - timeStamps[i - 1]);
+        }
+
+        return TimeSpansToMorse(timeSpans);
+    }
+
+    private static string TimeSpansToMorse(IReadOnlyList<TimeSpan> timeSpans)
+    {
+        var interval = (int)timeSpans.Min().TotalMilliseconds;
+        var morse = "";
+        for (var i = 0; i < timeSpans.Count; i++)
+        {
+            var millis = (int)timeSpans[i].TotalMilliseconds;
+            if (i % 2 == 0)
+                morse += MillisToOnMorse(interval, millis);
+            else
+                morse += MillisToOffMorse(interval, millis);
+        }
+
+        return morse;
+    }
+
+    private static string MillisToOnMorse(int interval, int millis) => millis == interval ? "." : "-";
+
+    private static string MillisToOffMorse(int interval, int millis)
+    {
+        if (millis == interval)
+            return "";
+        if (millis == interval * 3)
+            return " ";
+        return "   ";
+    }
+}
---
+++ aquaq/Puzzles/Aquaq25/Aquaq25Tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq25;
+
+public class Aquaq25Tests
+{
+    [Test]
+    public void EncodeMorse()
+    {
+        var result = Aquaq25.EncodeMorse("jam donut");
+
+        Assert.That(result, Is.EqualTo(".--- .- --   -.. --- -. ..- -"));
+    }
+
+    [Test]
+    public void DecodeMorse()
+    {
+        var result = Aquaq25.DecodeMorse(".--- .- --   -.. --- -. ..- -");
+
+        Assert.That(result, Is.EqualTo("jam donut"));
+    }
+    
+    [Test]
+    public void ClicksToMorse()
+    {
+        const string input =
+"""
+21:46:5.000
+21:46:5.001
+21:46:5.002
+21:46:5.005
+21:46:5.006
+21:46:5.009
+21:46:5.010
+21:46:5.013
+21:46:5.016
+21:46:5.017
+21:46:5.018
+21:46:5.021
+           
+21:46:5.024
+21:46:5.027
+21:46:5.028
+21:46:5.031
+21:46:5.038
+21:46:5.041
+21:46:5.042
+21:46:5.043
+""";
+
+        var result = Aquaq25.ClicksToMorse(input);
+
+        Assert.That(result, Is.EqualTo(".--- .- --   -."));
+    }
+}

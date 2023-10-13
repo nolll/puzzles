b1d4286cc3f0c1e00b5aeb5049bb047bfde3be3e
Henrik <henriks@gmail.com>
2023-10-14 01:03:43 +0200

    wip

---
+++ aquaq/Puzzles/Aquaq30/Aquaq30.cs
@@ -0,0 +1,36 @@
+﻿using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq30;
+
+public class Aquaq30 : AquaqPuzzle
+{
+    public override string Name => "Flip Out";
+
+    protected override PuzzleResult Run()
+    {
+        var decks = InputFile.Split(Environment.NewLine);
+        var result = decks.Sum(CountValidStartingMoves);
+
+        return new PuzzleResult(result);
+    }
+
+    public static int CountValidStartingMoves(string input)
+    {
+        Console.WriteLine();
+        Console.WriteLine(input);
+        var count = 0;
+        var cardFlipper = new CardFlipper();
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '1')
+            {
+                var flipped = cardFlipper.Flip(input, i);
+                var canBeSolved = cardFlipper.CanBeSolved(flipped.ToCharArray());
+                if (canBeSolved)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ aquaq/Puzzles/Aquaq30/CardFlipper.cs
@@ -0,0 +1,88 @@
+﻿namespace Aquaq.Puzzles.Aquaq30;
+
+public class CardFlipper
+{
+    private Dictionary<char[], bool> _cache = new();
+
+    public string Flip(string s, int index)
+    {
+        var modified = "";
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i == index)
+            {
+                modified += '.';
+            }
+            else if (i == index - 1 || i == index + 1)
+            {
+                if (s[i] == '1')
+                    modified += '0';
+                else if (s[i] == '0')
+                    modified += '1';
+                else
+                    modified += '.';
+            }
+            else
+            {
+                modified += s[i];
+            }
+
+        }
+
+        return modified;
+    }
+
+    public bool CanBeSolved(char[] state)
+    {
+        if (_cache.TryGetValue(state, out var canBeSolved))
+            return canBeSolved;
+
+        var foundZero = false;
+
+        var subStates = new List<char[]>();
+        for (var i = 0; i < state.Length; i++)
+        {
+            var c = state[i];
+            if (c == '0')
+                foundZero = true;
+
+            if (c == '1')
+            {
+                var left = state.Take(i).ToArray();
+                if (left.Length > 0)
+                {
+                    var lastChar = left.Last();
+                    if (lastChar == '1')
+                        left[^1] = '0';
+                    else if (lastChar == '0')
+                        left[^1] = '1';
+                }
+                var right = state.Skip(i + 1).ToArray();
+                if (right.Length > 0)
+                {
+                    var firstChar = right.Last();
+                    if (firstChar == '1')
+                        right[0] = '0';
+                    else if (firstChar == '0')
+                        right[0] = '1';
+                }
+
+                if (left.Any())
+                    subStates.Add(left);
+
+                if (right.Any())
+                    subStates.Add(right);
+
+                if (subStates.All(CanBeSolved))
+                {
+                    _cache.Add(state, true);
+                    return true;
+                }
+            }
+        }
+
+        var result = !foundZero;
+        _cache.Add(state, result);
+        return !foundZero;
+    }
+}

34f34fe88a2c4b63793e57eb93abbb13c9b6524c
Henrik <henriks@gmail.com>
2023-10-16 01:39:01 +0200

    wip

---
+++ aquaq/Puzzles/Aquaq31/RubiksCube.cs
@@ -0,0 +1,92 @@
+﻿namespace Aquaq.Puzzles.Aquaq31;
+
+public class RubiksCube
+{
+    public RubiksCubeFace Front { get; } = CreateFace('b');
+    public RubiksCubeFace Up { get; } = CreateFace('w');
+    public RubiksCubeFace Left { get; } = CreateFace('r');
+    public RubiksCubeFace Right { get; } = CreateFace('o');
+    public RubiksCubeFace Down { get; } = CreateFace('y');
+    public RubiksCubeFace Back { get; } = CreateFace('g');
+
+    public void Rotate(string instruction)
+    {
+        if (instruction == "U")
+            RotateUp();
+        if (instruction == "U'")
+            RotateUpPrime();
+        if (instruction == "L")
+            RotateLeft();
+        if (instruction == "B")
+            RotateBack();
+        if (instruction == "R")
+            RotateRight();
+    }
+
+    private void RotateUp()
+    {
+        Up.RotateRight();
+        var fromFront = Front.ReadRow(0);
+        var fromLeft = Left.ReadRow(0);
+        var fromBack = Back.ReadRow(0);
+        var fromRight = Right.ReadRow(0);
+        Front.WriteRow(0, fromRight);
+        Left.WriteRow(0, fromFront);
+        Right.WriteRow(0, fromBack);
+        Back.WriteRow(0, fromLeft);
+    }
+
+    private void RotateUpPrime()
+    {
+        Up.RotateLeft();
+        var fromFront = Front.ReadRow(0);
+        var fromLeft = Left.ReadRow(0);
+        var fromBack = Back.ReadRow(0);
+        var fromRight = Right.ReadRow(0);
+        Front.WriteRow(0, fromLeft);
+        Left.WriteRow(0, fromBack);
+        Right.WriteRow(0, fromFront);
+        Back.WriteRow(0, fromRight);
+    }
+
+    private void RotateLeft()
+    {
+        Left.RotateRight();
+        var fromFront = Front.ReadColumn(0);
+        var fromUp = Up.ReadColumn(0);
+        var fromDown = Down.ReadColumn(0);
+        var fromBack = Back.ReadColumn(2);
+        Front.WriteColumn(0, fromUp);
+        Up.WriteReverseColumn(0, fromBack);
+        Down.WriteColumn(0, fromFront);
+        Back.WriteReverseColumn(2, fromDown);
+    }
+
+    private void RotateBack()
+    {
+        Back.RotateRight();
+        var fromUp = Up.ReadRow(0);
+        var fromLeft = Left.ReadColumn(0);
+        var fromDown = Down.ReadRow(2);
+        var fromRight = Right.ReadColumn(2);
+        Left.WriteReverseColumn(0, fromUp);
+        Down.WriteRow(2, fromLeft);
+        Right.WriteReverseColumn(2, fromDown);
+        Up.WriteRow(0, fromRight);
+    }
+
+    private void RotateRight()
+    {
+        Right.RotateRight();
+        var fromFront = Front.ReadColumn(2);
+        var fromUp = Up.ReadColumn(2);
+        var fromDown = Down.ReadColumn(2);
+        var fromBack = Back.ReadColumn(0);
+        Up.WriteColumn(2, fromFront);
+        Front.WriteColumn(2, fromDown);
+        Down.WriteReverseColumn(2, fromBack);
+        Back.WriteReverseColumn(0, fromUp);
+    }
+
+    private static RubiksCubeFace CreateFace(char initial) => new(initial);
+}
---
+++ aquaq/Puzzles/Aquaq31/RubiksCubeFace.cs
@@ -0,0 +1,95 @@
+﻿using Common.CoordinateSystems.CoordinateSystem2D;
+
+namespace Aquaq.Puzzles.Aquaq31;
+
+public class RubiksCubeFace
+{
+    private const int Size = 3;
+    private Matrix<char> _matrix;
+
+    public RubiksCubeFace(char initial)
+    {
+        _matrix = new Matrix<char>(Size, Size, initial);
+    }
+
+    public char[] ReadColumn(int x)
+    {
+        return Enumerable.Range(0, Size).Select(o => _matrix.ReadValueAt(x, o)).ToArray();
+    }
+
+    public char[] ReadReverseColumn(int y)
+    {
+        return ReadColumn(y).Reverse().ToArray();
+    }
+
+    public void WriteRow(int y, IEnumerable<char> values)
+    {
+        var x = 0;
+        foreach (var value in values)
+        {
+            _matrix.WriteValueAt(x, y, value);
+            x++;
+        }
+    }
+
+    public void WriteReverseRow(int y, IEnumerable<char> values)
+    {
+        WriteRow(y, values.Reverse());
+    }
+
+    public char[] ReadRow(int y)
+    {
+        return Enumerable.Range(0, Size).Select(o => _matrix.ReadValueAt(o, y)).ToArray();
+    }
+
+    public char[] ReadReverseRow(int x)
+    {
+        return ReadRow(x).Reverse().ToArray();
+    }
+
+    public void WriteColumn(int x, IEnumerable<char> values)
+    {
+        var y = 0;
+        foreach (var value in values)
+        {
+            _matrix.WriteValueAt(x, y, value);
+            y++;
+        }
+    }
+
+    public void WriteReverseColumn(int x, IEnumerable<char> values)
+    {
+        WriteColumn(x, values.Reverse());
+    }
+    
+    public void RotateRight()
+    {
+        _matrix = _matrix.RotateRight();
+    }
+
+    public void RotateLeft()
+    {
+        _matrix = _matrix.RotateLeft();
+    }
+
+    public string Print()
+    {
+        return string.Join("", _matrix.Values);
+    }
+
+    public int Product => _matrix.Values.Select(GetColorValue).Aggregate(1, (a, b) => a * b);
+
+    private int GetColorValue(char c)
+    {
+        return c switch
+        {
+            'b' => 1,
+            'w' => 2,
+            'r' => 3,
+            'o' => 4,
+            'y' => 5,
+            'g' => 6,
+            _ => throw new Exception("Unknown color")
+        };
+    }
+}

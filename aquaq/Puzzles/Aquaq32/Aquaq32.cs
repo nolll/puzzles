886705fe8827c2d9e65ffd574d002bd86c2026d0
Henrik <henriks@gmail.com>
2023-10-16 20:35:59 +0200

    AquaQ Challenge 32 solved

---
+++ aquaq/Puzzles/Aquaq32/Aquaq32.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using Common.Puzzles;
+
+namespace Aquaq.Puzzles.Aquaq32;
+
+public class Aquaq32 : AquaqPuzzle
+{
+    private static readonly Regex ClutterRegex = new("[^\\(\\)\\[\\]\\{\\}]");
+
+    public override string Name => "In Parenthesis";
+
+    protected override PuzzleResult Run()
+    {
+        var lines = InputFile.Split(Environment.NewLine);
+        var result = lines.Count(IsBalanced);
+
+        return new PuzzleResult(result, 616);
+    }
+
+    public static bool IsBalanced(string input)
+    {
+        var s = RemoveMatchingParenthesis(RemoveClutter(input));
+        return s.Length == 0;
+    }
+
+    private static string RemoveMatchingParenthesis(string input)
+    {
+        var s = input;
+        var lastLength = int.MaxValue;
+        while (s.Length < lastLength)
+        {
+            lastLength = s.Length;
+            s = s.Replace("()", "").Replace("{}", "").Replace("[]", "");
+        }
+
+        return s;
+    }
+
+    private static string RemoveClutter(string input) => ClutterRegex.Replace(input, "");
+}
---
+++ aquaq/Puzzles/Aquaq32/Aquaq32Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Aquaq.Puzzles.Aquaq32;
+
+public class Aquaq32Tests
+{
+    [TestCase("()", true)]
+    [TestCase("([]{})", true)]
+    [TestCase("(a[b[]]c){}", true)]
+    [TestCase(")()", false)]
+    [TestCase("([a)]", false)]
+    [TestCase("]{}[", false)]
+    [TestCase("((a)){]", false)]
+    public void IsBalanced(string input, bool expected)
+    {
+        var result = Aquaq32.IsBalanced(input);
+
+        result.Should().Be(expected);
+    }
+}

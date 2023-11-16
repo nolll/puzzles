09b9993406193f611d14bf7427c733342891abcb
Henrik Severinson <henriks@gmail.com>
2023-11-17 00:30:45 +0100

    AquaQ Challenge 37 solved

---
+++ aquaq/Puzzles/Aquaq37/Aquaq37.cs
@@ -0,0 +1,117 @@
+﻿using Puzzles.common.Puzzles;
+
+namespace Puzzles.aquaq.Puzzles.Aquaq37;
+
+public class Aquaq37 : AquaqPuzzle
+{
+    public override string Name => "GUESS WORDS";
+
+    private const int WordLength = 5;
+
+    protected override PuzzleResult Run()
+    {
+        var words = FindWords(InputFile);
+        var score = words.Sum(GetWordScore);
+
+        return new PuzzleResult(score, "ba0ef798d7f57b80a0675236159ccfb1");
+    }
+
+    public List<string> FindWords(string input)
+    {
+        var words = CommonTextFile("Words.txt")
+            .Split(Environment.NewLine)
+            .Where(o => o.Length == WordLength)
+            .ToList();
+
+        var guesses = input
+            .Split(Environment.NewLine)
+            .Skip(1)
+            .Select(ParseGuess)
+            .ToList();
+
+        var foundWords = new List<string>();
+        var used = new HashSet<string>();
+        var remainingGuesses = guesses.ToList();
+        var matchingWords = words.ToList();
+        while (remainingGuesses.Any())
+        {
+            var guess = remainingGuesses.First();
+            remainingGuesses = remainingGuesses.Skip(1).ToList();
+            used.Add(guess.Word);
+
+            matchingWords = matchingWords
+                .Where(o => !used.Contains(o))
+                .Where(o => guess.IsMatch(o))
+                .ToList();
+
+            if (matchingWords.Count > 1)
+                continue;
+
+            var foundWord = matchingWords.First();
+            foundWords.Add(foundWord);
+            used.Clear();
+            matchingWords = words.ToList();
+        }
+
+        return foundWords;
+    }
+
+    private static Guess ParseGuess(string s)
+    {
+        var parts = s.Split(',');
+        var word = parts[0];
+        var results = parts[1].Split(' ').Select(int.Parse).ToArray();
+
+        return new Guess(word, results);
+    }
+
+    public static int GetWordScore(string word) => word.Select(o => o - 'a').Sum();
+
+    public class Guess
+    {
+        public string Word { get; }
+        private readonly int[] _result;
+
+        public Guess(string word, int[] result)
+        {
+            Word = word;
+            _result = result;
+        }
+
+        public bool IsMatch(string other)
+        {
+            for (var i = 0; i < WordLength; i++)
+            {
+                var result = _result[i];
+                var wordChar = Word[i];
+                var otherChar = other[i];
+                switch (result)
+                {
+                    case 2 when wordChar != otherChar:
+                    case 0 or 1 when wordChar == otherChar:
+                        return false;
+                }
+            }
+
+            var possibleChars = new List<char>();
+            var blockedChars = new List<char>();
+            var remainingChars = new List<char>();
+            for (var i = 0; i < WordLength; i++)
+            {
+                var result = _result[i];
+                var wordChar = Word[i];
+                var otherChar = other[i];
+                if (result is 0 or 1)
+                    remainingChars.Add(otherChar);
+
+                if (result == 1)
+                    possibleChars.Add(wordChar);
+
+                if (result == 0)
+                    blockedChars.Add(wordChar);
+            }
+
+            return possibleChars.All(remainingChars.Remove) && !blockedChars.Any(remainingChars.Contains);
+        }
+    }
+}
---
+++ aquaq/Puzzles/Aquaq37/Aquaq37Tests.cs
@@ -0,0 +1,113 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Puzzles.aquaq.Puzzles.Aquaq37;
+
+public class Aquaq37Tests
+{
+    private const string WordsInput = """
+guess,result
+guess,0 0 0 0 2
+twins,0 1 0 0 2
+bowls,0 2 1 0 2
+worms,2 2 2 0 2
+works,2 2 2 0 2
+""";
+
+    private const string MajorInput = """
+guess,result
+papal,0 2 0 0 0
+cacti,0 2 0 0 0
+bauds,0 2 0 0 0
+ragee,1 2 0 0 0
+marry,2 2 1 0 0
+manor,2 2 0 2 2
+""";
+
+    private const string StoryInput = """
+guess,result
+yills,1 0 0 0 1
+mousy,0 1 0 1 2
+savoy,2 0 0 1 2
+showy,2 0 2 0 2
+stony,2 2 2 0 2
+stogy,2 2 2 0 2
+""";
+
+    private const string BeganInput = """
+guess,result
+jotas,0 0 0 2 0
+redan,0 2 0 2 2
+leman,0 2 0 2 2
+vegan,0 2 2 2 2
+""";
+
+    private const string ThereInput = """
+guess,result
+buxom,0 0 0 0 0
+three,2 2 1 1 2
+""";
+    
+    private const string PriorInput = """
+guess,result
+short,0 0 1 1 0
+oiler,1 1 0 0 2
+""";
+
+    private const string UnderInput = """
+guess,result
+scend,0 0 1 1 1
+bendy,0 1 1 1 0
+endue,1 2 2 1 0
+""";
+
+    private const string GroupInput = """
+guess,result
+tabes,0 0 0 0 0
+hying,0 0 0 0 1
+doggo,0 1 1 0 0
+growl,2 2 2 0 0
+""";
+
+    [TestCase(WordsInput, "words")]
+    [TestCase(MajorInput, "major")]
+    [TestCase(StoryInput, "story")]
+    [TestCase(BeganInput, "began")]
+    [TestCase(ThereInput, "there")]
+    [TestCase(PriorInput, "prior")]
+    [TestCase(UnderInput, "under")]
+    [TestCase(GroupInput, "group")]
+    public void FindWords(string input, string expected)
+    {
+        var result = new Aquaq37().FindWords(input);
+
+        result.FirstOrDefault().Should().Be(expected);
+    }
+
+    [Test]
+    public void MajorMarry()
+    {
+        var guess = new Aquaq37.Guess("marry", new[] { 2, 2, 1, 0, 0 });
+        var result = guess.IsMatch("major");
+
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public void GroomDoggo()
+    {
+        var guess = new Aquaq37.Guess("doggo", new[] { 0, 1, 1, 0, 0 });
+        var result = guess.IsMatch("groom");
+
+        result.Should().BeFalse();
+    }
+
+    [TestCase("a", 0)]
+    [TestCase("wordsmince", 113)]
+    [TestCase("words", 74)]
+    [TestCase("mince", 39)]
+    public void WordScore(string input, int expected)
+    {
+        Aquaq37.GetWordScore(input).Should().Be(expected);
+    }
+}

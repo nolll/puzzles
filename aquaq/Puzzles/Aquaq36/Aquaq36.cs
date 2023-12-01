62a93132b4eacd97bb015a467fd5527e1e71eedb
Henrik <henriks@gmail.com>
2023-12-01 07:31:45 +0100

    AquaQ Challenge 36 solved

--- aquaq/Puzzles/Aquaq36/Aquaq36.cs
+++ aquaq/Puzzles/Aquaq36/Aquaq36.cs
@@ -1,27 +1,10 @@
-﻿using System.Diagnostics;
-using Puzzles.common.Combinatorics;
-using Puzzles.common.Maths;
+﻿using Puzzles.common.Maths;
 using Puzzles.common.Puzzles;
-using static Puzzles.aquaq.Puzzles.Aquaq36.Aquaq36;
 
 namespace Puzzles.aquaq.Puzzles.Aquaq36;
 
 public class Aquaq36 : AquaqPuzzle
 {
-/*
-Part 1: 30.332s211
-Part 1: 57.818s212
-Part 1: 58.071s260
-Part 1: 60.270s139
-Part 1: 60.686s82
-Part 1: 61.389s95
-Part 1: 74.507s178
-Part 1: 97.802s46
-Part 1: 126.687s55
-Part 1: 238.005s123
-Part 1: 407.183s60
-*/
-
     public override string Name => "Tetonor Terror";
 
     protected override PuzzleResult Run()
@@ -34,67 +17,78 @@ Part 1: 407.183s60
         {
             var rows = tetonor.Split(Environment.NewLine);
             var grid = rows[0][2..].Split(' ').Select(int.Parse).ToList();
-            var input = rows[1][2..].Split(' ').Where(o => o != "*").Select(int.Parse).ToList();
+            var input = rows[1][2..].Split(' ').Select(o => (int?)(o == "*" ? null : int.Parse(o))).ToList();
             var result = Solve(grid, input, factorProvider);
+
             sum += result;
         }
 
         return new PuzzleResult(sum);
     }
 
-    public static int Solve(List<int> grid, List<int> input, FactorCache? factorCache = null)
+    public static int Solve(List<int> grid, List<int?> input, FactorCache? factorCache = null)
     {
-        var possibleInputs = GetPossibleInputs(grid, input);
+        var concreteFactorCache = factorCache ?? new FactorCache();
+        var sortedGrid = grid.OrderDescending().ToList();
+        var factorSet = GetAllFactors(sortedGrid).Distinct().ToHashSet();
+        var inputs = FindPossibleInputNumbers(input, sortedGrid.Max(), factorSet);
 
-        foreach (var possibleInput in possibleInputs)
-        {
-            var result = Solve(grid, possibleInput, factorCache ?? new FactorCache(), 0);
-            if (result > 0)
-                return result;
-        }
+        var result = Solve(sortedGrid, inputs, concreteFactorCache, 0);
+        if (result > 0)
+            return result;
 
         return 0;
     }
 
-    private static List<List<int>> GetPossibleInputs(List<int> grid, List<int> input)
+    private static List<GridPair> GetGridPairs(int n, List<int> grid, FactorCache factorCache)
     {
-        var missingCount = grid.Count - input.Count;
-        var factors = GetAllFactors(grid);
-        var factorCombinations = CombinationGenerator.GetCombinationsFixedSize(factors, missingCount);
-        var possibleInputs = new List<List<int>>();
-        foreach (var combination in factorCombinations)
+        var list = new List<GridPair>();
+        var gridNumbers = factorCache.Get(n);
+        foreach (var gridNumber in gridNumbers)
         {
-            possibleInputs.Add(combination.Concat(input).ToList());
+            var sum = gridNumber.A + gridNumber.B;
+            if (grid.Contains(sum))
+            {
+                var a = Math.Min(gridNumber.A, gridNumber.B);
+                var b = Math.Max(gridNumber.A, gridNumber.B);
+                list.Add(new GridPair(n, sum, a, b));
+            }
         }
 
-        return possibleInputs;
+        return list;
     }
 
-    private static int Solve(List<int> grid, List<int> input, FactorCache factorCache, int sum)
+    private static int Solve(List<int> grid, List<HashSet<int>> inputs, FactorCache factorCache, int sum)
     {
         if (!grid.Any())
             return sum;
 
+        //var seen = new HashSet<GridPair>();
         foreach (var gridNumber in grid)
         {
-            var factorList = factorCache.Get(gridNumber);
-            foreach (var factors in factorList)
+            var pairs = GetGridPairs(gridNumber, grid, factorCache);
+            foreach (var pair in pairs)
             {
-                if (input.Contains(factors.a) && input.Contains(factors.b))
+                //seen.Add(pair);
+                var aIndices = IndicesContaining(inputs, pair.A);
+                var bIndices = IndicesContaining(inputs, pair.B);
+
+                foreach (var a in aIndices)
                 {
-                    var p = factors.a * factors.b;
-                    var s = factors.a + factors.b;
-                    if (grid.Contains(p) && grid.Contains(s))
+                    foreach (var b in bIndices)
                     {
-                        var newGrid = grid.ToList();
-                        var newInput = input.ToList();
-                        newGrid.Remove(p);
-                        newGrid.Remove(s);
-                        newInput.Remove(factors.a);
-                        newInput.Remove(factors.b);
-                        var result = Solve(newGrid, newInput, factorCache, sum + factors.b - factors.a);
-                        if (result > 0)
-                            return result;
+                        if (a != b)
+                        {
+                            var newGrid = grid.ToList();
+                            var newInput = inputs.ToList();
+                            newGrid.Remove(pair.Product);
+                            newGrid.Remove(pair.Sum);
+                            newInput.RemoveAt(Math.Max(a, b));
+                            newInput.RemoveAt(Math.Min(a, b));
+                            var result = Solve(newGrid, newInput, factorCache, sum + Math.Abs(pair.B - pair.A));
+                            if (result > 0)
+                                return result;
+                        }
                     }
                 }
             }
@@ -103,18 +97,29 @@ Part 1: 407.183s60
         return 0;
     }
 
+    public static IEnumerable<int> IndicesContaining(List<HashSet<int>> inputs, int n)
+    {
+        for (var i = 0; i < inputs.Count; i++)
+        {
+            if (inputs[i].Contains(n))
+                yield return i;
+        }
+    }
+
     public class FactorCache
     {
-        private readonly Dictionary<int, List<(int, int)>> _cache = new();
+        private readonly Dictionary<int, List<GridNumber>> _gridNumberCache = new();
+        private readonly Dictionary<int, List<int>> _factorCache = new();
 
-        public List<(int a, int b)> Get(int n)
+        public List<GridNumber> Get(int n)
         {
-            if (_cache.TryGetValue(n, out var factors))
-                return factors;
+            if (_gridNumberCache.TryGetValue(n, out var gridNumbers))
+                return gridNumbers;
 
-            factors = MathTools.GetMultiplicationFactors(n);
-            _cache.Add(n, factors);
-            return factors;
+            var factors = MathTools.GetMultiplicationFactors(n);
+            gridNumbers = factors.Select(o => new GridNumber(n, o.a, o.b)).ToList();
+            _gridNumberCache.Add(n, gridNumbers);
+            return gridNumbers;
         }
     }
 
@@ -130,12 +135,13 @@ Part 1: 407.183s60
         return list.Order().ToList();
     }
 
-    public static Dictionary<int, List<int>> FindPossibleInputNumbers(List<int?> input, int max, HashSet<int>? factors = null)
+    public static List<HashSet<int>> FindPossibleInputNumbers(List<int?> input, int max, HashSet<int>? factors = null)
     {
-        var d = new Dictionary<int, List<int>>();
+        var list = new List<HashSet<int>>();
         for (var i = 0; i < input.Count; i++)
         {
-            if (input[i] is null)
+            var v = input[i];
+            if (v is null)
             {
                 var lowerBound = FindLowerBound(input, i);
                 var upperBound = FindUpperBound(input, i, max);
@@ -143,16 +149,20 @@ Part 1: 407.183s60
                 var numbers = Enumerable.Range(lowerBound, rangeCount);
                 if (factors != null)
                     numbers = numbers.Where(factors.Contains);
-                d.Add(i, numbers.ToList());
+                list.Add(numbers.ToHashSet());
+            }
+            else
+            {
+                list.Add(new() { v.Value });
             }
         }
 
-        return d;
+        return list;
     }
 
     private static int FindLowerBound(List<int?> input, int index)
     {
-        for (var j = index - 1; j >= 0; j++)
+        for (var j = index - 1; j >= 0; j--)
         {
             var v = input[j];
             if (v is not null)
@@ -173,4 +183,7 @@ Part 1: 407.183s60
 
         return max;
     }
+
+    public record GridPair(int Product, int Sum, int A, int B);
+    public record GridNumber(int Product, int A, int B);
 }
--- aquaq/Puzzles/Aquaq36/Aquaq36Tests.cs
+++ aquaq/Puzzles/Aquaq36/Aquaq36Tests.cs
@@ -12,7 +12,7 @@ public class Aquaq36Tests
             .Split().Select(int.Parse).ToList();
 
         var inputNumbers = "1 2 2 5 6 6 8 10 14 16 21 23 24 26 28 42"
-            .Split().Select(int.Parse).ToList();
+            .Split().Select(int.Parse).Cast<int?>().ToList();
 
         var result = Aquaq36.Solve(gridNumbers, inputNumbers);
 
@@ -31,27 +31,9 @@ public class Aquaq36Tests
         
         result.Should().BeEquivalentTo(expected);
     }
-
+    
     [Test]
     public void PossibleInputNumbers()
-    {
-        var input = "* 2 3 * 4 5 6 * 7 7 * 10 10 13 * 15"
-            .Split().Select(o => (int?)(o == "*" ? null : int.Parse(o))).ToList();
-
-        var result = Aquaq36.FindPossibleInputNumbers(input, 70);
-
-        result.Should().BeEquivalentTo(new Dictionary<int, List<int>>
-        {
-            { 0, new List<int>{ 1, 2 }},
-            { 3, new List<int>{ 3, 4 }},
-            { 7, new List<int>{ 6, 7 }},
-            { 10, new List<int>{ 7, 8, 9, 10 }},
-            { 14, new List<int>{ 13, 14, 15 }},
-        });
-    }
-
-    [Test]
-    public void PossibleInputNumbersRestrictedToFactors()
     {
         var input = "* 2 3 * 4 5 6 * 7 7 * 10 10 13 * 15"
             .Split().Select(o => (int?)(o == "*" ? null : int.Parse(o))).ToList();
@@ -60,13 +42,69 @@ public class Aquaq36Tests
 
         var result = Aquaq36.FindPossibleInputNumbers(input, 70, factors);
 
-        result.Should().BeEquivalentTo(new Dictionary<int, List<int>>
+        result.Should().BeEquivalentTo(new List<List<int>>
         {
-            { 0, new List<int>{ 2 }},
-            { 3, new List<int>{ 4 }},
-            { 7, new List<int>{ 7 }},
-            { 10, new List<int>{ 7, 9 }},
-            { 14, new List<int>{ 14, 15 }},
+            new() { 2 },
+            new() { 2 },
+            new() { 3 },
+            new() { 4 },
+            new() { 4 },
+            new() { 5 },
+            new() { 6 },
+            new() { 7 },
+            new() { 7 },
+            new() { 7 },
+            new() { 7, 9 },
+            new() { 10 },
+            new() { 10 },
+            new() { 13 },
+            new() { 14, 15 },
+            new() { 15 },
         });
     }
+
+    //[Test]
+    //public void PossibleInputNumbersCombinationsRestrictedToFactors()
+    //{
+    //    var input = "* 2 3 * 4 5 6 * 7 7 * 10 10 13 * 15"
+    //        .Split().Select(o => (int?)(o == "*" ? null : int.Parse(o))).ToList();
+
+    //    var factors = new HashSet<int> { 2, 4, 7, 9, 14, 15 };
+
+    //    var positionPossibilities = Aquaq36.FindPossibleInputNumbers2(input, 70, factors);
+    //    var maxCounts = new Dictionary<int, int>();
+    //    var result = Aquaq36.FindInputNumberCombinations(positionPossibilities, maxCounts);
+
+    //    result.Should().BeEquivalentTo(new List<List<int>>
+    //    {
+    //        new() { 2, 2, 3, 4, 4, 5, 6, 7, 7, 7, 7, 10, 10, 13, 14, 15 },
+    //        new() { 2, 2, 3, 4, 4, 5, 6, 7, 7, 7, 7, 10, 10, 13, 15, 15 },
+    //        new() { 2, 2, 3, 4, 4, 5, 6, 7, 7, 7, 9, 10, 10, 13, 14, 15 },
+    //        new() { 2, 2, 3, 4, 4, 5, 6, 7, 7, 7, 9, 10, 10, 13, 15, 15 },
+    //    });
+    //}
+
+    //[Test]
+    //public void PossibleFullInputNumbersRestrictedToFactors()
+    //{
+    //    var input = "* 2 3 * 4 5 6 * 7 7 * 10 10 13 * 15"
+    //        .Split().Select(o => (int?)(o == "*" ? null : int.Parse(o))).ToList();
+
+    //    var factors = new HashSet<int> { 2, 4, 7, 9, 14, 15 };
+
+    //    var result = Aquaq36.FindPossibleInputNumbers(input, 70, factors);
+
+    //    result.Should().BeEquivalentTo(new Dictionary<int, List<int>>
+    //    {
+    //        { 0, new List<int>{ 2 }},
+    //        { 3, new List<int>{ 4 }},
+    //        { 7, new List<int>{ 7 }},
+    //        { 10, new List<int>{ 7, 9 }},
+    //        { 14, new List<int>{ 14, 15 }},
+    //    });
+
+    //    var unknownIndices = result.Keys.ToList();
+    //    var unknownValues = result.Values.ToList();
+    //    var result2 = 
+    //}
 }

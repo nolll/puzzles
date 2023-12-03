2fd4168ca3156d6331f8c329872475feb439148a
Henrik <henriks@gmail.com>
2023-12-03 12:46:16 +0100

    Aoc day 3 2023 solved

---
+++ aoc/Puzzles/Aoc2023/Aoc202303/Aoc202303.cs
@@ -0,0 +1,168 @@
+﻿using Puzzles.common.CoordinateSystems.CoordinateSystem2D;
+using Puzzles.common.Puzzles;
+
+namespace Puzzles.aoc.Puzzles.Aoc2023.Aoc202303;
+
+public class Aoc202303 : AocPuzzle
+{
+    public override string Name => "Gear Ratios";
+
+    protected override PuzzleResult RunPart1()
+    {
+        var result = Run(InputFile);
+
+        return new PuzzleResult(result.EngineParts, "9f5f9a7fa049bd552fc05c71b10aab1c");
+    }
+
+    protected override PuzzleResult RunPart2()
+    {
+        var result = Run(InputFile);
+
+        return new PuzzleResult(result.GearRatios, "0ec5347a4e1a8f41769180000882ae7d");
+    }
+
+    public static Result Run(string input)
+    {
+        var lines = input.Split(Environment.NewLine).ToList();
+        var width = lines.First().Length;
+        var height = lines.Count;
+        var numberCoordList = FindNumberCoords(lines);
+        var symbolMatrix = BuildSymbolMatrix(lines, width, height);
+        var numberMatrix = BuildNumberMatrix(numberCoordList, width, height);
+        var engineParts = FindEngineParts(numberCoordList, symbolMatrix);
+        var gearRatios = FindGearRatios(numberMatrix, symbolMatrix);
+
+        return new Result(engineParts.Sum(), gearRatios.Sum());
+    }
+
+    private static List<NumberCoord> FindNumberCoords(IEnumerable<string> lines)
+    {
+        var numberCoordList = new List<NumberCoord>();
+
+        var row = 0;
+        foreach (var line in lines)
+        {
+            var items = RemoveSymbols(line).Split('.');
+
+            var index = 0;
+            foreach (var item in items)
+            {
+                var numberLength = item.Length;
+
+                if (int.TryParse(item, out var number))
+                {
+                    var coords = new List<MatrixAddress>();
+                    for (var i = 0; i < numberLength; i++)
+                    {
+                        var x = index + i;
+                        coords.Add(new MatrixAddress(x, row));
+                    }
+
+                    numberCoordList.Add(new NumberCoord(number, coords));
+                    index += numberLength;
+                }
+                index++;
+            }
+
+            row++;
+        }
+
+        return numberCoordList;
+    }
+
+    private static string RemoveSymbols(string line)
+    {
+        var cleanedLine = line.ToCharArray();
+        for (var i = 0; i < cleanedLine.Length; i++)
+        {
+            var c = cleanedLine[i];
+            if (!char.IsDigit(c))
+                cleanedLine[i] = '.';
+        }
+
+        return string.Join("", cleanedLine);
+    }
+
+    private static Matrix<char> BuildSymbolMatrix(IEnumerable<string> lines, int width, int height)
+    {
+        var symbolMatrix = new Matrix<char>(width, height);
+        var row = 0;
+        foreach (var line in lines)
+        {
+            var chars = line.ToCharArray();
+            for (var i = 0; i < chars.Length; i++)
+            {
+                var c = chars[i];
+                var isDigit = char.IsDigit(c);
+                var isSymbol = !isDigit && c != '.';
+                var symbol = isSymbol ? c : '.';
+                symbolMatrix.WriteValueAt(i, row, symbol);
+            }
+
+            row++;
+        }
+
+        return symbolMatrix;
+    }
+
+    private static Matrix<int> BuildNumberMatrix(List<NumberCoord> numberCoordList, int width, int height)
+    {
+        var numberMatrix = new Matrix<int>(width, height);
+
+        foreach (var numberCoord in numberCoordList)
+        {
+            foreach (var coord in numberCoord.Coords)
+            {
+                numberMatrix.WriteValueAt(coord, numberCoord.Number);
+            }
+        }
+
+        return numberMatrix;
+    }
+
+    private static List<int> FindEngineParts(List<NumberCoord> numberCoordList, Matrix<char> symbolMatrix)
+    {
+        var engineParts = new List<int>();
+        foreach (var numberCoord in numberCoordList)
+        {
+            var hasAdjacentSymbol = false;
+            foreach (var coord in numberCoord.Coords)
+            {
+                var adjacent = symbolMatrix.AllAdjacentValuesTo(coord);
+                if (adjacent.Any(o => o != '.'))
+                    hasAdjacentSymbol = true;
+            }
+
+            if (hasAdjacentSymbol)
+                engineParts.Add(numberCoord.Number);
+        }
+
+        return engineParts;
+    }
+
+    private static List<int> FindGearRatios(Matrix<int> numberMatrix, Matrix<char> symbolMatrix)
+    {
+        var symbolCoords = symbolMatrix.Coords;
+        var gearRatios = new List<int>();
+        foreach (var symbolCoord in symbolCoords)
+        {
+            if (symbolMatrix.ReadValueAt(symbolCoord) != '*')
+                continue;
+
+            var adjacentValues = numberMatrix.AllAdjacentValuesTo(symbolCoord)
+                .Where(o => o > 0)
+                .Distinct()
+                .ToList();
+
+            if (adjacentValues.Count == 2)
+            {
+                gearRatios.Add(adjacentValues.First() * adjacentValues.Last());
+            }
+        }
+
+        return gearRatios;
+    }
+
+    private record NumberCoord(int Number, List<MatrixAddress> Coords);
+    public record Result(int EngineParts, int GearRatios);
+}
---
+++ aoc/Puzzles/Aoc2023/Aoc202303/Aoc202303Tests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Puzzles.aoc.Puzzles.Aoc2023.Aoc202303;
+
+public class Aoc202303Tests
+{
+    private const string Input = """
+                                 467..114..
+                                 ...*......
+                                 ..35..633.
+                                 ......#...
+                                 617*......
+                                 .....+.58.
+                                 ..592.....
+                                 ......755.
+                                 ...$.*....
+                                 .664.598..
+                                 """;
+
+    [Test]
+    public void EngineParts()
+    {
+        var result = Aoc202303.Run(Input);
+
+        result.EngineParts.Should().Be(4361);
+    }
+
+    [Test]
+    public void GearRatios()
+    {
+        var result = Aoc202303.Run(Input);
+
+        result.GearRatios.Should().Be(467835);
+    }
+}

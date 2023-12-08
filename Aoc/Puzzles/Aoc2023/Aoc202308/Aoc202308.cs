79cc74fef3d7ac96979a8b045c0cc5f5ac3e1d4e
Henrik <henriks@gmail.com>
2023-12-08 11:42:32 +0100

    Aoc day 8 2023 solved

---
+++ Aoc/Puzzles/Aoc2023/Aoc202308/Aoc202308.cs
@@ -0,0 +1,80 @@
+﻿using Pzl.Common;
+using Pzl.Tools.Maths;
+using Pzl.Tools.Strings;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202308;
+
+public class Aoc202308 : AocPuzzle
+{
+    public override string Name => "Haunted Wasteland";
+
+    protected override PuzzleResult RunPart1()
+    {
+        return new PuzzleResult(DesertPath1(InputFile), "fc2f3ff0b243fed57135f9ca59507411");
+    }
+
+    protected override PuzzleResult RunPart2()
+    {
+        return new PuzzleResult(DesertPath2(InputFile), "ea69e01b93e2a5ffbd263edd44ecc7e8");
+    }
+
+    public static long DesertPath1(string input)
+    {
+        var (directions, connections) = ParseInput(input);
+
+        bool CompareFunc(string s) => s != "ZZZ";
+        return GetStepCount(connections, directions, CompareFunc, "AAA");
+    }
+
+    public static long DesertPath2(string input)
+    {
+        var (directions, connections) = ParseInput(input);
+
+        var startPositions = connections.Keys.Where(o => o.EndsWith('A')).ToList();
+
+        bool CompareFunc(string s) => !s.EndsWith('Z');
+        var pathLengths = startPositions.Select(
+            startPosition => GetStepCount(connections, directions, CompareFunc, startPosition)).ToList();
+
+        return MathTools.Lcm(pathLengths);
+    }
+
+    private static long GetStepCount(
+        IReadOnlyDictionary<string, (string Left, string Right)> connections,
+        char[] directions,
+        Func<string, bool> compareFunc,
+        string startPos)
+    {
+        var stepCount = 0L;
+        var pos = startPos;
+        while (compareFunc(pos))
+        {
+            pos = directions[stepCount % directions.Length] == 'L' 
+                ? connections[pos].Left 
+                : connections[pos].Right;
+
+            stepCount++;
+        }
+
+        return stepCount;
+    }
+
+    private static (char[] Directions, Dictionary<string, (string Left, string Right)> Connections)
+        ParseInput(string input)
+    {
+        var connections = new Dictionary<string, (string Left, string Right)>();
+
+        var groups = StringReader.ReadLineGroups(input);
+        var directions = groups.First().First().ToCharArray();
+
+        foreach (var line in groups.Last())
+        {
+            var parts = line.Split(" = ");
+            var key = parts.First();
+            var destinations = parts.Last().TrimStart('(').TrimEnd(')').Split(", ").ToList();
+            connections.Add(key, (destinations.First(), destinations.Last()));
+        }
+
+        return (directions, connections);
+    }
+}
---
+++ Aoc/Puzzles/Aoc2023/Aoc202308/Aoc202308Tests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202308;
+
+public class Aoc202308Tests
+{
+    [Test]
+    public void DesertPart1Example1()
+    {
+        const string input ="""
+                            RL
+                            
+                            AAA = (BBB, CCC)
+                            BBB = (DDD, EEE)
+                            CCC = (ZZZ, GGG)
+                            DDD = (DDD, DDD)
+                            EEE = (EEE, EEE)
+                            GGG = (GGG, GGG)
+                            ZZZ = (ZZZ, ZZZ)
+                            """;
+
+        var result = Aoc202308.DesertPath1(input);
+
+        result.Should().Be(2);
+    }
+
+    [Test]
+    public void DesertPart1Example2()
+    {
+        const string input = """
+                             LLR
+                             
+                             AAA = (BBB, BBB)
+                             BBB = (AAA, ZZZ)
+                             ZZZ = (ZZZ, ZZZ)
+                             """;
+
+        var result = Aoc202308.DesertPath1(input);
+
+        result.Should().Be(6);
+    }
+
+    [Test]
+    public void DesertPart2()
+    {
+        const string input = """
+                             LR
+                             
+                             11A = (11B, XXX)
+                             11B = (XXX, 11Z)
+                             11Z = (11B, XXX)
+                             22A = (22B, XXX)
+                             22B = (22C, 22C)
+                             22C = (22Z, 22Z)
+                             22Z = (22B, 22B)
+                             XXX = (XXX, XXX)
+                             """;
+
+        var result = Aoc202308.DesertPath2(input);
+
+        result.Should().Be(6);
+    }
+}

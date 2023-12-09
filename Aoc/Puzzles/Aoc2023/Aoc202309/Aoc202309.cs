89bffcee9bbce459f7400494efe11762fad545e3
Henrik <henriks@gmail.com>
2023-12-09 11:08:54 +0100

    Aoc day 9 2023 solved

---
+++ Aoc/Puzzles/Aoc2023/Aoc202309/Aoc202309.cs
@@ -0,0 +1,79 @@
+﻿using System.Globalization;
+using Pzl.Common;
+using Pzl.Tools.Strings;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202309;
+
+public class Aoc202309 : AocPuzzle
+{
+    public override string Name => "Mirage Maintenance";
+
+    protected override PuzzleResult RunPart1()
+    {
+        var result = Part1(InputFile);
+        return new PuzzleResult(result, "42abec426a1f1903197aaf0635b6a29a");
+    }
+
+    protected override PuzzleResult RunPart2()
+    {
+        var result = Part2(InputFile);
+        return new PuzzleResult(result, "652e203bd80c5889b310a0a8efdb2301");
+    }
+
+    public static long Part1(string input)
+    {
+        var lines = StringReader.ReadLines(input);
+        return lines.Select(FindNextNumber).Sum();
+    }
+
+    public static long Part2(string input)
+    {
+        var lines = StringReader.ReadLines(input);
+        return lines.Select(FindPrevNumber).Sum();
+    }
+
+    public static long FindNextNumber(string input)
+    {
+        var numbers = input.Split(' ').Select(long.Parse).ToList();
+        var lastNumber = numbers.Last();
+        var steps = new List<long>();
+        while (numbers.Any(o => o != 0))
+        {
+            List<long> diffs = [];
+            for (var i = 0; i < numbers.Count - 1; i++)
+            {
+                diffs.Add(numbers[i + 1] - numbers[i]);
+            }
+
+            steps.Add(diffs.Last());
+            numbers = diffs;
+        }
+
+        steps.Reverse();
+        var totalSteps = steps.Sum();
+        return lastNumber + totalSteps;
+    }
+
+    public static long FindPrevNumber(string input)
+    {
+        var numbers = input.Split(' ').Select(long.Parse).ToList();
+        var firstNumber = numbers.First();
+        var steps = new List<long>();
+
+        while (numbers.Any(o => o != 0))
+        {
+            List<long> diffs = [];
+            for (var i = 0; i < numbers.Count - 1; i++)
+            {
+                diffs.Add(numbers[i + 1] - numbers[i]);
+            }
+
+            steps.Add(diffs.First());
+            numbers = diffs;
+        }
+
+        steps.Reverse();
+        var totalSteps = steps.Aggregate(0L, (current, step) => step - current);
+        return firstNumber - totalSteps;
+    }
+}
---
+++ Aoc/Puzzles/Aoc2023/Aoc202309/Aoc202309Tests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202309;
+
+public class Aoc202309Tests
+{
+    [Test]
+    public void Part1()
+    {
+        const string input = """
+                             0 3 6 9 12 15
+                             1 3 6 10 15 21
+                             10 13 16 21 30 45
+                             """;
+
+        var result = Aoc202309.Part1(input);
+
+        result.Should().Be(114);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        const string input = """
+                             0 3 6 9 12 15
+                             1 3 6 10 15 21
+                             10 13 16 21 30 45
+                             """;
+
+        var result = Aoc202309.Part2(input);
+
+        result.Should().Be(2);
+    }
+
+    [TestCase("0 3 6 9 12 15", 18)]
+    [TestCase("1 3 6 10 15 21", 28)]
+    [TestCase("10 13 16 21 30 45", 68)]
+    [TestCase("-1 -2 -3 -4 -5 -6", -7)]
+    public void NextNumber(string input, long expected)
+    {
+        var result = Aoc202309.FindNextNumber(input);
+
+        result.Should().Be(expected);
+    }
+
+    [TestCase("0 3 6 9 12 15", -3)]
+    [TestCase("1 3 6 10 15 21", 0)]
+    [TestCase("10 13 16 21 30 45", 5)]
+    [TestCase("-1 -2 -3 -4 -5 -6", 0)]
+    public void PrevNumber(string input, long expected)
+    {
+        var result = Aoc202309.FindPrevNumber(input);
+
+        result.Should().Be(expected);
+    }
+}

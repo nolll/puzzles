002adf0d8bd7ca33ea786f20e8975b84b79ccc33
Henrik <henriks@gmail.com>
2023-12-10 18:01:15 +0100

    Aoc day 10 2023 solved

---
+++ Aoc/Puzzles/Aoc2023/Aoc202310/Aoc202310.cs
@@ -0,0 +1,131 @@
+﻿using Pzl.Common;
+using Pzl.Tools.CoordinateSystems.CoordinateSystem2D;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202310;
+
+public class Aoc202310 : AocPuzzle
+{
+    public override string Name => "Pipe Maze";
+
+    protected override PuzzleResult RunPart1() => 
+        new(FarthestPoint(InputFile, '|'), "7307413d1efe2d8b5a4994a204a50a86");
+
+    protected override PuzzleResult RunPart2() => 
+        new(EnclosedTileCount(InputFile, '|'), "59413d1f83d3d395818ceddd09c64bbf");
+
+    public static int FarthestPoint(string input, char startPointChar)
+    {
+        var matrix = MatrixBuilder.BuildCharMatrix(input);
+        var startPoint = matrix.Coords.First(o => matrix.ReadValueAt(o) == 'S');
+        matrix.WriteValueAt(startPoint, startPointChar);
+        var loop = GetLoop(matrix, startPoint);
+
+        return loop.Count / 2;
+    }
+
+    public static int EnclosedTileCount(string input, char startPointChar)
+    {
+        var matrix = MatrixBuilder.BuildCharMatrix(input);
+        var startPoint = matrix.Coords.First(o => matrix.ReadValueAt(o) == 'S');
+        matrix.WriteValueAt(startPoint, startPointChar);
+        var loopPath = GetLoop(matrix, startPoint).ToHashSet();
+        var otherCoords = matrix.Coords.Where(o => !loopPath.Contains(o)).ToList();
+
+        foreach (var otherCoord in otherCoords)
+        {
+            matrix.WriteValueAt(otherCoord, '.');
+        }
+
+        var enlargedMatrix = EnlargeMatrix(matrix);
+        var target = new MatrixAddress(enlargedMatrix.XMax, enlargedMatrix.YMax);
+        var queue = new Queue<MatrixAddress>();
+        queue.Enqueue(target);
+        var seen = new HashSet<MatrixAddress>();
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            seen.Add(current);
+            if (enlargedMatrix.ReadValueAt(current) != '.')
+                continue; 
+
+            enlargedMatrix.WriteValueAt(current, 'O');
+            var adjacent = enlargedMatrix.PerpendicularAdjacentCoordsTo(current);
+            foreach (var a in adjacent)
+            {
+                if (!seen.Contains(a))
+                {
+                    queue.Enqueue(a);
+                }
+            }
+        }
+
+        return otherCoords.Count(o => enlargedMatrix.ReadValueAt(o.X * 2 + 1, o.Y * 2 + 1) == '.');
+    }
+
+    private static List<MatrixAddress> GetLoop(Matrix<char> matrix, MatrixAddress startPoint)
+    {
+        var currentPoint = startPoint;
+
+        var stepCount = 0;
+        var seen = new HashSet<MatrixAddress>();
+        var loop = new List<MatrixAddress>();
+        while (!seen.Contains(currentPoint) && stepCount < 100000)
+        {
+            var connections = FindConnections(matrix, currentPoint);
+            seen.Add(currentPoint);
+            loop.Add(currentPoint);
+            var nextPoint = connections.FirstOrDefault(o => !seen.Contains(o));
+            if (nextPoint is null)
+                break;
+            currentPoint = nextPoint;
+            stepCount++;
+        }
+
+        return loop;
+    }
+
+    private static IEnumerable<MatrixAddress> FindConnections(Matrix<char> matrix, MatrixAddress currentPoint)
+    {
+        var currentPipePart = matrix.ReadValueAt(currentPoint);
+
+        var (x, y) = currentPoint.Tuple;
+        var top = new MatrixAddress(x, y - 1);
+        var right = new MatrixAddress(x + 1, y);
+        var bottom = new MatrixAddress(x, y + 1);
+        var left = new MatrixAddress(x - 1, y);
+
+        return currentPipePart switch
+        {
+            '-' => [right, left],
+            '|' => [top, bottom],
+            '7' => [bottom, left],
+            'J' => [top, left],
+            'L' => [top, right],
+            'F' => [right, bottom],
+            _ => [right, bottom]
+        };
+    }
+
+    public static Matrix<char> EnlargeMatrix(Matrix<char> matrix)
+    {
+        var enlarged = new Matrix<char>(matrix.Width * 2 + 2, matrix.Height * 2 + 2, '.');
+        foreach (var coord in matrix.Coords)
+        {
+            var x = coord.X * 2 + 1;
+            var y = coord.Y * 2 + 1;
+            var v = matrix.ReadValueAt(coord);
+            var downValue = v is '|' or 'F' or '7'
+                ? '|'
+                : '.';
+            var rightValue = v is '-' or 'F' or 'L'
+                ? '-'
+                : '.';
+            enlarged.WriteValueAt(x, y, v);
+            enlarged.WriteValueAt(x + 1, y, rightValue);
+            enlarged.WriteValueAt(x, y + 1, downValue);
+            enlarged.WriteValueAt(x + 1, y + 1, '.');
+        }
+
+        return enlarged;
+    }
+}
---
+++ Aoc/Puzzles/Aoc2023/Aoc202310/Aoc202310Tests.cs
@@ -0,0 +1,140 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Pzl.Tools.CoordinateSystems.CoordinateSystem2D;
+using Pzl.Tools.CoordinateSystems.CoordinateSystem3D;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202310;
+
+public class Aoc202310Tests
+{
+    [Test]
+    public void Pipes()
+    {
+        const string input = """
+                             .....
+                             .S-7.
+                             .|.|.
+                             .L-J.
+                             .....
+                             """;
+
+        var result = Aoc202310.FarthestPoint(input, 'F');
+
+        result.Should().Be(4);
+    }
+
+    [Test]
+    public void Enlarge()
+    {
+        const string input = """
+                             .....
+                             .F-7.
+                             .|.|.
+                             .L-J.
+                             .....
+                             """;
+
+        const string expected = """
+                             ............
+                             ............
+                             ............
+                             ...F---7....
+                             ...|...|....
+                             ...|...|....
+                             ...|...|....
+                             ...L---J....
+                             ............
+                             ............
+                             ............
+                             ............
+                             """;
+
+        var inputMatrix = MatrixBuilder.BuildCharMatrix(input);
+
+        var result = Aoc202310.EnlargeMatrix(inputMatrix);
+
+        result.Print().Should().Be(expected);
+    }
+
+    [Test]
+    public void Enclosed1()
+    {
+        const string input = """
+                             ...........
+                             .S-------7.
+                             .|F-----7|.
+                             .||.....||.
+                             .||.....||.
+                             .|L-7.F-J|.
+                             .|..|.|..|.
+                             .L--J.L--J.
+                             ...........
+                             """;
+
+        var result = Aoc202310.EnclosedTileCount(input, 'F');
+
+        result.Should().Be(4);
+    }
+
+    [Test]
+    public void Enclosed2()
+    {
+        const string input = """
+                             ..........
+                             .S------7.
+                             .|F----7|.
+                             .||....||.
+                             .||....||.
+                             .|L-7F-J|.
+                             .|..||..|.
+                             .L--JL--J.
+                             ..........
+                             """;
+
+        var result = Aoc202310.EnclosedTileCount(input, 'F');
+
+        result.Should().Be(4);
+    }
+    
+    [Test]
+    public void Enclosed3()
+    {
+        const string input = """
+                             .F----7F7F7F7F-7....
+                             .|F--7||||||||FJ....
+                             .||.FJ||||||||L7....
+                             FJL7L7LJLJ||LJ.L-7..
+                             L--J.L7...LJS7F-7L7.
+                             ....F-J..F7FJ|L7L7L7
+                             ....L7.F7||L7|.L7L7|
+                             .....|FJLJ|FJ|F7|.LJ
+                             ....FJL-7.||.||||...
+                             ....L---J.LJ.LJLJ...
+                             """;
+
+        var result = Aoc202310.EnclosedTileCount(input, 'F');
+
+        result.Should().Be(8);
+    }
+
+    [Test]
+    public void Enclosed4()
+    {
+        const string input = """
+                             FF7FSF7F7F7F7F7F---7
+                             L|LJ||||||||||||F--J
+                             FL-7LJLJ||||||LJL-77
+                             F--JF--7||LJLJ7F7FJ-
+                             L---JF-JLJ.||-FJLJJ7
+                             |F|F-JF---7F7-L7L|7|
+                             |FFJF7L7F-JF7|JL---7
+                             7-L-JL7||F7|L7F-7F7|
+                             L.L7LFJ|||||FJL7||LJ
+                             L7JLJL-JLJLJL--JLJ.L
+                             """;
+
+        var result = Aoc202310.EnclosedTileCount(input, '7');
+
+        result.Should().Be(10);
+    }
+}

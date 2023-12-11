c4117bdfa885c187bebb20df04fa87ee970c30aa
Henrik <henriks@gmail.com>
2023-12-11 08:36:05 +0100

    Aoc day 11 2023 solved

---
+++ Aoc/Puzzles/Aoc2023/Aoc202311/Aoc202311.cs
@@ -0,0 +1,94 @@
+﻿using Pzl.Common;
+using Pzl.Tools.CoordinateSystems.CoordinateSystem2D;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202311;
+
+[Name("Cosmic Expansion")]
+public class Aoc202311 : AocPuzzle
+{
+    protected override PuzzleResult RunPart1()
+    {
+        return new PuzzleResult(Distances(InputFile, 1), "04a7795748b2f6d2e38898623e8ef01a");
+    }
+
+    protected override PuzzleResult RunPart2()
+    {
+        return new PuzzleResult(Distances(InputFile, 1_000_000), "73bccfdcdd826f77dcdaf67bedecded9");
+    }
+
+    public static long Distances(string input, long multiplier)
+    {
+        var matrix = MatrixBuilder.BuildCharMatrix(input);
+        var expandedCols = new List<long>();
+        var expandedX = 0L;
+        for (var x = 0; x <= matrix.XMax; x++)
+        {
+            var galaxies = 0;
+            for (var y = 0; y <= matrix.YMax; y++)
+            {
+                if (matrix.ReadValueAt(x, y) == '#')
+                {
+                    galaxies++;
+                }
+            }
+
+            if(galaxies == 0)
+            {
+                expandedX += multiplier - 1;
+            }
+
+            expandedCols.Add(x + expandedX);
+        }
+
+        var expandedRows = new List<long>();
+        var expandedY = 0L;
+        for (var y = 0; y <= matrix.YMax; y++)
+        {
+            var galaxies = 0;
+            for (var x = 0; x <= matrix.XMax; x++)
+            {
+                if (matrix.ReadValueAt(x, y) == '#')
+                {
+                    galaxies++;
+                }
+            }
+
+            if (galaxies == 0)
+            {
+                expandedY += multiplier - 1;
+            }
+
+            expandedRows.Add(y + expandedY);
+        }
+
+        var galaxyCoords = matrix.Coords.Where(o => matrix.ReadValueAt(o) == '#');
+        var expandedCoords = galaxyCoords.Select(o => (expandedCols[o.X], expandedRows[o.Y])).ToList();
+
+        var total = 0L;
+        foreach (var a in expandedCoords)
+        {
+            foreach (var b in expandedCoords)
+            {
+                if (!a.Equals(b))
+                {
+                    total += ManhattanDistance(a, b);
+                }
+            }
+        }
+
+        return total / 2;
+    }
+
+    private static long ManhattanDistance((long x, long y) a, (long x, long y) b)
+    {
+        var xMax = Math.Max(a.x, b.x);
+        var xMin = Math.Min(a.x, b.x);
+        var xDiff = xMax - xMin;
+
+        var yMax = Math.Max(a.y, b.y);
+        var yMin = Math.Min(a.y, b.y);
+        var yDiff = yMax - yMin;
+
+        return xDiff + yDiff;
+    }
+}
---
+++ Aoc/Puzzles/Aoc2023/Aoc202311/Aoc202311Tests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202311;
+
+public class Aoc202311Tests
+{
+    [Test]
+    public void Distances1()
+    {
+        const string input = """
+                             ...#......
+                             .......#..
+                             #.........
+                             ..........
+                             ......#...
+                             .#........
+                             .........#
+                             ..........
+                             .......#..
+                             #...#.....
+                             """;
+
+        var result = Aoc202311.Distances(input, 1);
+
+        result.Should().Be(374);
+    }
+
+    [Test]
+    public void Distances2()
+    {
+        const string input = """
+                             ...#......
+                             .......#..
+                             #.........
+                             ..........
+                             ......#...
+                             .#........
+                             .........#
+                             ..........
+                             .......#..
+                             #...#.....
+                             """;
+
+        var result = Aoc202311.Distances(input, 9);
+
+        result.Should().Be(1030);
+    }
+}

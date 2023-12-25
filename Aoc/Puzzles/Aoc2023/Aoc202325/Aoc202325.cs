da92b1c00c8b7972f841a9017bfce20de2fe32ae
Henrik <henriks@gmail.com>
2023-12-25 12:33:52 +0100

    Parsing components for day 25

--- Aoc/Puzzles/Aoc2023/Aoc202325/Aoc202325.cs
+++ Aoc/Puzzles/Aoc2023/Aoc202325/Aoc202325.cs
@@ -1,4 +1,6 @@
+using System.Reflection.PortableExecutable;
 using Pzl.Common;
+using Pzl.Tools.Strings;
 
 namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202325;
 
@@ -13,5 +15,55 @@ public class Aoc202325(string input) : AocPuzzle
     protected override PuzzleResult RunPart2()
     {
         return PuzzleResult.Empty;
+    }
+
+    public static int DivideIntoGroups(string s)
+    {
+        var components = ParseComponents(s);
+
+        return 0;
+    }
+
+    private static Dictionary<string, Component> ParseComponents(string s)
+    {
+        var lines = StringReader.ReadLines(s);
+        var components = new Dictionary<string, Component>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(": ");
+            var compName = parts[0];
+            if (!components.TryGetValue(compName, out var mainComp))
+            {
+                mainComp = new Component(parts[0]);
+                components.Add(compName, mainComp);
+            }
+
+            var connectedNames = parts[1].Split(' ');
+            foreach (var connectedName in connectedNames)
+            {
+                if (!components.TryGetValue(connectedName, out var connectedComp))
+                {
+                    connectedComp = new Component(connectedName);
+                    components.Add(connectedName, connectedComp);
+                }
+
+                connectedComp.Connections.Add(mainComp);
+                mainComp.Connections.Add(connectedComp);
+            }
+        }
+
+        return components;
+    }
+
+    private class Component
+    {
+        public string Name { get; }
+        public List<Component> Connections { get; } = new();
+
+        public Component(string name)
+        {
+            Name = name;
+        }
     }
 }
--- Aoc/Puzzles/Aoc2023/Aoc202325/Aoc202325Tests.cs
+++ Aoc/Puzzles/Aoc2023/Aoc202325/Aoc202325Tests.cs
@@ -1,6 +1,31 @@
+using FluentAssertions;
+using NUnit.Framework;
+
 namespace Pzl.Aoc.Puzzles.Aoc2023.Aoc202325;
 
 public class Aoc202325Tests
 {
+    [Test]
+    public void DivideIntoGroups()
+    {
+        const string input = """
+                             jqt: rhn xhk nvd
+                             rsh: frs pzl lsr
+                             xhk: hfx
+                             cmg: qnr nvd lhk bvb
+                             rhn: xhk bvb hfx
+                             bvb: xhk hfx
+                             pzl: lsr hfx nvd
+                             qnr: nvd
+                             ntq: jqt hfx bvb xhk
+                             nvd: lhk
+                             lsr: lhk
+                             rzs: qnr cmg lsr rsh
+                             frs: qnr lhk lsr
+                             """;
 
+        var result = Aoc202325.DivideIntoGroups(input);
+
+        result.Should().Be(54);
+    }
 }

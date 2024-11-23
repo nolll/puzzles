c0eae054fa76d4e70cc26dcfec5badf309ee609c
Henrik Severinson <henriks@gmail.com>
2024-11-23 19:18:04 +0100

    Everybody Codes Quest 4 Solved

---
+++ Everybody/Puzzles/Everybody04/Everybody04.cs
@@ -0,0 +1,53 @@
+using Pzl.Common;
+using Pzl.Tools.Strings;
+
+namespace Pzl.Everybody.Puzzles.Everybody04;
+
+[Name("Royal Smith's Puzzle")]
+public class Everybody04(string[] inputs) : EverybodyPuzzle
+{
+    protected override PuzzleResult RunPart1()
+    {
+        var result = RunPart1And2(inputs[0]);
+
+        return new PuzzleResult(result, "eabb36cc8d28867fc0f0d332eda02eb3");
+    }
+
+    protected override PuzzleResult RunPart2()
+    {
+        var result = RunPart1And2(inputs[1]);
+
+        return new PuzzleResult(result, "0a37c73499ec6bdb6af25453b7099e38");
+    }
+
+    protected override PuzzleResult RunPart3()
+    {
+        var result = RunPart3(inputs[2]);
+
+        return new PuzzleResult(result, "17d992743f071f1de07d587e4026af89");
+    }
+
+    public static int RunPart1And2(string input)
+    {
+        var list = input.Split(LineBreaks.Single).Select(int.Parse).ToList();
+        var smallest = list.Order().First();
+        return list.Sum(o => o - smallest);
+    }
+    
+    public static int RunPart3(string input)
+    {
+        var list = input.Split(LineBreaks.Single).Select(int.Parse).ToList();
+        var ordered = list.Order().ToList();
+        var smallest = ordered.First();
+        var largest = ordered.Last();
+
+        var smallestDiff = int.MaxValue;
+        for (var i = smallest; i <= largest; i++)
+        {
+            var diff = list.Sum(o => Math.Abs(o - i));
+            smallestDiff = Math.Min(smallestDiff, diff);
+        }
+
+        return smallestDiff;
+    }
+}
---
+++ Everybody/Puzzles/Everybody04/Everybody04Tests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pzl.Everybody.Puzzles.Everybody04;
+
+public class Everybody04Tests
+{
+    [Test]
+    public void Part1And2()
+    {
+        const string input = """
+                             3
+                             4
+                             7
+                             8
+                             """;
+
+        Everybody04.RunPart1And2(input).Should().Be(10);
+    }
+    
+    [Test]
+    public void Part3()
+    {
+        const string input = """
+                             2
+                             4
+                             5
+                             6
+                             8
+                             """;
+
+        Everybody04.RunPart3(input).Should().Be(8);
+    }
+}

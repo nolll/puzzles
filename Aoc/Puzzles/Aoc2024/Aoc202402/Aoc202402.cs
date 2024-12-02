5992e57dfba342a5f09a4d50c84fb447fab66880
Henrik Severinson <henriks@gmail.com>
2024-12-02 10:01:48 +0100

    Aoc 2024 day 2 part 2 solved

--- Aoc/Puzzles/Aoc2024/Aoc202402/Aoc202402.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202402/Aoc202402.cs
@@ -3,22 +3,45 @@ using Pzl.Tools.Strings;
 
 namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202402;
 
-[Name("")]
+[Name("Red-Nosed Reports")]
 public class Aoc202402 : AocPuzzle
 {
     public PuzzleResult Part1(string input)
     {
-        var reports = input.Split(LineBreaks.Single).Select(o => o.Split(' ').Select(int.Parse).ToArray());
+        var reports = input.Split(LineBreaks.Single).Select(o => o.Split(' ').Select(int.Parse).ToList());
         var safeCount = reports.Count(IsSafe);
         return new PuzzleResult(safeCount, "d02b04efe3e126e637aa49339a13e490");
     }
+    
+    public PuzzleResult Part2(string input)
+    {
+        var reports = input.Split(LineBreaks.Single).Select(o => o.Split(' ').Select(int.Parse).ToList());
+        var safeCount = reports.Count(IsSafeWithDamping);
+        return new PuzzleResult(safeCount, "913ea818784ae836ec632c9e92026a7b");
+    }
 
-    private static bool IsSafe(int[] levels)
+    private static bool IsSafe(List<int> levels)
     {
         return IsSafeDecreasing(levels) || IsSafeIncreasing(levels);
     }
+    
+    private static bool IsSafeWithDamping(List<int> levels)
+    {
+        for (var i = 0; i < levels.Count; i++)
+        {
+            var current = levels.ToList();
+            current.RemoveAt(i);
+            
+            if (IsSafeIncreasing(current))
+                return true;
+            
+            if (IsSafeDecreasing(current))
+                return true;
+        }
+        return IsSafeDecreasing(levels) || IsSafeIncreasing(levels);
+    }
 
-    private static bool IsSafeDecreasing(int[] levels)
+    private static bool IsSafeDecreasing(List<int> levels)
     {
         var lastLevel = levels.First();
         foreach (var level in levels.Skip(1))
@@ -32,7 +55,7 @@ public class Aoc202402 : AocPuzzle
         return true;
     }
     
-    private static bool IsSafeIncreasing(int[] levels)
+    private static bool IsSafeIncreasing(List<int> levels)
     {
         var lastLevel = levels.First();
         foreach (var level in levels.Skip(1))
--- Aoc/Puzzles/Aoc2024/Aoc202402/Aoc202402Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202402/Aoc202402Tests.cs
@@ -5,20 +5,27 @@ namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202402;
 
 public class Aoc202402Tests
 {
+    private const string Input = """
+                                 7 6 4 2 1
+                                 1 2 7 8 9
+                                 9 7 6 2 1
+                                 1 3 2 4 5
+                                 8 6 4 4 1
+                                 1 3 6 7 9
+                                 """;
+
     [Test]
     public void Part1()
     {
-        const string input = """
-                             7 6 4 2 1
-                             1 2 7 8 9
-                             9 7 6 2 1
-                             1 3 2 4 5
-                             8 6 4 4 1
-                             1 3 6 7 9
-                             """;
-
-        var result = Sut.Part1(input);
+        var result = Sut.Part1(Input);
         result.Answer.Should().Be("2");
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        var result = Sut.Part2(Input);
+        result.Answer.Should().Be("4");
     }
 
     private static Aoc202402 Sut => new();

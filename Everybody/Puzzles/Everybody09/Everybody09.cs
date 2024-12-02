4b8e0f403a10965d7fc3596a09945befe4446579
Henrik Severinson <henriks@gmail.com>
2024-12-03 00:30:09 +0100

    Everybody codes quest 9 part 3 solved (with a little help from Jonathan Paulson)

--- Everybody/Puzzles/Everybody09/Everybody09.cs
+++ Everybody/Puzzles/Everybody09/Everybody09.cs
@@ -3,179 +3,78 @@ using Pzl.Tools.Strings;
 
 namespace Pzl.Everybody.Puzzles.Everybody09;
 
-[Name("")]
+[Name("Sparkling Bugs")]
 public class Everybody09 : EverybodyPuzzle
 {
     public PuzzleResult RunPart1(string input)
     {
-        var balls = ParseBalls(input);
         int[] stamps = [1, 3, 5, 10];
-        var beetleCounts = new List<int>();
-        
-        foreach (var ball in balls)
-        {
-            var sum = 0;
-            var i = stamps.Length - 1;
-            var beetleCount = 0;
-            while (sum < ball)
-            {
-                var test = sum + stamps[i];
-                if (test == ball)
-                {
-                    beetleCount++;
-                    break;
-                }
-
-                if (test > ball)
-                {
-                    i--;
-                    continue;
-                }
-
-                beetleCount++;
-                sum += stamps[i];
-            }
-            
-            beetleCounts.Add(beetleCount);
-        }
-
-        var result = beetleCounts.Sum();
+        var balls = ParseBalls(input);
+        var precomputedCounts = PrecomputeCounts(stamps, balls.Max());
+        var result = balls.Sum(o => precomputedCounts[o]);
         
         return new PuzzleResult(result, "dffa64bee7ea0c0ad66724de7afe7c08");
     }
 
     public PuzzleResult RunPart2(string input)
     {
-        var balls = ParseBalls(input);
         int[] stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30];
-        var orderedStamps = stamps.OrderDescending().ToList();
-        var stampSum = stamps.Sum();
-        var results = new List<(int, List<int>)>();
-        var largestStamp = orderedStamps.First();
-        
-        foreach (var ball in balls)
-        {
-            List<int> combinations = [];
-            var b = ball;
-            while (b > stampSum)
-            {
-                b -= largestStamp;
-                combinations.Add(largestStamp);
-            }
-            var shortestCombination = new ShortestCombination();
-            var solutions = GetCombinations(orderedStamps, b, combinations, shortestCombination).OrderBy(o => o.Count);
-            var shortestSolution = solutions.First();
-            results.Add((ball, shortestSolution));
-        }
-        
-        var result = results.Sum(o => o.Item2.Count);
-        
+        var balls = ParseBalls(input);
+        var precomputedCounts = PrecomputeCounts(stamps, balls.Max());
+        var result = balls.Sum(o => precomputedCounts[o]);
+
         return new PuzzleResult(result, "6032109447891782512325cb9251f9e2");
     }
     
     public PuzzleResult RunPart3(string input)
     {
-        var seen = new Dictionary<int, List<int>>();
-        List<int> stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101];
-        var orderedStamps = stamps.OrderDescending().ToList();
+        const int rangeSize = 100;
+        int[] stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101];
+        var precomputeMax = ParseBalls(input).Max() / 2 + rangeSize;
+        var precomputed = PrecomputeCounts(stamps, precomputeMax);
+        
         var combinedSums = input.Split(LineBreaks.Single).Select(int.Parse);
         var total = 0;
         foreach (var sum in combinedSums)
         {
             var mid = sum / 2;
-            var min = mid - 100;
-            var max = mid + 100;
-            var minCount = int.MaxValue;
-            for (var i = min; i <= max; i++)
+            var min = mid - rangeSize;
+            var max = mid + rangeSize;
+            var best = int.MaxValue;
+            for (var a = min; a <= max; a++)
             {
-                var a = i;
-                var b = sum - i;
-                if (!seen.TryGetValue(a, out var s1))
+                var b = sum - a;
+                if (Math.Abs(a - b) <= 100)
                 {
-                    s1 = SolveBall(a, orderedStamps);
-                    seen.Add(a, s1);
-                }
-                
-                if (!seen.TryGetValue(b, out var s2))
-                {
-                    s2 = SolveBall(b, orderedStamps);
-                    seen.Add(b, s2);
-                }
-                
-                if (Math.Abs(s1.Sum() - s2.Sum()) <= 100)
-                {
-                    var count = s1.Count + s2.Count;
-                    minCount = Math.Min(minCount, count);
+                    best = Math.Min(best, precomputed[a] + precomputed[b]);
                 }
             }
-            
-            Console.WriteLine(minCount);
 
-            total += minCount;
+            total += best;
         }
 
-        return new PuzzleResult(total);
-    }
-    
-    private static List<int> SolveBall(int ball, List<int> stamps)
-    {
-        List<int> combinations = [];
-        var stampSum = stamps.Sum();
-        var largestStamp = stamps.First();
-
-        while (ball > stampSum)
-        {
-            ball -= largestStamp;
-            combinations.Add(largestStamp);
-        }
-        
-        var shortestCombination = new ShortestCombination();
-        var solutions = GetCombinations(stamps, ball, combinations, shortestCombination).OrderBy(o => o.Count);
-        return solutions.First();
+        return new PuzzleResult(total, "fb766ccc85f0b992daa8d54a92c61b5c");
     }
 
     private static int[] ParseBalls(string input) => 
         input.Split(LineBreaks.Single).Select(int.Parse).ToArray();
 
-    private class ShortestCombination
+    // Thanks, Jonathan Paulson
+    private static int[] PrecomputeCounts(int[] stamps, int max)
     {
-        public int Length { get; set; } = int.MaxValue;
-
-        public void Report(List<int> combination)
+        var counts = new int[max + 1];
+        for (var x = 1; x < max + 1; x++)
         {
-            var count = combination.Count;
-            if (count < Length)
-                Length = count;
-        }
-    }
-    
-    private static List<List<int>> GetCombinations(
-        IReadOnlyCollection<int> denominations, 
-        int target, 
-        List<int> combination,
-        ShortestCombination shortestCombination)
-    {
-        if (target < 0 || !denominations.Any())
-            return [];
-
-        if (target == 0)
-        {
-            shortestCombination.Report(combination);
-            return [combination];
+            var best = int.MaxValue;
+            foreach (var o in stamps)
+            {
+                if (x - o >= 0)
+                    best = Math.Min(best, 1 + counts[x - o]);
+            }
+            
+            counts[x] = best;
         }
 
-        if (combination.Count > shortestCombination.Length)
-            return [];
-
-        var denomination = denominations.First();
-        var remainingDenominations = denominations.Skip(1).ToList();
-
-        var returnedCombinations = new List<List<int>>();
-        var nextCombination = combination.ToList();
-        nextCombination.Add(denomination);
-        returnedCombinations.AddRange(GetCombinations(denominations, target - denomination, nextCombination, shortestCombination));
-        returnedCombinations.AddRange(GetCombinations(remainingDenominations, target, combination.ToList(), shortestCombination));
-
-        return returnedCombinations;
+        return counts;
     }
 }

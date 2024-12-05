857efc5a2d8ade1c5afac6bbd1339804ebdd6742
Henrik Severinson <henriks@gmail.com>
2024-12-05 06:53:04 +0100

    Aoc 2024 day 5 solved

--- Aoc/Puzzles/Aoc2024/Aoc202405/Aoc202405.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202405/Aoc202405.cs
@@ -1,4 +1,5 @@
 using Pzl.Common;
+using Pzl.Tools.Strings;
 
 namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202405;
 
@@ -7,11 +8,93 @@ public class Aoc202405 : AocPuzzle
 {
     public PuzzleResult Part1(string input)
     {
-        return new PuzzleResult(0);
+        var parts = input.Split(LineBreaks.Double);
+        var rules = parts[0].Split(LineBreaks.Single).Select(o => o.Split('|').Select(int.Parse).ToArray()).ToArray();
+        var updates = parts[1].Split(LineBreaks.Single).Select(o => o.Split(',').Select(int.Parse).ToArray()).ToArray();
+
+        var correct = new List<int[]>();
+        var failed = new List<int>();
+        foreach (var update in updates)
+        {
+            var isCorrect = IsCorrect(rules, update);
+            
+            if(isCorrect)
+            {
+                correct.Add(update);
+            }
+        }
+
+        var result = correct.Sum(o => o[o.Length / 2]);
+        
+        return new PuzzleResult(result, "52e320d42cefb1f61089609c0f8bbf25");
     }
 
     public PuzzleResult Part2(string input)
     {
-        return new PuzzleResult(0);
+        var parts = input.Split(LineBreaks.Double);
+        var rules = parts[0].Split(LineBreaks.Single).Select(o => o.Split('|').Select(int.Parse).ToArray()).ToArray();
+        var updates = parts[1].Split(LineBreaks.Single).Select(o => o.Split(',').Select(int.Parse).ToArray()).ToArray();
+
+        var correct = new List<int[]>();
+        var failed = new List<int>();
+        foreach (var update in updates)
+        {
+            if (IsCorrect(rules, update))
+                continue;
+            
+            while (!IsCorrect(rules, update))
+            {
+                Fix(rules, update);
+            }
+            
+            correct.Add(update);
+        }
+
+        var result = correct.Sum(o => o[o.Length / 2]);
+        
+        return new PuzzleResult(result, "4e2cd8cd0086290e712bf6f5f8799fe3");
+    }
+
+    private bool IsCorrect(int[][] rules, int[] update)
+    {
+        foreach (var rule in rules)
+        {
+            if (update.Contains(rule[0]) && update.Contains(rule[1]))
+            {
+                var leftRule = rule[0];
+                var rightRule = rule[1];
+                var leftPos = Array.IndexOf(update, leftRule);
+                var rightPos = Array.IndexOf(update, rightRule);
+
+                if (rightPos < leftPos)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
+    private int[] Fix(int[][] rules, int[] update)
+    {
+        foreach (var rule in rules)
+        {
+            if (update.Contains(rule[0]) && update.Contains(rule[1]))
+            {
+                var leftRule = rule[0];
+                var rightRule = rule[1];
+                var leftPos = Array.IndexOf(update, leftRule);
+                var rightPos = Array.IndexOf(update, rightRule);
+                
+                if (rightPos < leftPos)
+                {
+                    update[leftPos] = rightRule;
+                    update[rightPos] = leftRule;
+                }
+            }
+        }
+
+        return update;
     }
 }
--- Aoc/Puzzles/Aoc2024/Aoc202405/Aoc202405Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202405/Aoc202405Tests.cs
@@ -5,20 +5,47 @@ namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202405;
 
 public class Aoc202405Tests
 {
+    private const string Input = """
+                                 47|53
+                                 97|13
+                                 97|61
+                                 97|47
+                                 75|29
+                                 61|13
+                                 75|53
+                                 29|13
+                                 97|29
+                                 53|29
+                                 61|53
+                                 97|53
+                                 61|29
+                                 47|13
+                                 75|47
+                                 97|75
+                                 47|61
+                                 75|61
+                                 47|29
+                                 75|13
+                                 53|13
+
+                                 75,47,61,53,29
+                                 97,61,53,29,13
+                                 75,29,13
+                                 75,97,47,61,53
+                                 61,13,29
+                                 97,13,75,29,47
+                                 """;
+
     [Test]
     public void Part1()
     {
-        const string input = "";
-
-        Sut.Part1(input).Answer.Should().Be("0");
+        Sut.Part1(Input).Answer.Should().Be("143");
     }
 
     [Test]
     public void Part2()
     {
-        const string input = "";
-
-        Sut.Part2(input).Answer.Should().Be("0");
+        Sut.Part2(Input).Answer.Should().Be("123");
     }
 
     private static Aoc202405 Sut => new();

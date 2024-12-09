3b5674fcf16aef3306b214114ee0ef0dae36b91f
Henrik Severinson <henriks@gmail.com>
2024-12-09 18:35:01 +0100

    Aoc 2024 day 9 part 2 solved

--- Aoc/Puzzles/Aoc2024/Aoc202409/Aoc202409.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202409/Aoc202409.cs
@@ -46,6 +46,88 @@ public class Aoc202409 : AocPuzzle
             r--;
         }
 
+        var checksum = GetChecksum(blocks);
+        
+        return new PuzzleResult(checksum, "4ca7aa30f44261b47440a1e8e3647d44");
+    }
+
+    public PuzzleResult Part2(string input)
+    {
+        var nums = input.ToCharArray().Select(o => int.Parse(o.ToString())).ToArray();
+        var blocks = new int[nums.Sum()];
+
+        var b = 0;
+        var isFile = true;
+        var filePositions = new List<int>();
+        var fileLengths = new List<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var fileLength = 0;
+            var filePos = b;
+            for (int j = 0; j < nums[i]; j++)
+            {
+                blocks[b] = isFile ? i / 2 : -1;
+                b++;
+                fileLength++;
+            }
+            
+            if(isFile)
+            {
+                filePositions.Add(filePos);
+                fileLengths.Add(fileLength);
+            }
+
+            isFile = !isFile;
+        }
+
+        var fileIndex = fileLengths.Count - 1;
+        
+        while (fileIndex > 0)
+        {
+            var f = 0;
+            var filePos = filePositions[fileIndex];
+
+            while (f < filePos)
+            {
+                if (blocks[f] > -1)
+                {
+                    f++;
+                    continue;
+                }
+                
+                var availableSpace = 0;
+                var localPos = 0;
+                while (f + localPos < blocks.Length && blocks[f + localPos] == -1)
+                {
+                    availableSpace++;
+                    localPos++;
+                }
+                
+                if (fileLengths[fileIndex] <= availableSpace)
+                {
+                    for (var i = 0; i < fileLengths[fileIndex]; i++)
+                    {
+                        blocks[f] = blocks[filePositions[fileIndex] + i];
+                        blocks[filePositions[fileIndex] + i] = -1;
+                        f++;
+                    }
+
+                    continue;
+                }
+
+                f++;
+            }
+
+            fileIndex--;
+        }
+
+        var checksum = GetChecksum(blocks);
+        
+        return new PuzzleResult(checksum, "f3d4ebfe09ec0844f2c28f0c22458833");
+    }
+
+    private static long GetChecksum(int[] blocks)
+    {
         var checksum = 0L;
         for (var a = 0; a < blocks.Length; a++)
         {
@@ -53,12 +135,7 @@ public class Aoc202409 : AocPuzzle
             if (v > -1)
                 checksum += a * v;
         }
-        
-        return new PuzzleResult(checksum, "4ca7aa30f44261b47440a1e8e3647d44");
-    }
 
-    public PuzzleResult Part2(string input)
-    {
-        return new PuzzleResult(0);
+        return checksum;
     }
 }
--- Aoc/Puzzles/Aoc2024/Aoc202409/Aoc202409Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202409/Aoc202409Tests.cs
@@ -5,20 +5,18 @@ namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202409;
 
 public class Aoc202409Tests
 {
+    private const string Input = "2333133121414131402";
+
     [Test]
     public void Part1()
     {
-        const string input = "2333133121414131402";
-
-        Sut.Part1(input).Answer.Should().Be("1928");
+        Sut.Part1(Input).Answer.Should().Be("1928");
     }
 
     [Test]
     public void Part2()
     {
-        const string input = "";
-
-        Sut.Part2(input).Answer.Should().Be("0");
+        Sut.Part2(Input).Answer.Should().Be("2858");
     }
 
     private static Aoc202409 Sut => new();

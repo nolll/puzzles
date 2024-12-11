79209d59193eafdbfbbf4f91581ba6baadb3e1c3
Henrik Severinson <henriks@gmail.com>
2024-12-11 12:55:20 +0100

    Aoc 2024 day 11 refactored

--- Aoc/Puzzles/Aoc2024/Aoc202411/Aoc202411.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202411/Aoc202411.cs
@@ -9,11 +9,11 @@ public class Aoc202411 : AocPuzzle
     public PuzzleResult Part1(string input) => new(Solve(input, 25), "adf47aa5d6a1f75ef41af973082e5a60");
     public PuzzleResult Part2(string input) => new(Solve(input, 75), "a58ca60460c322a69523c3daf83c423d");
 
-    private long Solve(string input, int blinkCount) => input.Split(' ')
+    private static long Solve(string input, int blinkCount) => input.Split(' ')
         .Select(long.Parse)
         .Sum(o => CountStones(o, blinkCount, new Dictionary<(long, int), long>()));
 
-    private long CountStones(long n, int depth, Dictionary<(long, int), long> seen)
+    private static long CountStones(long n, int depth, Dictionary<(long, int), long> seen)
     {
         if (seen.TryGetValue((n, depth), out var cached))
             return cached;
@@ -22,33 +22,25 @@ public class Aoc202411 : AocPuzzle
             return 1;
 
         var nextDepth = depth - 1;
+        long result;
         
         if (n == 0)
-        {
-            var p = 1;
-            var c = CountStones(1, nextDepth, seen);
-            var key = (p, nextDepth);
-            seen.TryAdd(key, c);
-            return c;
-        }
+            result = CountStones(1, nextDepth, seen);
+        else if (n.ToString().Length % 2 == 0)
+            result = Cut(n).Sum(o => CountStones(o, nextDepth, seen));
+        else
+            result = CountStones(n * 2024, nextDepth, seen);
 
-        var s = n.ToString(); 
-        if(s.Length % 2 == 0)
-        {
-            var halfLength = s.Length / 2;
-            var p1 = long.Parse(s[..halfLength]);
-            var p2 = long.Parse(s[halfLength..]);
-            var c1 = CountStones(p1, nextDepth, seen);
-            var c2 = CountStones(p2, nextDepth, seen);
-            seen.TryAdd((p1, nextDepth), c1);
-            seen.TryAdd((p2, nextDepth), c2);
-            return c1 + c2;
-        }
+        seen.TryAdd((n, depth), result);
+        return result;
+    }
 
-        var pp = n * 2024;
-        var cc = CountStones(pp, nextDepth, seen);
-        var kk = (pp, nextDepth);
-        seen.TryAdd(kk, cc);
-        return cc;
+    private static long[] Cut(long x)
+    {
+        var s = x.ToString();
+        var halfLength = s.Length / 2;
+        var p1 = long.Parse(s[..halfLength]);
+        var p2 = long.Parse(s[halfLength..]);
+        return [p1, p2];
     }
 }
--- Aoc/Puzzles/Aoc2024/Aoc202411/Aoc202411Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202411/Aoc202411Tests.cs
@@ -8,16 +8,10 @@ public class Aoc202411Tests
     private const string Input = "125 17";
 
     [Test]
-    public void Part1()
-    {
-        Sut.Part1(Input).Answer.Should().Be("55312");
-    }
-    
+    public void Part1() => Sut.Part1(Input).Answer.Should().Be("55312");
+
     [Test]
-    public void Part2()
-    {
-        Sut.Part2(Input).Answer.Should().Be("65601038650482");
-    }
+    public void Part2() => Sut.Part2(Input).Answer.Should().Be("65601038650482");
 
     private static Aoc202411 Sut => new();
 }

d00c09a4bb561ff313e7fbdb7416ff0229d4b800
Henrik Severinson <henriks@gmail.com>
2024-12-14 19:44:29 +0100

    Aoc 2024 day 13 part 2 solved

--- Aoc/Puzzles/Aoc2024/Aoc202413/Aoc202413Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202413/Aoc202413Tests.cs
@@ -28,54 +28,12 @@ public class Aoc202413Tests
     {
         Sut.Part1(Input).Answer.Should().Be("480");
     }
-
-    [Test]
-    public void IsSolvable1()
-    {
-        const string input = """
-                             Button A: X+94, Y+34
-                             Button B: X+22, Y+67
-                             Prize: X=8400, Y=5400
-                             """;
-    
-        Sut.IsSolvable(input).Should().BeFalse();
-    }
     
     [Test]
-    public void IsSolvable2()
+    public void Part2()
     {
-        const string input = """
-                             Button A: X+26, Y+66
-                             Button B: X+67, Y+21
-                             Prize: X=12748, Y=12176
-                             """;
-    
-        Sut.IsSolvable(input).Should().BeTrue();
+        Aoc202413.Solve(Input, 0).Should().Be(480);
     }
     
-    [Test]
-    public void IsSolvable3()
-    {
-        const string input = """
-                             Button A: X+17, Y+86
-                             Button B: X+84, Y+37
-                             Prize: X=7870, Y=6450
-                             """;
-    
-        Sut.IsSolvable(input).Should().BeFalse();
-    }
-    
-    [Test]
-    public void IsSolvable4()
-    {
-        const string input = """
-                             Button A: X+69, Y+23
-                             Button B: X+27, Y+71
-                             Prize: X=18641, Y=10279
-                             """;
-    
-        Sut.IsSolvable(input).Should().BeTrue();
-    }
-
     private static Aoc202413 Sut => new();
 }

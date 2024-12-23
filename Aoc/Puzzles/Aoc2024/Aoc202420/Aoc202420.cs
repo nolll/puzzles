d78d0b69c187c6fcb38fb62e55ca8b75a52883c9
Henrik Severinson <henriks@gmail.com>
2024-12-23 01:35:39 +0100

    Aoc 2024 day 20 part 2 solved

--- Aoc/Puzzles/Aoc2024/Aoc202420/Aoc202420.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202420/Aoc202420.cs
@@ -1,6 +1,5 @@
 using Pzl.Common;
 using Pzl.Tools.CoordinateSystems.CoordinateSystem2D;
-using Pzl.Tools.HashSets;
 
 namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202420;
 
@@ -9,80 +8,22 @@ public class Aoc202420 : AocPuzzle
 {
     public PuzzleResult Part1(string input)
     {
-        var result = CountCheatsBetterThanPart1(input, 100);
+        var result = CountCheatsBetterThan(input, 2, 100);
         
         return new PuzzleResult(result, "345ad458eb8fecd2bee1b07cda111f5b");
     }
 
     public PuzzleResult Part2(string input)
     {
-        var result = CountCheatsBetterThanPart2(input, 100);
+        var result = CountCheatsBetterThan(input, 20, 100);
         
-        return new PuzzleResult(0);
+        return new PuzzleResult(result, "180cd20a00ef865aeaee6f0f6484da86");
     }
 
-    public int CountCheatsBetterThan(string input, int limit, int cheatCount)
-    {
-        var results = ScoresWithCheats(input, cheatCount);
-        return results.paths.Count(o => results.baseScore - o.Count >= limit);
-    }
-    
-    public int CountCheatsBetterThanPart1(string input, int limit)
-    {
-        var matrix = MatrixBuilder.BuildCharMatrix(input);
-        var start = matrix.FindAddresses('S').First();
-        var end = matrix.FindAddresses('E').First();
-        matrix.WriteValueAt(start, '.');
-        matrix.WriteValueAt(end, '.');
-        
-        var path = PathFinder.ShortestPathTo(matrix, start, end);
-        path = [start, ..path];
+    public static int CountCheatsBetterThan(string input, int radius, int limit) => 
+        GetCheats(input, radius).Count(o => o.Value >= limit);
 
-        var distanceToTarget = new Dictionary<MatrixAddress, int>();
-        for (var i = 0; i < path.Count; i++)
-        {
-            distanceToTarget.Add(path[i], path.Count - i);
-        }
-
-        var cheats = new List<int>();
-        foreach (var coord in path)
-        {
-            matrix.MoveTo(coord);
-            matrix.TurnTo(MatrixDirection.Up);
-            for (var i = 0; i < 4; i++)
-            {
-                matrix.TurnRight();
-                if (!matrix.TryMoveForward())
-                    continue;
-
-                if(matrix.ReadValue() == '.')
-                {
-                    matrix.MoveBackward();
-                    continue;
-                }
-
-                if (!matrix.MoveForward())
-                    continue;
-                
-                if(matrix.ReadValue() == '.')
-                {
-                    var diff = distanceToTarget[coord] - distanceToTarget[matrix.Address];
-                    if(diff > 0)
-                        cheats.Add(diff);
-                }
-
-                matrix.MoveBackward();
-                matrix.MoveBackward();
-            }
-        }
-
-        return cheats.Count(o => o > limit);
-    }
-
-    public int CountCheatsBetterThanPart2(string input, int limit) => 
-        GetCheatsPart2(input, limit).Count(o => o.Value > limit);
-
-    public Dictionary<(MatrixAddress from, MatrixAddress to), int> GetCheatsPart2(string input, int limit)
+    public static Dictionary<(MatrixAddress from, MatrixAddress to), int> GetCheats(string input, int radius)
     {
         var matrix = MatrixBuilder.BuildCharMatrix(input);
         var start = matrix.FindAddresses('S').First();
@@ -98,173 +39,28 @@ public class Aoc202420 : AocPuzzle
         {
             distanceToTarget.Add(path[i], path.Count - i - 1);
         }
-        
+
         var cheats = new Dictionary<(MatrixAddress from, MatrixAddress to), int>();
-        var counter = 1;
-        Console.WriteLine($" Total count: {path.Count}");
         foreach (var coord in path)
         {
-            var closeTargets = path
-                .Where(o =>
-                {
-                    var distance = o.ManhattanDistanceTo(coord);
-                    var isClose = distance is > 0 and <= 20;
-                    if (!isClose)
-                        return false;
-                    return distanceToTarget[coord] - distanceToTarget[o] - distance > limit;
-                }).ToList();
-            
-            Console.WriteLine($" {counter}: {coord.Id}: {closeTargets.Count}");
-            counter++;
-            
-            foreach (var target in closeTargets)
+            for (var y = -radius; y <= radius; y++)
             {
-                var p = PathFinder.ShortestPathTo(matrix, coord, target, NeighborFunc);
-                if (p.Count > 0)
+                for (var x = -radius; x <= radius; x++)
                 {
-                    var diff = distanceToTarget[coord] - distanceToTarget[target] - p.Count;
-                    if (diff > 0) 
-                        cheats[(coord, target)] = diff;
+                    if (Math.Abs(y) + Math.Abs(x) > radius)
+                        continue;
+                    
+                    var nc = new MatrixAddress(coord.X + x, coord.Y + y);
+                    if (matrix.IsOutOfRange(nc) || matrix.ReadValueAt(nc) == '#')
+                        continue;
+
+                    var diff = distanceToTarget[coord] - distanceToTarget[nc] - coord.ManhattanDistanceTo(nc);
+                    if (diff > 0)
+                        cheats.Add((coord, nc), diff);
                 }
-
-                continue;
-
-                List<MatrixAddress> NeighborFunc(Matrix<char> m, MatrixAddress c) => 
-                    matrix.OrthogonalAdjacentCoords.Where(o => matrix.Address.Equals(end) || matrix.ReadValueAt(o) == '#')
-                        .ToList();
             }
         }
 
         return cheats;
-    }
-    
-    public (int baseScore, List<List<MatrixAddress>> paths) ScoresWithCheats(string input, int cheatCount)
-    {
-        var matrix = MatrixBuilder.BuildCharMatrix(input);
-        var start = matrix.FindAddresses('S').First();
-        var end = matrix.FindAddresses('E').First();
-        matrix.WriteValueAt(start, '.');
-        matrix.WriteValueAt(end, '.');
-
-        var path = PathFinder.ShortestPathTo(matrix, start, end);
-        path = [start, ..path];
-        
-        var precalc = new Dictionary<MatrixAddress, List<MatrixAddress>>();
-        for (var i = 0; i < path.Count; i++)
-        {
-            var c = path[i];
-            precalc.Add(c, path.Skip(i + 1).ToList());
-        }
-        
-        var cheatPaths = new List<List<MatrixAddress>>();
-        for (var i = 0; i < path.Count; i++)
-        {
-            if (i % 100 == 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"{i} of {path.Count}");
-            }
-
-            var coord = path[i];
-            var cheatStarts = matrix.OrthogonalAdjacentCoordsTo(coord).Where(o => matrix.ReadValueAt(o) == '#');
-            foreach (var cs in cheatStarts)
-            {
-                var cheatPath = Bfs(matrix, cs, end, cheatCount - 1, precalc);
-                List<MatrixAddress> fullPath = [..path.Skip(1).Take(i), ..cheatPath];
-                cheatPaths.Add(fullPath);
-            }
-        }
-
-        return (path.Count, cheatPaths);
-    }
-
-    public List<List<MatrixAddress>> CheatOne(string input, int cheatIndex)
-    {
-        const int cheatCount = 2;
-        var matrix = MatrixBuilder.BuildCharMatrix(input);
-        var start = matrix.FindAddresses('S').First();
-        var end = matrix.FindAddresses('E').First();
-        matrix.WriteValueAt(start, '.');
-        matrix.WriteValueAt(end, '.');
-
-        var path = PathFinder.ShortestPathTo(matrix, start, end);
-        path = [start, ..path];
-        
-        var precalc = new Dictionary<MatrixAddress, List<MatrixAddress>>();
-        for (var i = 0; i < path.Count; i++)
-        {
-            var c = path[i];
-            precalc.Add(c, path.Skip(i + 1).ToList());
-        }
-        
-        var cheatPaths = new List<List<MatrixAddress>>();
-        var coord = path[cheatIndex];
-        var cheatStarts = matrix.OrthogonalAdjacentCoordsTo(coord).Where(o => matrix.ReadValueAt(o) == '#');
-        var newPaths = new List<List<MatrixAddress>>();
-        foreach (var cs in cheatStarts)
-        {
-            var cheatPath = Bfs(matrix, cs, end, cheatCount - 1, precalc);
-            List<MatrixAddress> fullPath = [..path.Skip(1).Take(cheatIndex), ..cheatPath];
-            newPaths.Add(fullPath);
-        }
-        cheatPaths.AddRange(newPaths);
-        
-        return cheatPaths;
-    }
-    
-    private List<MatrixAddress> Bfs(
-        Matrix<char> matrix, 
-        MatrixAddress start, 
-        MatrixAddress end, 
-        int cheatCount,
-        Dictionary<MatrixAddress, List<MatrixAddress>> precalc)
-    {
-        var queue = new Queue<State>();
-        var visited = new HashSet<MatrixAddress>();
-        queue.Enqueue(new State(start, [], cheatCount - 1));
-
-        while (queue.Count > 0)
-        {
-            var state = queue.Dequeue();
-            var path = state.Path;
-            var node = state.Coord;
-
-            path = [..path, node];
-            visited.Add(node);
-            
-            if (state.CheatStepsLeft == 0 && matrix.ReadValueAt(node) == '.' && path.Count + precalc.Count < precalc[node].Count)
-            {
-                List<MatrixAddress> ret = [..path, ..precalc[node].SkipLast(1)];
-                return ret;
-            }
-
-            if (node.Equals(end))
-                return path;
-
-            var adjNodes = matrix.OrthogonalAdjacentCoordsTo(node);
-            if (state.CheatStepsLeft == 0)
-                adjNodes = adjNodes.Where(o => matrix.ReadValueAt(o) == '.').ToList();
-
-            foreach (var adj in adjNodes)
-            {
-                if (!visited.Contains(adj))
-                {
-                    if(state.CheatStepsLeft > 0 && matrix.ReadValueAt(adj) == '#')
-                        queue.Enqueue(new State(adj, path.ToList(), state.CheatStepsLeft - 1));
-                    
-                    if(matrix.ReadValueAt(adj) == '.')
-                        queue.Enqueue(new State(adj, path.ToList(), 0));
-                }
-            }
-        }
-
-        return [];
-    }
-
-    private class State(MatrixAddress coord, List<MatrixAddress> path, int cheatStepsLeft)
-    {
-        public MatrixAddress Coord { get; } = coord;
-        public List<MatrixAddress> Path { get; } = path;
-        public int CheatStepsLeft { get; } = cheatStepsLeft;
     }
 }
--- Aoc/Puzzles/Aoc2024/Aoc202420/Aoc202420Tests.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202420/Aoc202420Tests.cs
@@ -5,7 +5,6 @@ namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202420;
 
 public class Aoc202420Tests
 {
-    private const int BaseCount = 84;
     private const string Input = """
                                  ###############
                                  #...#...#.....#
@@ -25,54 +24,45 @@ public class Aoc202420Tests
                                  """;
 
     [Test]
-    public void Part1_1()
-    {
-        Sut.CountCheatsBetterThanPart1(Input, 35).Should().Be(4);
-    }
-    
+    public void Part1_1() => Aoc202420.CountCheatsBetterThan(Input, 2, 35).Should().Be(4);
+
     [Test]
-    public void Part2_1()
-    {
-        Sut.CountCheatsBetterThanPart2(Input, 73).Should().Be(7);
-    }
-    
+    public void Part2_1() => Aoc202420.CountCheatsBetterThan(Input, 20, 50).Should().Be(285);
+
     [Test]
     public void Part1_2()
     {
-        var r = Sut.ScoresWithCheats(Input, 2);
-        var rSavings = r.paths.Select(o => r.baseScore - o.Count - 1).ToList();
-
-        var rstr = string.Join(",", r.paths.Count);
+        var result = Aoc202420.GetCheats(Input, 2);
         
-        var bucket2 = rSavings.Count(o => o == 2);
-        var bucket4 = rSavings.Count(o => o == 4);
-        var bucket6 = rSavings.Count(o => o == 6);
-        var bucket8 = rSavings.Count(o => o == 8);
-        var bucket10 = rSavings.Count(o => o == 10);
-        var bucket12 = rSavings.Count(o => o == 12);
-        var bucket20 = rSavings.Count(o => o == 20);
-        var bucket36 = rSavings.Count(o => o == 36);
-        var bucket38 = rSavings.Count(o => o == 38);
-        var bucket40 = rSavings.Count(o => o == 40);
-        var bucket64 = rSavings.Count(o => o == 64);
+        var bucket2 = result.Where(o => o.Value == 2).ToList();
+        var bucket4 = result.Where(o => o.Value == 4).ToList();
+        var bucket6 = result.Where(o => o.Value == 6).ToList();
+        var bucket8 = result.Where(o => o.Value == 8).ToList();
+        var bucket10 = result.Where(o => o.Value == 10).ToList();
+        var bucket12 = result.Where(o => o.Value == 12).ToList();
+        var bucket20 = result.Where(o => o.Value == 20).ToList();
+        var bucket36 = result.Where(o => o.Value == 36).ToList();
+        var bucket38 = result.Where(o => o.Value == 38).ToList();
+        var bucket40 = result.Where(o => o.Value == 40).ToList();
+        var bucket64 = result.Where(o => o.Value == 64).ToList();
         
-        bucket2.Should().Be(14);
-        bucket4.Should().Be(14);
-        bucket6.Should().Be(2);
-        bucket8.Should().Be(4);
-        bucket10.Should().Be(2);
-        bucket12.Should().Be(3);
-        bucket20.Should().Be(1);
-        bucket36.Should().Be(1);
-        bucket38.Should().Be(1);
-        bucket40.Should().Be(1);
-        bucket64.Should().Be(1);
+        bucket2.Count.Should().Be(14);
+        bucket4.Count.Should().Be(14);
+        bucket6.Count.Should().Be(2);
+        bucket8.Count.Should().Be(4);
+        bucket10.Count.Should().Be(2);
+        bucket12.Count.Should().Be(3);
+        bucket20.Count.Should().Be(1);
+        bucket36.Count.Should().Be(1);
+        bucket38.Count.Should().Be(1);
+        bucket40.Count.Should().Be(1);
+        bucket64.Count.Should().Be(1);
     }
     
     [Test]
     public void Part2_2()
     {
-        var rSavings = Sut.GetCheatsPart2(Input, 2);
+        var rSavings = Aoc202420.GetCheats(Input, 20);
         
         var bucket50 = rSavings.Where(o => o.Value == 50).ToList();
         var bucket52 = rSavings.Where(o => o.Value == 52).ToList();
@@ -103,109 +93,6 @@ public class Aoc202420Tests
         bucket72.Count.Should().Be(22);
         bucket74.Count.Should().Be(4);
         bucket76.Count.Should().Be(3);
-    }
-    
-    [Test]
-    public void Part2_3()
-    {
-        var rSavings = Sut.GetCheatsPart2(Input, 2);
-        
-        var bucket72 = rSavings.Where(o => o.Value == 72).OrderBy(o => o.Key.from.X).ThenBy(o => o.Key.from.Y).ToList();
-        
-        bucket72.Count.Should().Be(22);
-        
-    }
-
-    [TestCase(0, 4)]
-    [TestCase(1, 2)]
-    [TestCase(2, 0)]
-    [TestCase(3, 0)]
-    [TestCase(4, 4)]
-    [TestCase(5, 2)]
-    [TestCase(6, 0)]
-    [TestCase(7, 0)]
-    [TestCase(8, 4)]
-    [TestCase(9, 2)]
-    [TestCase(10, 0)]
-    [TestCase(11, 0)]
-    [TestCase(12, 12)]
-    [TestCase(13, 10)]
-    [TestCase(14, 8)]
-    [TestCase(15, 6)]
-    [TestCase(16, 4)]
-    [TestCase(17, 2)]
-    [TestCase(18, 64)]
-    [TestCase(19, 38)]
-    [TestCase(20, 36)]
-    [TestCase(21, 0)]
-    [TestCase(22, 12)]
-    [TestCase(23, 10)]
-    [TestCase(24, 8)]
-    [TestCase(25, 0)]
-    [TestCase(26, 0)]
-    [TestCase(27, 0)]
-    [TestCase(28, 4)]
-    [TestCase(29, 2)]
-    [TestCase(30, 0)]
-    [TestCase(31, 0)]
-    [TestCase(32, 4)]
-    [TestCase(33, 2)]
-    [TestCase(34, 0)]
-    [TestCase(35, 0)]
-    [TestCase(36, 4)]
-    [TestCase(37, 2)]
-    [TestCase(38, 0)]
-    [TestCase(39, 0)]
-    [TestCase(40, 4)]
-    [TestCase(41, 2)]
-    [TestCase(42, 0)]
-    [TestCase(43, 0)]
-    [TestCase(44, 12)]
-    [TestCase(45, 2)]
-    [TestCase(46, 0)]
-    [TestCase(47, 0)]
-    [TestCase(48, 0)]
-    [TestCase(49, 0)]
-    [TestCase(50, 4)]
-    [TestCase(51, 2)]
-    [TestCase(52, 0)]
-    [TestCase(53, 0)]
-    [TestCase(54, 8)]
-    [TestCase(55, 6)]
-    [TestCase(56, 4)]
-    [TestCase(57, 2)]
-    [TestCase(58, 0)]
-    [TestCase(59, 0)]
-    [TestCase(60, 0)]
-    [TestCase(61, 0)]
-    [TestCase(62, 4)]
-    [TestCase(63, 2)]
-    [TestCase(64, 0)]
-    [TestCase(65, 0)]
-    [TestCase(66, 4)]
-    [TestCase(67, 2)]
-    [TestCase(68, 0)]
-    [TestCase(69, 0)]
-    [TestCase(70, 8)]
-    [TestCase(71, 2)]
-    [TestCase(72, 0)]
-    [TestCase(73, 0)]
-    [TestCase(74, 0)]
-    [TestCase(75, 0)]
-    [TestCase(76, 0)]
-    [TestCase(77, 0)]
-    [TestCase(78, 0)]
-    [TestCase(79, 0)]
-    [TestCase(80, 0)]
-    [TestCase(81, 0)]
-    [TestCase(82, 0)]
-    [TestCase(83, 0)]
-    [TestCase(84, 0)]
-    public void CheatOne(int index, int savings)
-    {
-        var results = Sut.CheatOne(Input, index);
-        var result = Math.Max(BaseCount - results.Min(o => o.Count), 0);
-        result.Should().Be(savings);
     }
 
     private static Aoc202420 Sut => new();

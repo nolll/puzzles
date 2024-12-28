601646456877bdd03c26ff96a2365337232e6c8a
Henrik Severinson <henriks@gmail.com>
2024-12-28 15:18:09 +0100

    Aoc 2024 day 6 performance. 60s -> 5s

--- Aoc/Puzzles/Aoc2024/Aoc202406/Aoc202406.cs
+++ Aoc/Puzzles/Aoc2024/Aoc202406/Aoc202406.cs
@@ -3,15 +3,14 @@ using Pzl.Tools.CoordinateSystems.CoordinateSystem2D;
 
 namespace Pzl.Aoc.Puzzles.Aoc2024.Aoc202406;
 
-[IsSlow]
-[Comment("Many matrix loops")]
 [Name("Guard Gallivant")]
 public class Aoc202406 : AocPuzzle
 {
     public PuzzleResult Part1(string input)
     {
         var matrix = MatrixBuilder.BuildCharMatrix(input);
-        var visitCount = GetVisitCount(matrix) ?? 0;
+        var startCoord = matrix.FindAddresses('^').First();
+        var visitCount = GetVisitCount(matrix, startCoord) ?? 0;
         
         return new PuzzleResult(visitCount, "471731c253513e4cf775d088dc806f6a");
     }
@@ -19,37 +18,34 @@ public class Aoc202406 : AocPuzzle
     public PuzzleResult Part2(string input)
     {
         var matrix = MatrixBuilder.BuildCharMatrix(input);
+        var startCoord = matrix.FindAddresses('^').First();
+        var visited = GetVisited(matrix, startCoord);
+        var loopCount = visited!.Sum(blockedCoord => GetVisitCount(matrix, startCoord, blockedCoord) is null ? 1 : 0);
 
-        var blockedCoords = matrix.Coords.Where(o => matrix.ReadValueAt(o) != '#' && matrix.ReadValueAt(o) != '^');
-
-        var loopCount = 0;
-        foreach (var blockedCoord in blockedCoords)
-        {
-            var copy = matrix.Clone();
-            loopCount += GetVisitCount(copy, blockedCoord) is null ? 1 : 0;
-        }
-        
         return new PuzzleResult(loopCount, "ff1f7615465eed4279ca5994cb797751");
     }
+
+    private static int? GetVisitCount(
+        Matrix<char> matrix,
+        MatrixAddress startCoord,
+        MatrixAddress? blockedCoord = null) =>
+        GetVisited(matrix, startCoord, blockedCoord)?.Count;
     
-    private static int? GetVisitCount(Matrix<char> matrix, MatrixAddress? blockedCoord = null)
+    private static HashSet<MatrixAddress>? GetVisited(Matrix<char> matrix, MatrixAddress startCoord, MatrixAddress? blockedCoord = null)
     {
-        var startCoord = matrix.FindAddresses('^').First();
         var cache = new HashSet<(MatrixAddress, MatrixDirection)>();
-        
-        if(blockedCoord is not null)
-            matrix.WriteValueAt(blockedCoord, '#');
+        var visited = new HashSet<MatrixAddress>();
         
         matrix.MoveTo(startCoord);
         matrix.TurnTo(MatrixDirection.Up);
-        matrix.WriteValue('X');
+        visited.Add(matrix.Address);
         
         while (true)
         {
             if (!matrix.TryMoveForward())
                 break;
 
-            if (matrix.ReadValue() == '#')
+            if (matrix.Address.Equals(blockedCoord) || matrix.ReadValue() == '#')
             {
                 matrix.MoveBackward();
                 matrix.TurnRight();
@@ -59,10 +55,10 @@ public class Aoc202406 : AocPuzzle
             if(cache.Contains((matrix.Address, matrix.Direction)))
                 return null;
             
-            matrix.WriteValue('X');
+            visited.Add(matrix.Address);
             cache.Add((matrix.Address, matrix.Direction));
         }
 
-        return matrix.FindAddresses('X').Count;
+        return visited;
     }
 }

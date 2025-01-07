5101e3f1148ab63b593420ba91059dfda900a65b
Henrik Severinson <henriks@gmail.com>
2025-01-07 23:35:08 +0100

    Everybody Codes Quest 14 cleanup

--- Everybody/Puzzles/Everybody14/Everybody14.cs
+++ Everybody/Puzzles/Everybody14/Everybody14.cs
@@ -10,24 +10,24 @@ public class Everybody14 : EverybodyPuzzle
     public PuzzleResult Part1(string input)
     {
         var y = 0;
-        var miny = 0;
         var maxy = 0;
 
         foreach (var instr in input.Split(','))
         {
+            var c = instr[0];
             var v = int.Parse(instr[1..]);
-            if (instr.StartsWith("U"))
-                y -= v;
-            else if (instr.StartsWith("D")) 
-                y += v;
-
-            miny = Math.Min(y, miny);
+            var diff = c switch
+            {
+                'U' => v,
+                'D' => -v,
+                _ => 0
+            };
+            y += diff;
+            
             maxy = Math.Max(y, maxy);
         }
-
-        var result = maxy - miny;
         
-        return new PuzzleResult(result, "4d7ad96354959558ed0b95fa70be777c");
+        return new PuzzleResult(maxy, "4d7ad96354959558ed0b95fa70be777c");
     }
 
     public PuzzleResult Part2(string input)

ab579ec9b4adea8301817fa5a97eef42ba38f651
Henrik Severinson <henriks@gmail.com>
2025-01-11 13:16:53 +0100

    Everybody Codes Quest 16 part 2 work in progress

--- Everybody/Puzzles/Everybody16/Everybody16.cs
+++ Everybody/Puzzles/Everybody16/Everybody16.cs
@@ -1,4 +1,6 @@
+using Newtonsoft.Json;
 using Pzl.Common;
+using Pzl.Tools.Maths;
 using Pzl.Tools.Strings;
 
 namespace Pzl.Everybody.Puzzles.Everybody16;
@@ -47,7 +49,79 @@ public class Everybody16 : EverybodyPuzzle
 
     public PuzzleResult Part2(string input)
     {
-        return new PuzzleResult(0);
+        var parts = input.Split(LineBreaks.Double);
+        var increments = parts[0].Split(',').Select(int.Parse).ToArray();
+        var wheels = new List<List<string>>();
+        
+        for (var i = 0; i < increments.Length; i++)
+        {
+            var s = i * 4;
+            var wheel = new List<string>();
+                
+            foreach (var line in parts[1].Split(LineBreaks.Single))
+            {
+                if (line.Length <= s)
+                    break;
+
+                var cat = line.Substring(s, 3);
+                if (cat == "   ")
+                    break;
+                
+                wheel.Add(cat);
+            }
+            
+            wheels.Add(wheel);
+        }
+
+        var lcm = MathTools.Lcm(increments.Select(o => (long)o));
+        Console.WriteLine($"lcm: {lcm}");
+
+        var seen = new HashSet<string>();
+        var count = 1;
+        var score = 0L;
+        while (true)
+        {
+            var cats = new List<string>();
+            for (var i = 0; i < wheels.Count; i++)
+            {
+                var wheel = wheels[i];
+                var cat = wheel[count * increments[i] % wheel.Count];
+                cats.Add(cat);
+            }
+            
+            var full = string.Join("", cats);
+            if (seen.Contains(full))
+                break;
+
+            var stripped = string.Join("", cats.Select(o => $"{o[0]}{o[2]}"));
+            score += Score(stripped);
+            
+            seen.Add(full);
+            count++;
+        }
+
+        var result = 0;
+        
+        return new PuzzleResult(result);
+    }
+
+    public int Score(string s)
+    {
+        var counts = new Dictionary<char, int>();
+        foreach (var c in s)
+        {
+            if (!counts.TryAdd(c, 1))
+                counts[c]++;
+        }
+
+        var score = 0;
+        foreach (var kv in counts)
+        {
+            if (kv.Value >= 3)
+                score += kv.Value - 2;
+        }
+        
+        return score;
     }
 
     public PuzzleResult Part3(string input)
--- Everybody/Puzzles/Everybody16/Everybody16Tests.cs
+++ Everybody/Puzzles/Everybody16/Everybody16Tests.cs
@@ -24,10 +24,24 @@ public class Everybody16Tests
     [Test]
     public void Part2()
     {
-        const string input = "";
+        const string input = """
+                             1,2,3
+                             
+                             ^_^ -.- ^,-
+                             >.- ^_^ >.<
+                             -_- -.- >.<
+                                 -.^ ^_^
+                                 >.>
+                             """;
 
         Sut.Part2(input).Answer.Should().Be("0");
     }
+
+    [TestCase(">.--.-^_^", 1)]
+    [TestCase("-_->.>>.<", 1)]
+    [TestCase("^_^^_^>.<", 2)]
+    [TestCase("^_^^_^^_^", 5)]
+    public void Scoring(string input, int expected) => Sut.Score(input).Should().Be(expected);
 
     [Test]
     public void Part3()

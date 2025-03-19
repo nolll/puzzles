072fb820acac4b82189e1c574c20b20382c1f709
Henrik Severinson <henriks@gmail.com>
2025-03-19 22:03:12 +0100

    Codyssi Challenge 2 2025 refactored

--- Codyssi/Puzzles/Codyssi02/Codyssi02.cs
+++ Codyssi/Puzzles/Codyssi02/Codyssi02.cs
@@ -9,53 +9,47 @@ public class Codyssi02 : CodyssiPuzzle
 {
     public PuzzleResult Part1(string input)
     {
-        var parts = input.Split(LineBreaks.Double);
-        var opnums = Numbers.IntsFromString(parts.First());
-        var items = parts.Last().Split(LineBreaks.Single).Select(long.Parse).Order().ToArray();
-        var n = items[items.Length / 2];
-        n = (long)Math.Pow(n, opnums[2]);
-        n *= opnums[1];
-        n += opnums[0];
+        var (parameters, rooms) = Parse(input);
+        var cost = ApplyFunction(rooms[rooms.Length / 2], parameters);
         
-        return new PuzzleResult(n, "08f294141be67fb8ce4f13e39d3f12d0");
+        return new PuzzleResult(cost, "08f294141be67fb8ce4f13e39d3f12d0");
     }
 
     public PuzzleResult Part2(string input)
     {
-        var parts = input.Split(LineBreaks.Double);
-        var opnums = Numbers.IntsFromString(parts.First());
-        var items = parts.Last().Split(LineBreaks.Single).Select(long.Parse).Order().ToArray();
-        var n = items.Where(o => o % 2 == 0).Sum();
-        n = (long)Math.Pow(n, opnums[2]);
-        n *= opnums[1];
-        n += opnums[0];
+        var (parameters, rooms) = Parse(input);
+        var cost = ApplyFunction(rooms.Where(o => o % 2 == 0).Sum(), parameters);
         
-        return new PuzzleResult(n, "f53524668fe091cf2cd65be1bf22d73f");
+        return new PuzzleResult(cost, "f53524668fe091cf2cd65be1bf22d73f");
     }
 
     public PuzzleResult Part3(string input)
     {
         const long max = 15_000_000_000_000;
         
-        var parts = input.Split(LineBreaks.Double);
-        var opnums = Numbers.IntsFromString(parts.First());
-        var items = parts.Last().Split(LineBreaks.Single).Select(long.Parse).Order().ToArray();
-        var best = 0L;
-        var bestRoom = 0L;
-        foreach (var item in items)
+        var (parameters, rooms) = Parse(input);
+        (long room, long cost) best = (0, 0);
+        foreach (var item in rooms)
         {
-            var n = (long)Math.Pow(item, opnums[2]);
-            n *= opnums[1];
-            n += opnums[0];
+            var cost = ApplyFunction(item, parameters);
 
-            if (n > max || n <= best)
+            if (cost > max || cost <= best.cost)
                 continue;
-            
-            best = n;
-            bestRoom = item;
+
+            best = (item, cost);
         }
         
-        
-        return new PuzzleResult(bestRoom, "4366f7c950d582ccdb298e25d136b3b9");
+        return new PuzzleResult(best.room, "4366f7c950d582ccdb298e25d136b3b9");
+    }
+
+    private static long ApplyFunction(long input, int[] parameters) => 
+        (long)Math.Pow(input, parameters[2]) * parameters[1] + parameters[0];
+
+    private static (int[] parameters, long[] rooms) Parse(string input)
+    {
+        var parts = input.Split(LineBreaks.Double);
+        var parameters = Numbers.IntsFromString(parts.First());
+        var rooms = parts.Last().Split(LineBreaks.Single).Select(long.Parse).Order().ToArray();
+        return (parameters, rooms);
     }
 }

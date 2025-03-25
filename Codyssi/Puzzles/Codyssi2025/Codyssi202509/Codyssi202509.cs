278fb9bd7a5c21375df002a09745f83559ab790f
Henrik Severinson <henriks@gmail.com>
2025-03-26 00:00:23 +0100

    Codyssi Challenge 9 2025 part 3 solved

--- Codyssi/Puzzles/Codyssi2025/Codyssi202509/Codyssi202509.cs
+++ Codyssi/Puzzles/Codyssi2025/Codyssi202509/Codyssi202509.cs
@@ -13,8 +13,7 @@ public class Codyssi202509 : CodyssiPuzzle
 
         foreach (var (from, to, amount) in transactions)
         {
-            officials[from].Balance -= amount;
-            officials[to].Balance += amount;
+            officials[from].PayPlain(officials[to], amount);
         }
 
         var sum = officials.Values.Select(o => o.Balance).OrderDescending().Take(3).Sum();
@@ -28,9 +27,7 @@ public class Codyssi202509 : CodyssiPuzzle
 
         foreach (var (from, to, amount) in transactions)
         {
-            var amt = Math.Min(amount, officials[from].Balance);
-            officials[from].Balance -= amt;
-            officials[to].Balance += amt;
+            officials[from].PayLimited(officials[to], amount);
         }
 
         var sum = officials.Values.Select(o => o.Balance).OrderDescending().Take(3).Sum();
@@ -40,39 +37,18 @@ public class Codyssi202509 : CodyssiPuzzle
 
     public PuzzleResult Part3(string input)
     {
-        // var (officials, transactions) = Parse(input);
-        //
-        // foreach (var (from, to, amount) in transactions)
-        // {
-        //     var amountToPay = amount;
-        //     var amountToReceive = amount;
-        //     if (officials[from].Balance >= amountToPay)
-        //     {
-        //         officials[from].Balance -= amountToPay;
-        //     }
-        //     else
-        //     {
-        //         amountToReceive = officials[from].Balance;
-        //         var debt = amount - officials[from].Balance;
-        //         officials[from].Balance = 0;
-        //         officials[from].Debts.Add(new Debt(to, debt, officials[from].NextDebtPriority));
-        //         officials[from].NextDebtPriority++;
-        //     }
-        //
-        //     while (officials[to].HasDebt)
-        //     {
-        //         var debt = officials[to].Debts.First();
-        //         if(debt.Amount)
-        //     }
-        //     
-        //     
-        // }
-        //
-        // var sum = officials.Values.Select(o => o.Balance).OrderDescending().Take(3).Sum();
-        //
-        return new PuzzleResult(0);
+        var (officials, transactions) = Parse(input);
+        
+        foreach (var (from, to, amount) in transactions)
+        {
+            officials[from].PayWithDebt(officials[to], amount);
+        }
+        
+        var sum = officials.Values.Select(o => o.Balance).OrderDescending().Take(3).Sum();
+        
+        return new PuzzleResult(sum, "2636591baa425722553a06d9e6e95dd3");
     }
-    
+
     private static (
         Dictionary<string, Official> officials, 
         IEnumerable<(string from, string to, int amount)> transactions) Parse(string input)
@@ -90,16 +66,68 @@ public class Codyssi202509 : CodyssiPuzzle
 
     private class Official(int balance)
     {
-        public int Balance { get; set; } = balance;
-        public List<Debt> Debts { get; set; } = [];
-        public int NextDebtPriority { get; set; } = 0;
-        public bool HasDebt => Debts.Count > 0;
+        private List<Debt> _debts = [];
+        public int Balance { get; private set; } = balance;
+
+        public void PayPlain(Official recipient, int amount)
+        {
+            Balance -= amount;
+            recipient.RecievePlain(amount);
+        }
+        
+        public void PayLimited(Official recipient, int amount)
+        {
+            var amt = Math.Min(amount, Balance);
+            Balance -= amt;
+            recipient.RecievePlain(amt);
+        }
+        
+        public void PayWithDebt(Official recipient, int amount)
+        {
+            var prevBalance = Balance;
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                recipient.RecieveWithDebt(amount);
+                return;
+            }
+
+            Balance = 0;
+            recipient.RecieveWithDebt(prevBalance);
+            _debts.Add(new Debt(recipient, amount - prevBalance));
+        }
+
+        private void RecieveWithDebt(int amount)
+        {
+            while (amount > 0 && _debts.Count(o => o.Amount > 0) > 0)
+            {
+                var debt = _debts.First(o => o.Amount > 0);
+                if (debt.Amount > amount)
+                {
+                    debt.Amount -= amount;
+                    debt.Creditor.RecieveWithDebt(amount);
+                    amount = 0;
+                }
+                else
+                {
+                    amount -= debt.Amount;
+                    var amountToPay = debt.Amount;
+                    debt.Amount = 0;
+                    debt.Creditor.RecieveWithDebt(amountToPay);
+                }
+
+                _debts = _debts.Where(o => o.Amount > 0).ToList();
+            }
+
+            Balance += amount;
+        }
+
+        private void RecievePlain(int amount) => Balance += amount;
     }
 
-    private class Debt(string to, int amount, int priority)
+    private class Debt(Official creditor, int amount)
     {
-        public string To { get; } = to;
+        public Official Creditor { get; } = creditor;
         public int Amount { get; set; } = amount;
-        public int Priority { get; } = priority;
     }
 }

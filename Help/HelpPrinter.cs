39bf7dd17a436a8cd3cbfed4658873553d60f533
Henrik Severinson <henriks@gmail.com>
2025-04-06 20:19:29 +0200

    Added codyssi to help text

--- Help/HelpPrinter.cs
+++ Help/HelpPrinter.cs
@@ -5,7 +5,7 @@ namespace Pzl.Client.Help;
 public class HelpPrinter
 {
     private const string HelpText = """
-                                    My solutions to Advent of Code, AquaQ Challenge, Project Euler and Everybody Codes.
+                                    My solutions to Advent of Code, AquaQ Challenge, Project Euler, Everybody Codes and Codyssi.
                                     https://github.com/nolll/puzzles
 
                                     Usage:
@@ -16,8 +16,11 @@ public class HelpPrinter
                                                       --tags aoc,2022 (runs all 2022 aoc puzzles)
                                                       --tags aquaq (runs all aquaq puzzles)
                                                       --tags euler,4 (runs euler puzzle 4)
+                                                      --tags everybody,12 (runs everybody codes quest 12)
+                                                      --tags codyssi,2025,15 (runs codyssi challenge 15 2025)
 
-                                    -s    --search    search query. Use quotes if the query contains spaces
+                                    -s    --search    search query. Searches titles, class names and comments.
+                                                      Use quotes if the query contains spaces
 
                                     -h    --help      display this help text
 

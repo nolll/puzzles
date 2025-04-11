c52536210581b528d1c6e3e8350e73282bbf84b0
Henrik Severinson <henriks@gmail.com>
2025-04-11 19:01:41 +0200

    Refactored and added tests for day 1 2015

--- Aoc/Puzzles/Aoc2015/Aoc201501/Aoc201501.cs
+++ Aoc/Puzzles/Aoc2015/Aoc201501/Aoc201501.cs
@@ -5,17 +5,34 @@ namespace Pzl.Aoc.Puzzles.Aoc2015.Aoc201501;
 [Name("Not Quite Lisp")]
 public class Aoc201501 : AocPuzzle
 {
-    public PuzzleResult Run1(string input)
+    public PuzzleResult Part1(string input)
     {
-        var navigator = new FloorNavigator(input);
-
-        return new PuzzleResult(navigator.DestinationFloor, "d8ee6b0475f3971b598eab03bf31bed4");
+        var destination = input.ToCharArray().Sum(FloorDelta);
+        return new PuzzleResult(destination, "d8ee6b0475f3971b598eab03bf31bed4");
     }
 
-    public PuzzleResult RunPart2(string input)
+    public PuzzleResult Part2(string input)
     {
-        var navigator = new FloorNavigator(input);
-            
-        return new PuzzleResult(navigator.FirstBasementInstruction, "eda1f9a68a0c771a5fdedc4228d1080c");
+        var instructions = input.ToCharArray();
+        var index = 1;
+        var destination = 0;
+        foreach (var c in instructions)
+        {
+            destination += FloorDelta(c);
+
+            if (destination < 0)
+                return new PuzzleResult(index, "eda1f9a68a0c771a5fdedc4228d1080c");
+
+            index++;
+        }
+
+        throw new Exception("Never entered the basement");
     }
+    
+    private static int FloorDelta(char c) => c switch
+    {
+        '(' => 1,
+        ')' => -1,
+        _ => 0
+    };
 }

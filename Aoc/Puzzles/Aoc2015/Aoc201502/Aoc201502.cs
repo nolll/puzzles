4edb6f4f6ef493ebce6d1dc040288a89ebaded1a
Henrik Severinson <henriks@gmail.com>
2025-04-12 10:48:51 +0200

    Refactored day 2 2015

--- Aoc/Puzzles/Aoc2015/Aoc201502/Aoc201502.cs
+++ Aoc/Puzzles/Aoc2015/Aoc201502/Aoc201502.cs
@@ -1,21 +1,46 @@
 ﻿using Pzl.Common;
+using Pzl.Tools.Numbers;
+using Pzl.Tools.Strings;
 
 namespace Pzl.Aoc.Puzzles.Aoc2015.Aoc201502;
 
 [Name("I Was Told There Would Be No Math")]
 public class Aoc201502 : AocPuzzle
 {
-    public PuzzleResult RunPart1(string input)
+    public PuzzleResult Part1(string input)
     {
-        var paperResult = GiftWrappingCalculator.GetRequiredPaper(input);
-
+        var paperResult = GetRequiredPaper(input);
         return new PuzzleResult(paperResult, "dfdf9c79dfad493d6417a9a284a9670b");
     }
 
-    public PuzzleResult RunPart2(string input)
+    public PuzzleResult Part2(string input)
     {
-        var ribbonResult = GiftWrappingCalculator.GetRequiredRibbon(input);
-            
+        var ribbonResult = GetRequiredRibbon(input);
         return new PuzzleResult(ribbonResult, "385d3372d91329e3166413c1cb3126d5");
+    }
+
+    public int GetRequiredPaper(string input) => 
+        input.Split(LineBreaks.Single).Sum(GetRequiredPaperForOneBox);
+
+    public int GetRequiredPaperForOneBox(string input)
+    {
+        var dimensions = Numbers.IntsFromString(input);
+        var sides = new List<int>
+        {
+            dimensions[0] * dimensions[1],
+            dimensions[0] * dimensions[2],
+            dimensions[1] * dimensions[2]
+        }.OrderBy(o => o).ToList();
+
+        return sides[0] * 3 + sides[1] * 2 + sides[2] * 2;
+    }
+
+    public int GetRequiredRibbon(string input) => 
+        input.Split(LineBreaks.Single).Sum(GetRequiredRibbonForOneBox);
+
+    public int GetRequiredRibbonForOneBox(string input)
+    {
+        var dimensions = Numbers.IntsFromString(input).OrderBy(o => o).ToList();
+        return dimensions[0] * 2 + dimensions[1] * 2 + dimensions[0] * dimensions[1] * dimensions[2];
     }
 }
--- Aoc/Puzzles/Aoc2015/Aoc201502/Aoc201502Tests.cs
+++ Aoc/Puzzles/Aoc2015/Aoc201502/Aoc201502Tests.cs
@@ -5,51 +5,26 @@ namespace Pzl.Aoc.Puzzles.Aoc2015.Aoc201502;
 
 public class Aoc201502Tests
 {
+    private const string MultipleInput = """
+                                         2x3x4
+                                         1x1x10
+                                         """;
+
     [TestCase("2x3x4", 58)]
     [TestCase("1x1x10", 43)]
-    public void CorrectSquareFeetForSingleGift(string input, int expected)
-    {
-        var result = GiftWrappingCalculator.GetRequiredPaperForOneBox(input);
-
-        result.Should().Be(expected);
-    }
+    public void CorrectSquareFeetForSingleGift(string input, int expected) => 
+        Sut.GetRequiredPaperForOneBox(input).Should().Be(expected);
 
     [Test]
-    public void CorrectSquareFeetForMultipleGifts()
-    {
-        const string input = """
-                             2x3x4
-                             1x1x10
-                             """;
-
-        const int expected = 101;
-
-        var result = GiftWrappingCalculator.GetRequiredPaper(input);
-
-        result.Should().Be(expected);
-    }
+    public void CorrectSquareFeetForMultipleGifts() => Sut.Part1(MultipleInput).Answer.Should().Be("101");
 
     [TestCase("2x3x4", 34)]
     [TestCase("1x1x10", 14)]
-    public void CorrectRibbonLength(string input, int expected)
-    {
-        var result = GiftWrappingCalculator.GetRequiredRibbonForOneBox(input);
-
-        result.Should().Be(expected);
-    }
+    public void CorrectRibbonLength(string input, int expected) => 
+        Sut.GetRequiredRibbonForOneBox(input).Should().Be(expected);
 
     [Test]
-    public void CorrectRibbonLengthForMultipleGifts()
-    {
-        const string input = """
-                             2x3x4
-                             1x1x10
-                             """;
+    public void CorrectRibbonLengthForMultipleGifts() => Sut.Part2(MultipleInput).Answer.Should().Be("48");
 
-        const int expected = 48;
-
-        var result = GiftWrappingCalculator.GetRequiredRibbon(input);
-
-        result.Should().Be(expected);
-    }
+    private static Aoc201502 Sut => new();
 }

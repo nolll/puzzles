df482e27a4a787188dfd7e445005e7f52c0bfe04
Henrik Severinson <henriks@gmail.com>
2025-09-03 20:12:29 +0200

    Everybody Codes Story 1, part 2-3 solved

--- Everybody/Puzzles/Ecs01/Ecs0102/Ecs0102.cs
+++ Everybody/Puzzles/Ecs01/Ecs0102/Ecs0102.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.Reflection.Metadata.Ecma335;
 using Pzl.Common;
 using Pzl.Tools.Strings;
 
@@ -8,130 +7,150 @@ namespace Pzl.Everybody.Puzzles.Ecs01.Ecs0102;
 [Name("Tangled Trees")]
 public class Ecs0102 : EverybodyStoryPuzzle
 {
-    public PuzzleResult Part1(string input)
+    public PuzzleResult Part1(string input) => new(Run(SimpleSwap, input), "d01fa4a99d218a85cc2d618feac1fc3a");
+    public PuzzleResult Part2(string input) => new(Run(SimpleSwap, input), "f9758c5ab09c84d7c166731aedc8beaa");
+    public PuzzleResult Part3(string input) => new(Run(FullSwap, input), "034d6deb943709125409c3ed3f859821");
+
+    private static string Run(Action<TreeNode, string> swap, string input)
     {
         var lines = input.Split(LineBreaks.Single);
         var root = new TreeNode("", "", 0);
-        var leftNodes = new List<TreeNode>();
-        var rightNodes = new List<TreeNode>();
         foreach (var line in lines)
         {
-            var parts = line.Split(' ');
-            var id = parts[1].Replace("id=", "");
-            var leftParts = parts[2].Replace("left=[", "").Replace("]", "").Split(',');
-            var rightParts = parts[3].Replace("right=[", "").Replace("]", "").Split(',');
-            var leftNode = new TreeNode(id, leftParts.Last(), int.Parse(leftParts.First()));
-            root.AddLeft(leftNode);
-            leftNodes.Add(leftNode);
-            var rightNode = new TreeNode(id, rightParts.Last(), int.Parse(rightParts.First()));
-            root.AddRight(rightNode);
-            rightNodes.Add(rightNode);
-        }
-
-        var l = leftNodes.GroupBy(o => o.Level).OrderByDescending(o => o.Count()).First().OrderBy(o => o.Score);
-        var r = rightNodes.GroupBy(o => o.Level).OrderByDescending(o => o.Count()).First().OrderBy(o => o.Score);
-        var ls = string.Join("", l.Select(o => o.Name));
-        var rs = string.Join("", r.Select(o => o.Name));
-        
-        return new PuzzleResult(ls + rs, "d01fa4a99d218a85cc2d618feac1fc3a");
-    }
-
-    public PuzzleResult Part2(string input)
-    {
-        var lines = input.Split(LineBreaks.Single);
-        var root = new TreeNode("", "", 0);
-        var lSet = new Dictionary<string, TreeNode>();
-        var rSet = new Dictionary<string, TreeNode>();
-        for (var i = 0; i < lines.Length; i++)
-        {
-            var line = lines[i];
-            var parts = line.Split(' ');
-            if (parts.Length == 2)
+            if (line.StartsWith("SWAP"))
             {
-                var swapId = parts[1];
-                var node1 = lSet[swapId];
-                var node2 = rSet[swapId];
-                var score1 = node1.Score;
-                var name1 = node1.Name;
-                var score2 = node2.Score;
-                var name2 = node2.Name;
-                node1.Score = score2;
-                node1.Name = name2;
-                node2.Score = score1;
-                node2.Name = name1;
+                swap(root, ParseSwapId(line));
                 continue;
             }
 
-            var id = parts[1].Replace("id=", "");
-            var leftParts = parts[2].Replace("left=[", "").Replace("]", "").Split(',');
-            var rightParts = parts[3].Replace("right=[", "").Replace("]", "").Split(',');
-            var leftNode = new TreeNode(id, leftParts.Last(), int.Parse(leftParts.First()));
+            var (leftNode, rightNode) = ParseNodes(line);
             root.AddLeft(leftNode);
-            lSet.Add(id, leftNode);
-            var rightNode = new TreeNode(id, rightParts.Last(), int.Parse(rightParts.First()));
             root.AddRight(rightNode);
-            rSet.Add(id, rightNode);
         }
-
-        var l = lSet.Values.GroupBy(o => o.Level).OrderByDescending(o => o.Count()).First().OrderBy(o => o.SortString);
-        var r = rSet.Values.GroupBy(o => o.Level).OrderByDescending(o => o.Count()).First().OrderBy(o => o.SortString);
-        var ls = string.Join("", l.Select(o => o.Name));
-        var rs = string.Join("", r.Select(o => o.Name));
         
-        return new PuzzleResult(ls + rs, "f9758c5ab09c84d7c166731aedc8beaa");
+        return GetCombinedMessage(root);
     }
 
-    public PuzzleResult Part3(string input)
+    private static void SimpleSwap(TreeNode root, string swapId)
     {
-        return new PuzzleResult(0);
+        var matchingNodes = GetAllNodes(root).Where(o => o.Id == swapId).ToArray();
+        var node1 = matchingNodes.First();
+        var node2 = matchingNodes.Last();
+        (node1.Score, node2.Score) = (node2.Score, node1.Score);
+        (node1.Name, node2.Name) = (node2.Name, node1.Name);
+    }
+    
+    private static void FullSwap(TreeNode root, string swapId)
+    {
+        var matchingNodes = GetAllNodes(root).Where(o => o.Id == swapId).ToArray();
+        var node1 = matchingNodes.First();
+        var node2 = matchingNodes.Last();
+        
+        var sort1 = node1.SortString;
+        var sort2 = node2.SortString;
+        var parent1 = node1.Parent!;
+        var parent2 = node2.Parent!;
+                
+        if(sort1 == "L")
+            parent1.SetLeft(node2);
+        else
+            parent1.SetRight(node2);
+                
+        if(sort2 == "L")
+            parent2.SetLeft(node1);
+        else
+            parent2.SetRight(node1);
+    }
+
+    private static (TreeNode leftNode, TreeNode rightNode) ParseNodes(string line)
+    {
+        var parts = line.Split(' ');
+        var id = parts[1].Replace("id=", "");
+        var leftParts = parts[2].Replace("left=[", "").Replace("]", "").Split(',');
+        var rightParts = parts[3].Replace("right=[", "").Replace("]", "").Split(',');
+        var leftNode = new TreeNode(id, leftParts.Last(), int.Parse(leftParts.First()));
+        var rightNode = new TreeNode(id, rightParts.Last(), int.Parse(rightParts.First()));
+        return (leftNode, rightNode);
+    }
+    
+    private static string ParseSwapId(string line) => line.Split(' ')[1];
+
+    private static IEnumerable<TreeNode> GetAllNodes(TreeNode rootLeft)
+    {
+        var q = new Queue<TreeNode>();
+        q.Enqueue(rootLeft);
+
+        while (q.Any())
+        {
+            var n = q.Dequeue();
+            yield return n;
+            
+            if(n.Left is not null)
+                q.Enqueue(n.Left);
+            
+            if(n.Right is not null)
+                q.Enqueue(n.Right);
+        }
+    }
+
+    private static string GetCombinedMessage(TreeNode node) => GetMessage(node.Left) + GetMessage(node.Right);
+
+    private static string GetMessage(TreeNode? node)
+    {
+        if (node is null) 
+            return "";
+        
+        var nodeNames = GetAllNodes(node).GroupBy(o => o.Level).OrderByDescending(o => o.Count()).ThenBy(o => o.Key)
+            .First()
+            .OrderBy(o => o.SortOrder)
+            .Select(o => o.Name);
+
+        return string.Join("", nodeNames);
     }
 }
 
 [DebuggerDisplay("{Score},{Name}")]
 public class TreeNode(string id, string name, int score)
 {
-    private TreeNode? _left;
-    private TreeNode? _right;
-
+    public TreeNode? Left { get; private set; }
+    public TreeNode? Right { get; private set; }
+    public TreeNode? Parent { get; private set; }
+    
     public string Id { get; } = id;
     public string Name { get; set; } = name;
     public int Score { get; set; } = score;
-    public int Level { get; set; }
-    public TreeNode? Parent { get; set; }
-    public int SortOrder { get; set; }
-    public string SortString { get; set; } = "";
+    public int Level => Parent?.Level + 1 ?? 0;
+    public string SortOrder => (Parent?.SortOrder ?? "") + SortString;
+    public string SortString { get; private set; } = "";
 
     public void AddLeft(TreeNode node)
     {
-        if (_left == null)
-        {
-            _left = node;
-            node.Parent = this;
-            node.Level = Level + 1;
-            node.SortString = SortString + "L";
-            node.SortOrder = SortOrder - 1;
-        }
+        if (Left == null)
+            SetLeft(node);
         else
-        {
-            node.SortOrder -= 1;
-            _left.Add(node);
-        }
+            Left.Add(node);
     }
     
     public void AddRight(TreeNode node)
     {
-        if (_right == null)
-        {
-            _right = node;
-            node.Parent = this;
-            node.Level = Level + 1;
-            node.SortString = SortString + "R";
-            node.SortOrder = SortOrder + 1;
-        }
+        if (Right == null)
+            SetRight(node);
         else
-        {
-            _right.Add(node);
-        }
+            Right.Add(node);
+    }
+
+    public void SetLeft(TreeNode node)
+    {
+        Left = node;
+        node.Parent = this;
+        node.SortString = "L";
+    }
+    
+    public void SetRight(TreeNode node)
+    {
+        Right = node;
+        node.Parent = this;
+        node.SortString = "R";
     }
 
     private void Add(TreeNode node)

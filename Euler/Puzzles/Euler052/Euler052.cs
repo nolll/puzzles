71c1612ffc82f9902ebb96ea67926c46dabbe32e
Henrik Severinson <henriks@gmail.com>
2025-09-29 18:46:36 +0200

    Euler 52 solved

---
+++ Euler/Puzzles/Euler052/Euler052.cs
@@ -0,0 +1,36 @@
+using Pzl.Common;
+using Pzl.Tools.Numbers;
+
+namespace Pzl.Euler.Puzzles.Euler052;
+
+[Name("Permuted Multiples")]
+public class Euler052 : EulerPuzzle
+{
+    public PuzzleResult Run(string input)
+    {
+        var n = 1;
+
+        while (true)
+        {
+            var digits = GetDigits(n);
+            var isValid = true;
+            
+            for (var m = 2; m <= 6; m++)
+            {
+                var c = n * m;
+                if (GetDigits(c) == digits)
+                    continue;
+                
+                isValid = false;
+                break;
+            }
+
+            if (isValid)
+                return new PuzzleResult(n, "7e25d491fc51dd8d228120bc98f3eb50");
+
+            n++;
+        }
+    }
+
+    private static string GetDigits(int n) => string.Join("", n.ToString().ToCharArray().Order());
+}

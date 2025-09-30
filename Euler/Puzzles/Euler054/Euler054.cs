84b2e1f1be46c9e07717e3100121381e6cfe3f7c
Henrik Severinson <henriks@gmail.com>
2025-09-30 23:08:30 +0200

    Almost done. Needs some more str8 comparison

--- Euler/Puzzles/Euler054/Euler054.cs
+++ Euler/Puzzles/Euler054/Euler054.cs
@@ -1,4 +1,5 @@
 using Pzl.Common;
+using Pzl.Tools.Debug;
 using Pzl.Tools.Strings;
 
 namespace Pzl.Euler.Puzzles.Euler054;
@@ -12,12 +13,27 @@ public class Euler054 : EulerPuzzle
         foreach (var line in lines)
         {
             var cards = line.Split();
-            var h1 = cards.Take(5);
-            var h2 = cards.Skip(5);
+            var h1 = new Hand(cards.Take(5));
+            var h2 = new Hand(cards.Skip(5));
+
+            var comparison = h1.CompareTo(h2);
+            var result = GetWinDescription(comparison);
+            
+            DebugPrinter.Print(string.Join(" ", h1.Cards.Select(o => o.ToString())), h1.Rank.ToString());
+            DebugPrinter.Print(string.Join(" ", h2.Cards.Select(o => o.ToString())), h2.Rank.ToString());
+            DebugPrinter.Print(result);
+            Console.WriteLine();
         }
 
         return new PuzzleResult(0);
     }
+
+    private string GetWinDescription(int comparison) => comparison switch
+    {
+        1 => "P1 Wins",
+        -1 => "P2 Wins",
+        _ => "Draw"
+    };
 
     public class Card
     {
@@ -36,7 +52,7 @@ public class Euler054 : EulerPuzzle
         {
         }
 
-        private int GetRank(char value) => value switch
+        private static int GetRank(char value) => value switch
         {
             'A' => 14,
             'K' => 13,
@@ -45,86 +61,138 @@ public class Euler054 : EulerPuzzle
             'T' => 10,
             _ => int.Parse(value.ToString())
         };
+
+        public override string ToString() => $"{Value}{Suit}";
     }
 
+    public enum HandRank
+    {
+        Str8Flush = 9,
+        Quads = 8,
+        Boat = 7,
+        Flush = 6,
+        Str8 = 5,
+        Trips = 4,
+        TwoPair = 3,
+        OnePair = 2,
+        HighCard = 1
+    }
+    
     public class Hand
     {
+        private int[] _rankComparer = [];
+        
+        public HandRank Rank { get; private set; }
+        
         public Hand(Card[] cards)
         {
             Cards = cards;
+            EvaluateHand(cards);
         }
         
-        public Hand(string cards) : this(cards.Split().Select(o => new Card(o)).ToArray())
+        public Hand(string cards) : this(cards.Split())
+        {
+        }
+        
+        public Hand(IEnumerable<string> cards) : this(cards.Select(o => new Card(o)).ToArray())
         {
         }
 
         public Card[] Cards { get; }
 
-        public bool IsStr8Flush => IsFlush && IsStr8;
-
-        public bool IsQuads
+        private static bool IsStr8(Card[] cards)
         {
-            get
-            {
-                var groups = Cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ToList();
-                return groups.Count == 2 && groups.First().Count() == 4;
-            }
+            var sorted = cards.OrderByDescending(o => o.Rank).ToArray();
+            
+            return sorted[0].Rank - 1 == sorted[1].Rank &&
+                   sorted[1].Rank - 1 == sorted[2].Rank &&
+                   sorted[2].Rank - 1 == sorted[3].Rank &&
+                   sorted[3].Rank - 1 == sorted[4].Rank ||
+                   sorted[0].Rank == 14 &&
+                   sorted[1].Rank == 5 &&
+                   sorted[2].Rank == 4 &&
+                   sorted[3].Rank == 3 &&
+                   sorted[4].Rank == 2;
         }
 
-        public bool IsBoat
+        public int CompareTo(Hand other)
         {
-            get
+            if (other.Rank > Rank)
+                return 1;
+            if (other.Rank < Rank)
+                return -1;
+
+            for (var i = 0; i < _rankComparer.Length; i++)
             {
-                var groups = Cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ToList();
-                return groups.Count == 2 && groups.First().Count() == 3 && groups.Last().Count() == 2;
+                if (other._rankComparer[i] > _rankComparer[i])
+                    return 1;
+                if (other._rankComparer[i] < _rankComparer[i])
+                    return 1;
             }
+            
+            return 0;
         }
-
-        public bool IsFlush => Cards.Select(o => o.Suit).ToHashSet().Count == 1;
-
-        public bool IsStr8
+        
+        private void EvaluateHand(Card[] cards)
         {
-            get
+            var rankGroups = cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ThenByDescending(o => o.First().Rank).ToList();
+            var rankGroupCount = rankGroups.Count;
+            var largestGroupSize = rankGroups.First().Count();
+            _rankComparer = rankGroups.Select(o => o.First().Rank).ToArray();
+            
+            if (rankGroupCount == 2 && largestGroupSize == 4)
             {
-                var sorted = Cards.OrderByDescending(o => o.Rank).ToArray();
-                
-                return sorted[0].Rank - 1 == sorted[1].Rank &&
-                       sorted[1].Rank - 1 == sorted[2].Rank &&
-                       sorted[2].Rank - 1 == sorted[3].Rank &&
-                       sorted[3].Rank - 1 == sorted[4].Rank ||
-                       sorted[0].Rank == 14 &&
-                       sorted[1].Rank == 5 &&
-                       sorted[2].Rank == 4 &&
-                       sorted[3].Rank == 3 &&
-                       sorted[4].Rank == 2;
+                Rank = HandRank.Quads;
+                return;
             }
-        }
+            
+            if (rankGroupCount == 2 && largestGroupSize == 3)
+            {
+                Rank = HandRank.Boat;
+                return;
+            }
 
-        public bool IsTrips
-        {
-            get
+            if (rankGroupCount == 3 && largestGroupSize == 3)
             {
-                var groups = Cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ToList();
-                return groups.Count == 3 && groups.First().Count() == 3;
+                Rank = HandRank.Trips;
+                return;
             }
-        }
 
-        public bool Is2Pair
-        {
-            get
+            if (rankGroupCount == 3 && largestGroupSize == 2)
             {
-                var groups = Cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ToList();
-                return groups.Count == 3 && groups.First().Count() == 2;
+                Rank = HandRank.TwoPair;
+                return;
             }
-        }
 
-        public bool IsPair
-        {
-            get
+            if (rankGroupCount == 4 && largestGroupSize == 2)
             {
-                var groups = Cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ToList();
-                return groups.Count == 4 && groups.First().Count() == 2;
+                Rank = HandRank.OnePair;
+                return;
             }
+            
+            var suitGroups = cards.GroupBy(o => o.Suit);
+            var isFlush = suitGroups.Count() == 1;
+            var isStr8 = IsStr8(cards);
+            
+            if (isStr8 && isFlush)
+            {
+                Rank = HandRank.Str8Flush;
+                return;
+            }
+            
+            if (isFlush)
+            {
+                Rank = HandRank.Flush;
+                return;
+            }
+
+            if (isStr8)
+            {
+                Rank = HandRank.Str8;
+                return;
+            }
+
+            Rank = HandRank.HighCard;
         }
     }
 }

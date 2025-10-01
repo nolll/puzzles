40c4187cc6fb556df6a5a40e61fbe295dfdedf0e
Henrik S <henriks@gmail.com>
2025-10-01 20:28:55 +0200

    Euler 54 solved
    * wip euler 54
    
    * Almost done. Needs some more str8 comparison
    
    * Euler 54 solved

---
+++ Euler/Puzzles/Euler054/Euler054.cs
@@ -0,0 +1,162 @@
+using Pzl.Common;
+using Pzl.Tools.Debug;
+using Pzl.Tools.Strings;
+
+namespace Pzl.Euler.Puzzles.Euler054;
+
+[Name("Poker Hands")]
+public class Euler054 : EulerPuzzle
+{
+    public PuzzleResult Run(string input)
+    {
+        var lines = input.Split(LineBreaks.Single);
+        var p1WinCount = 0;
+        foreach (var line in lines)
+        {
+            var cards = line.Split();
+            var h1 = new Hand(cards.Take(5));
+            var h2 = new Hand(cards.Skip(5));
+
+            var comparison = h1.CompareTo(h2);
+            if (comparison > 0)
+                p1WinCount++;
+        }
+
+        return new PuzzleResult(p1WinCount, "8d522a87a80f6f36a9599b3158b73a2b");
+    }
+
+    private class Card(char suit, char value)
+    {
+        public char Suit { get; } = suit;
+        public char Value { get; } = value;
+        public int Rank { get; } = GetRank(value);
+
+        public Card(string id) : this(id[1], id[0]) {}
+
+        private static int GetRank(char value) => value switch
+        {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            _ => int.Parse(value.ToString())
+        };
+
+        public override string ToString() => $"{Value}{Suit}";
+    }
+
+    public enum HandRank
+    {
+        Str8Flush = 9,
+        Quads = 8,
+        Boat = 7,
+        Flush = 6,
+        Str8 = 5,
+        Trips = 4,
+        TwoPair = 3,
+        OnePair = 2,
+        HighCard = 1
+    }
+    
+    public class Hand
+    {
+        private int[] _rankComparer = [];
+        
+        public HandRank Rank { get; private set; }
+
+        private Hand(Card[] cards)
+        {
+            EvaluateHand(cards);
+        }
+        
+        public Hand(string cards) : this(cards.Split()) {}
+        
+        public Hand(IEnumerable<string> cards) : this(cards.Select(o => new Card(o)).ToArray()) {}
+
+        public int CompareTo(Hand other)
+        {
+            if (other.Rank > Rank)
+                return -1;
+            if (other.Rank < Rank)
+                return 1;
+
+            for (var i = 0; i < _rankComparer.Length; i++)
+            {
+                if (other._rankComparer[i] > _rankComparer[i])
+                    return -1;
+                if (other._rankComparer[i] < _rankComparer[i])
+                    return 1;
+            }
+            
+            return 0;
+        }
+        
+        private void EvaluateHand(Card[] cards)
+        {
+            var rankGroups = cards.GroupBy(o => o.Value).OrderByDescending(o => o.Count()).ThenByDescending(o => o.First().Rank).ToList();
+            var rankGroupCount = rankGroups.Count;
+            var largestGroupSize = rankGroups.First().Count();
+            _rankComparer = rankGroups.Select(o => o.First().Rank).ToArray();
+            
+            switch (rankGroupCount)
+            {
+                case 2 when largestGroupSize == 4:
+                    Rank = HandRank.Quads;
+                    return;
+                case 2 when largestGroupSize == 3:
+                    Rank = HandRank.Boat;
+                    return;
+                case 3 when largestGroupSize == 3:
+                    Rank = HandRank.Trips;
+                    return;
+                case 3 when largestGroupSize == 2:
+                    Rank = HandRank.TwoPair;
+                    return;
+                case 4 when largestGroupSize == 2:
+                    Rank = HandRank.OnePair;
+                    return;
+            }
+
+            var suitGroups = cards.GroupBy(o => o.Suit);
+            var isFlush = suitGroups.Count() == 1;
+            
+            var sorted = cards.OrderByDescending(o => o.Rank).ToArray();
+
+            var isWheel = sorted[0].Rank == 14 &&
+                          sorted[1].Rank == 5 &&
+                          sorted[2].Rank == 4 &&
+                          sorted[3].Rank == 3 &&
+                          sorted[4].Rank == 2;
+            
+            var isStr8 = isWheel || 
+                         sorted[0].Rank - 1 == sorted[1].Rank &&
+                         sorted[1].Rank - 1 == sorted[2].Rank &&
+                         sorted[2].Rank - 1 == sorted[3].Rank &&
+                         sorted[3].Rank - 1 == sorted[4].Rank;
+                          
+            if (isWheel)
+                _rankComparer = [5, 4, 3, 2, 1];
+            
+            if (isStr8 && isFlush)
+            {
+                Rank = HandRank.Str8Flush;
+                return;
+            }
+            
+            if (isFlush)
+            {
+                Rank = HandRank.Flush;
+                return;
+            }
+
+            if (isStr8)
+            {
+                Rank = HandRank.Str8;
+                return;
+            }
+
+            Rank = HandRank.HighCard;
+        }
+    }
+}
---
+++ Euler/Puzzles/Euler054/Euler054Tests.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Pzl.Euler.Puzzles.Euler054;
+
+public class Euler054Tests
+{
+    [TestCase("AH KH QH JH TH", Euler054.HandRank.Str8Flush)]
+    [TestCase("AH 2H 3H 4H 5H", Euler054.HandRank.Str8Flush)]
+    [TestCase("AH AS AD AC KS", Euler054.HandRank.Quads)]
+    [TestCase("AH AS AD KC KS", Euler054.HandRank.Boat)]
+    [TestCase("AH KC QS JD TH", Euler054.HandRank.Str8)]
+    [TestCase("AH 2C 3S 4D 5H", Euler054.HandRank.Str8)]
+    [TestCase("AH AS AD KC QS", Euler054.HandRank.Trips)]
+    [TestCase("AH AS KD KC QS", Euler054.HandRank.TwoPair)]
+    [TestCase("AH AS KD QC JS", Euler054.HandRank.OnePair)]
+    [TestCase("AH KS QD JC 9S", Euler054.HandRank.HighCard)]
+    public void IsParsedCorrect(string hand, Euler054.HandRank expected) => new Euler054.Hand(hand).Rank.Should().Be(expected);
+    
+    [TestCase("2H 3H 4H 5H 6H", "AH 2H 3H 4H 5H")]
+    [TestCase("AH KH QH JH TH", "2H 3H 4H 5H 6H")]
+    [TestCase("AH AS AD AC KS", "AH AS AD AC QS")]
+    [TestCase("AH AS AD AC KS", "KH KS KD KC AS")]
+    [TestCase("AH AS AD KC KS", "AH AS AD QC QS")]
+    [TestCase("AH AS AD QC QS", "KH KS KD QC QS")]
+    [TestCase("3S 4D 5H 6C 7D", "2S 3D 4H 5C 6D")]
+    [TestCase("AH AS AD QC JS", "KH KS KD QC JS")]
+    [TestCase("AH AS AD KC QS", "AH AS AD KC JS")]
+    [TestCase("AH AS KD KC QS", "AH AS QD QC JS")]
+    [TestCase("AH AS KD KC QS", "AH AS KD KC JS")]
+    [TestCase("AH AS QD JC 9S", "AH AS QD JC 8S")]
+    [TestCase("AH AS QD JC 9S", "KH KS QD JC 9S")]
+    [TestCase("AH KS QD JC 9S", "AH KS QD JC 8S")]
+    public void CompareHands_LeftHandWins(string left, string right) => new Euler054.Hand(left).CompareTo(new Euler054.Hand(right)).Should().Be(1);
+}

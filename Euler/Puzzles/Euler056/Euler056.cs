9c47bcf07f4084430c2388b2b3010bfdbdec3318
Henrik Severinson <henriks@gmail.com>
2025-10-03 10:10:40 +0200

    Euler 56 solved

---
+++ Euler/Puzzles/Euler056/Euler056.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+using Pzl.Common;
+using Pzl.Tools.Strings;
+
+namespace Pzl.Euler.Puzzles.Euler056;
+
+[Name("Powerful Digit Sum")]
+public class Euler056 : EulerPuzzle
+{
+    public PuzzleResult Run(string input)
+    {
+        const int limit = 100;
+        var best = 0;
+        
+        for (var n = 0; n < limit; n++)
+        {
+            for (var exp = 0; exp < limit; exp++)
+            {
+                var sum = BigInteger.Pow(new BigInteger(n), exp).ToString().Select(o => int.Parse(o.ToString())).Sum();
+                best = Math.Max(best, sum);
+            }
+        }
+        
+        return new PuzzleResult(best, "376414390a4252665a1c96ce1464a605");
+    }
+}

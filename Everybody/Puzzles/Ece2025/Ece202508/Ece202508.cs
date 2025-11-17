ae44887ba0efb5e9f074f6397692b512529b2600
Henrik Severinson <henriks@gmail.com>
2025-11-17 11:33:28 +0100

    Everybody codes 2025 day 8 part 2 and 3 solved

--- Everybody/Puzzles/Ece2025/Ece202508/Ece202508.cs
+++ Everybody/Puzzles/Ece2025/Ece202508/Ece202508.cs
@@ -7,19 +7,68 @@ public class Ece202508 : EverybodyEventPuzzle
 {
     public PuzzleResult Part1(string input) => new(Part1(input, 32), "13e1a785ec9325ce343a99dda7a6745f");
 
-    public int Part1(string input, int nailCount)
-    {
-        var instructions = input.Split(',').Select(int.Parse).ToArray();
-        return instructions.Zip(instructions.Skip(1)).Count(o => Math.Abs(o.Second - o.First) == nailCount / 2);
-    }
+    public int Part1(string input, int nailCount) => 
+        Parse(input).Zip(Parse(input).Skip(1)).Count(o => Math.Abs(o.Second - o.First) == nailCount / 2);
 
     public PuzzleResult Part2(string input)
     {
-        return new PuzzleResult(0);
+        var numbers = Parse(input);
+        List<(int, int)> strings = [];
+        var knotCount = 0;
+        foreach (var (x, y) in numbers.Zip(numbers.Skip(1)))
+        {
+            foreach (var (a, b) in strings)
+            {
+                if (x == a || x == b || y == a || y == b)
+                    continue;
+
+                
+                if ((x > a && x < b) != (y > a && y < b))
+                    knotCount++;
+            }
+            
+            strings.Add((Math.Min(x, y), Math.Max(x, y)));
+        }
+        
+        return new PuzzleResult(knotCount, "b5e871831bb11b7eddb421c81e170748");
     }
 
-    public PuzzleResult Part3(string input)
+    public PuzzleResult Part3(string input) => new(Part3(input, 256), "2681ae91ee4e40ef531ab7d2cdd31f8b");
+
+    public int Part3(string input, int nailCount)
     {
-        return new PuzzleResult(0);
+        var numbers = Parse(input);
+        List<(int, int)> strings = [];
+        foreach (var (x, y) in numbers.Zip(numbers.Skip(1)))
+        {
+            strings.Add((Math.Min(x, y), Math.Max(x, y)));
+        }
+
+        var best = 0;
+
+        for (var x = 1; x <= nailCount; x++)
+        {
+            for (var y = x + 1; y <= nailCount; y++)
+            {
+                var intersectionCount = 0;
+                foreach (var (a, b) in strings)
+                {
+                    if (x == a && y == b || y == a && x == b)
+                        intersectionCount++;
+                    
+                    if (x == a || x == b || y == a || y == b)
+                        continue;
+                    
+                    if ((x > a && x < b) != (y > a && y < b))
+                        intersectionCount++;
+                }
+
+                best = Math.Max(best, intersectionCount);
+            }
+        }
+        
+        return best;
     }
+    
+    private static int[] Parse(string input) => input.Split(',').Select(int.Parse).ToArray();
 }
--- Everybody/Puzzles/Ece2025/Ece202508/Ece202508Tests.cs
+++ Everybody/Puzzles/Ece2025/Ece202508/Ece202508Tests.cs
@@ -13,17 +13,17 @@ public class Ece202508Tests
     [Fact]
     public void Part2()
     {
-        const string input = "";
+        const string input = "1,5,2,6,8,4,1,7,3,5,7,8,2";
 
-        Sut.Part2(input).Answer.Should().Be("0");
+        Sut.Part2(input).Answer.Should().Be("21");
     }
 
     [Fact]
     public void Part3()
     {
-        const string input = "";
+        const string input = "1,5,2,6,8,4,1,7,3,6";
 
-        Sut.Part3(input).Answer.Should().Be("0");
+        Sut.Part3(input, 8).Should().Be(7);
     }
 
     private static Ece202508 Sut => new();

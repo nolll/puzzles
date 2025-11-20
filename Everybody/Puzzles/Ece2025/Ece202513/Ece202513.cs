d5eefe972ec5360f3df94c98566bdce0c01072e2
Henrik Severinson <henriks@gmail.com>
2025-11-20 15:34:48 +0100

    Everybody codes 2025 day 13 refactor

--- Everybody/Puzzles/Ece2025/Ece202513/Ece202513.cs
+++ Everybody/Puzzles/Ece2025/Ece202513/Ece202513.cs
@@ -1,57 +1,18 @@
 using Pzl.Common;
-using Pzl.Tools.Lists;
 using Pzl.Tools.Strings;
 
 namespace Pzl.Everybody.Puzzles.Ece2025.Ece202513;
 
-[Name("")]
+[Name("Unlocking the Mountain")]
 public class Ece202513 : EverybodyEventPuzzle
 {
-    public PuzzleResult Part1(string input)
-    {
-        var inputNumbers = input.Split(LineBreaks.Single).Select(int.Parse).ToList();
-        List<int> numbers = [1];
-        var startPos = 0;
-        for (var i = 0; i < inputNumbers.Count; i++)
-        {
-            if(i % 2 == 0)
-            {
-                numbers.Add(inputNumbers[i]);
-            }
-            else
-            {
-                numbers.Insert(0, inputNumbers[i]);
-                startPos++;
-            }
-        }
-        
-        var result = (startPos + 2025) % numbers.Count;
-        
-        return new PuzzleResult(numbers[result], "18ccf03875ad98259b4de347203fb45a");
-    }
+    public PuzzleResult Part1(string input) => new(Solve(input, 2025), "18ccf03875ad98259b4de347203fb45a");
+    public PuzzleResult Part2(string input) => new(Solve(input, 20252025), "120dc70bb2265cf1dd44b351a776f4eb");
+    public PuzzleResult Part3(string input) => new(Solve(input, 202520252025), "ad5947829c2a32c895e2406a54daa389");
 
-    public PuzzleResult Part2(string input) => new(Part2And3(input, 20252025), "120dc70bb2265cf1dd44b351a776f4eb");
-    public PuzzleResult Part3(string input) => new(Part2And3(input, 202520252025), "ad5947829c2a32c895e2406a54daa389");
-
-    private int Part2And3(string input, long steps)
+    private static int Solve(string input, long steps)
     {
-        var inputRanges = input.Split(LineBreaks.Single);
-        List<Interval> intervals = [new(1, 1)];
-        var startPos = 0;
-        for (var i = 0; i < inputRanges.Length; i++)
-        {
-            var (lower, upper) = inputRanges[i].Split('-').Select(int.Parse).ToArray();
-            if(i % 2 == 0)
-            {
-                intervals.Add(new Interval(lower, upper));
-            }
-            else
-            {
-                var interval = new Interval(upper, lower);
-                intervals.Insert(0, interval);
-                startPos += interval.Length;
-            }
-        }
+        var (intervals, startPos) = ParseDial(input);
         
         var target = (int)((startPos + steps) % intervals.Sum(o => o.Length));
         var pos = 0;
@@ -65,6 +26,37 @@ public class Ece202513 : EverybodyEventPuzzle
             pos += intervals[index].Length;
             index++;
         }
+    }
+
+    private static (List<Interval> intervals, int startPos) ParseDial(string input)
+    {
+        var inputRanges = input.Split(LineBreaks.Single).Select(ParseRange).ToArray();
+        List<Interval> intervals = [new(1, 1)];
+        var startPos = 0;
+        var clockwise = true;
+        foreach (var (lower, upper) in inputRanges)
+        {
+            if(clockwise)
+            {
+                intervals.Add(new Interval(lower, upper));
+            }
+            else
+            {
+                var interval = new Interval(upper, lower);
+                intervals.Insert(0, interval);
+                startPos += interval.Length;
+            }
+
+            clockwise = !clockwise;
+        }
+
+        return (intervals, startPos);
+    }
+
+    private static (int lower, int upper) ParseRange(string s)
+    {
+        var parts = s.Split('-').Select(int.Parse).ToArray();
+        return parts.Length > 1 ? (parts[0], parts[1]) : (parts[0], parts[0]);
     }
 
     private class Interval(int start, int end)

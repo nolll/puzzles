2a81a30d1805a5832586043393a6ab9ffcbe19e8
Henrik Severinson <henriks@gmail.com>
2025-11-24 22:47:43 +0100

    Aoc 2016 day 23 from 98s to 97s! Yay

--- Aoc/Puzzles/Aoc2016/Aoc201623/Aoc201623.cs
+++ Aoc/Puzzles/Aoc2016/Aoc201623/Aoc201623.cs
@@ -2,21 +2,20 @@ using Pzl.Common;
 
 namespace Pzl.Aoc.Puzzles.Aoc2016.Aoc201623;
 
-[IsSlow] // 132s for part 2
+[IsSlow] // 98s for part 2
 [Name("Safe Cracking")]
 [Comment("Factorial of 12")]
 public class Aoc201623 : AocPuzzle
 {
     public PuzzleResult RunPart1(string input)
     {
-        var computer = new SafeCrackingComputerPart1(input, 7, 0);
+        var computer = new SafeCrackingComputerPart1(input, 7, 0); // 7! + 7708
         return new PuzzleResult(computer.ValueA, "11e66781d74c9188561ba3937d053d99");
     }
 
     public PuzzleResult RunPart2(string input)
     {
-        // By inspecting output from the computer I realized that it is calculating the factorial of 12
-        var computer = new SafeCrackingComputerPart2(input, 12, 0);
+        var computer = new SafeCrackingComputerPart2(input, 12, 0); // 12! + 7708
         return new PuzzleResult(computer.ValueA, "eb0c83e21e8bd77e7f0b5686e8f1c31a");
     }
 }
--- Aoc/Puzzles/Aoc2016/Aoc201623/SafeCrackingComputerPart2.cs
+++ Aoc/Puzzles/Aoc2016/Aoc201623/SafeCrackingComputerPart2.cs
@@ -20,80 +20,72 @@ public class SafeCrackingComputerPart2
             ['d'] = 0
         };
         _index = 0;
-
+        
         while (_index < instructions.Length)
         {
             var s = instructions[_index];
             var command = s.Name;
-            try
+            if (command == "cpy")
             {
-                if (command == "cpy")
-                {
-                    var value = s.Args[0];
-                    var target = s.Args[1].First();
-                    _registers[target] = int.TryParse(value, out var num) 
-                        ? num 
-                        : _registers[value.First()];
+                var value = s.Args[0];
+                var target = s.Args[1][0];
+                _registers[target] = int.TryParse(value, out var num) 
+                    ? num 
+                    : _registers[value.First()];
 
-                    IncrementIndex();
+                IncrementIndex();
+            }
+
+            else if (command == "inc")
+            {
+                var target = s.Args[0][0];
+                _registers[target]++;
+                IncrementIndex();
+            }
+
+            else if (command == "dec")
+            {
+                var target = s.Args[0][0];
+                _registers[target]--;
+                IncrementIndex();
+            }
+
+            else if (command == "jnz")
+            {
+                var value = s.Args[0];
+                var isInt = int.TryParse(s.Args[1], out var steps);
+                steps = isInt ? steps : _registers[s.Args[1][0]];
+
+                if (int.TryParse(value, out var num))
+                {
+                    IncrementIndex(num != 0 ? steps : 1);
                 }
-
-                else if (command == "inc")
+                else
                 {
-                    var target = s.Args[0].First();
-                    _registers[target]++;
-                    IncrementIndex();
+                    IncrementIndex(_registers[value.First()] != 0 ? steps : 1);
                 }
+            }
 
-                else if (command == "dec")
+            else if (command == "tgl")
+            {
+                var indexToToggle = _index + _registers[s.Args[0][0]];
+                if (indexToToggle >= 0 && indexToToggle < instructions.Length)
                 {
-                    var target = s.Args[0].First();
-                    _registers[target]--;
-                    IncrementIndex();
-                }
-
-                else if (command == "jnz")
-                {
-                    var value = s.Args[0];
-                    var isInt = int.TryParse(s.Args[1], out var steps);
-                    steps = isInt ? steps : _registers[s.Args[1].First()];
-
-                    if (int.TryParse(value, out var num))
+                    var instructionToToggle = instructions[indexToToggle];
+                    if (instructionToToggle.Args.Count == 1)
                     {
-                        IncrementIndex(num != 0 ? steps : 1);
+                        instructionToToggle.Name = instructionToToggle.Name == "inc" 
+                            ? "dec" 
+                            : "inc";
                     }
                     else
                     {
-                        IncrementIndex(_registers[value.First()] != 0 ? steps : 1);
+                        instructionToToggle.Name = instructionToToggle.Name == "jnz" 
+                            ? "cpy" 
+                            : "jnz";
                     }
                 }
 
-                else if (command == "tgl")
-                {
-                    var indexToToggle = _index + _registers[s.Args[0].First()];
-                    if (indexToToggle >= 0 && indexToToggle < instructions.Length)
-                    {
-                        var instructionToToggle = instructions[indexToToggle];
-                        var name = instructionToToggle.Name;
-                        if (instructionToToggle.Args.Count == 1)
-                        {
-                            instructionToToggle.Name = name == "inc" 
-                                ? "dec" 
-                                : "inc";
-                        }
-                        else
-                        {
-                            instructionToToggle.Name = name == "jnz" 
-                                ? "cpy" 
-                                : "jnz";
-                        }
-                    }
-
-                    IncrementIndex();
-                }
-            }
-            catch
-            {
                 IncrementIndex();
             }
         }

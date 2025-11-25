d01bac5d3eeea78376402c1d78f05f9cfbb2655e
Henrik Severinson <henriks@gmail.com>
2025-11-25 18:08:52 +0100

    Everybody codes 2025 day 16 part 3 solved

--- Everybody/Puzzles/Ece2025/Ece202516/Ece202516.cs
+++ Everybody/Puzzles/Ece2025/Ece202516/Ece202516.cs
@@ -8,27 +8,25 @@ public class Ece202516 : EverybodyEventPuzzle
 {
     public PuzzleResult Part1(string input)
     {
-        var numbers = Numbers.IntsFromString(input);
-        var blocks = new List<List<int>>();
-        for (var i = 0; i < 90; i++)
-        {
-            blocks.Add([]);
-        }
-        for (var i = 1; i <= 90; i++)
-        {
-            foreach (var number in numbers)
-            {
-                if (i % number == 0)
-                {
-                    blocks[i - 1].Add(number);
-                }
-            }
-        }
-        
-        return new PuzzleResult(blocks.Select(o => o.Count).Sum(), "c43ffe414cd1f0d7024f9014c035703a");
+        var spell = Numbers.IntsFromString(input).ToList();
+        var blockCount = CountRequiredBlocks(spell, 90);
+
+        return new PuzzleResult(blockCount, "c43ffe414cd1f0d7024f9014c035703a");
     }
 
     public PuzzleResult Part2(string input)
+    {
+        var spell = GetSpell(input);
+        var product = spell.Aggregate(1L, (current, b) => current * b);
+
+        return new PuzzleResult(product, "d543f8d5b11ac6d4e21685cd8674fc6f");
+    }
+
+    public PuzzleResult Part3(string input) => new(Part3(input, 202_520_252_025_000), "b91ced40f2ab99acef778dbb6b61aeee");
+
+    public long Part3(string input, long blocksAvailable) => BinarySearch(GetSpell(input), blocksAvailable);
+
+    private static List<int> GetSpell(string input)
     {
         var wall = Numbers.IntsFromString(input);
 
@@ -50,13 +48,28 @@ public class Ece202516 : EverybodyEventPuzzle
             block++;
         }
 
-        var product = spell.Aggregate(1L, (current, b) => current * b);
-
-        return new PuzzleResult(product, "d543f8d5b11ac6d4e21685cd8674fc6f");
+        return spell;
     }
 
-    public PuzzleResult Part3(string input)
+    private static long BinarySearch(List<int> spell, long blocksAvailable)
     {
-        return new PuzzleResult(0);
+        var low = 0L;
+        var high = blocksAvailable;
+        
+        while (low <= high) {
+            var mid = low + (high - low) / 2;
+            var requiredBlocks = CountRequiredBlocks(spell, mid);
+            
+            if (requiredBlocks == blocksAvailable)
+                return mid;
+            if (requiredBlocks < blocksAvailable)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return high;
     }
+
+    private static long CountRequiredBlocks(List<int> spell, long wallLength) => spell.Sum(n => wallLength / n);
 }

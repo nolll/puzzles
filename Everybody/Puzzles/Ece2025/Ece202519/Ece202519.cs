e51072759040f6f363334c93aec5f22e04638118
Henrik Severinson <henriks@gmail.com>
2025-11-28 18:27:58 +0100

    Everybody codes 2025 day 19 part 3 solved

--- Everybody/Puzzles/Ece2025/Ece202519/Ece202519.cs
+++ Everybody/Puzzles/Ece2025/Ece202519/Ece202519.cs
@@ -1,6 +1,7 @@
 using Pzl.Common;
 using Pzl.Tools.Graphs;
 using Pzl.Tools.Grids.Grids2d;
+using Pzl.Tools.HashSets;
 using Pzl.Tools.Strings;
 
 namespace Pzl.Everybody.Puzzles.Ece2025.Ece202519;
@@ -8,210 +9,66 @@ namespace Pzl.Everybody.Puzzles.Ece2025.Ece202519;
 [Name("Flappy Quack")]
 public class Ece202519 : EverybodyEventPuzzle
 {
-    public PuzzleResult Part1(string input)
+    public PuzzleResult Part1(string input) => new(Solve(input), "0e9f546a10105800c2c014d1d8205cc2");
+    public PuzzleResult Part2(string input) => new(Solve(input), "42fd601e0fc19354f8ef71fe2e4d3564");
+    public PuzzleResult Part3(string input) => new(Solve(input), "ef0c8788bb5ab3f59c9942f9beea54f0");
+
+    private static int Solve(string input)
     {
         var numberLists = input.Split(LineBreaks.Single).Select(o => o.Split(',').Select(int.Parse).ToArray()).ToList();
-        var maxHeight = numberLists.Max(o => o[1] + o[2]);
-        var gridHeight = maxHeight + 1;
-
-        var grid = new Grid<char>(1, gridHeight, '.');
-        grid.MoveTo(0, 0);
-
-        foreach (var numberlist in numberLists)
+        var wallsWithHoles = numberLists.Select(o => o[0]).Distinct().ToDictionary(k => k, _ => new List<Coord>());
+        
+        foreach (var list in numberLists)
         {
-            var x = numberlist[0];
+            var x = list[0];
+            var holeStart = list[1];
+            var holeLength = list[2];
 
-            grid.MoveTo(x, 0);
-            grid.WriteValue('#');
-            while (grid.Coord.Y < grid.YMax)
+            for (var y = 0; y < holeLength; y++)
             {
-                grid.MoveDown();
-                grid.WriteValue('#');
+                wallsWithHoles[x].Add(new Coord(x, holeStart + y));
             }
         }
         
-        foreach (var numberlist in numberLists)
-        {
-            var x = numberlist[0];
-            var passageStart = numberlist[1];
-            var passageHeight = numberlist[2] - 1;
+        wallsWithHoles.Add(0, [new Coord(0, 0)]);
 
-            grid.MoveTo(x, passageStart);
-            grid.WriteValue('.');
-            
-            for (var _ = 0; _ < passageHeight; _++)
-            {
-                grid.MoveDown();
-                grid.WriteValue('.');
-            }
-        }
-
-        var targets = grid.Coords.Where(o => o.X == grid.XMax && grid.ReadValueAt(o) == '.');
+        var walls = wallsWithHoles.Keys.Order().ToList();
         
+        HashSet<Coord> startPoints = [new(0, 0)];
         var edges = new List<GraphEdge>();
-        foreach (var coord in grid.Coords)
+        foreach (var (wa, wb) in walls.Zip(walls.Skip(1)))
         {
-            var val = grid.ReadValueAt(coord);
-            if (val == '#')
-                continue;
-
-            var up = new Coord(coord.X + 1, coord.Y + 1);
-            var down = new Coord(coord.X + 1, coord.Y - 1);
-
-            if (!grid.IsOutOfRange(up) && grid.ReadValueAt(up) == '.')
-            {
-                edges.Add(new GraphEdge(coord.Id, up.Id, 1));
-            }
+            var xdistance = wb - wa;
+            var targetHoles = wallsWithHoles[wb];
+            var newStartPoints = new HashSet<Coord>();
             
-            if (!grid.IsOutOfRange(down) && grid.ReadValueAt(down) == '.')
+            foreach (var startPoint in startPoints)
             {
-                edges.Add(new GraphEdge(coord.Id, down.Id, 0));
-            }
-        }
-        
-        var cost = Dijkstra.BestCost(edges, new Coord(0, 0).Id, targets.Select(o => o.Id).ToList());
+                var maxReachY = startPoint.Y + xdistance;
+                var minReachY = startPoint.Y - xdistance;
+                var targets = targetHoles.Where(o => o.Y >= minReachY && o.Y <= maxReachY && o.Y % 2 == o.X % 2).ToList();
 
-        //var flippedGrid = grid.FlipVertical();
-        //Console.WriteLine(flippedGrid.Print());
+                foreach (var target in targets)
+                {
+                    edges.Add(new GraphEdge(startPoint.Id, target.Id, CalculateCost(startPoint, target)));
+                }
+                
+                newStartPoints.AddRange(targets);
+            }
+
+            startPoints = newStartPoints;
+        }
+
+        var lastWallTargets = startPoints;
         
-        return new PuzzleResult(cost, "0e9f546a10105800c2c014d1d8205cc2");
+        return Dijkstra.BestCost(edges, new Coord(0, 0).Id, lastWallTargets.Select(o => o.Id).ToList());
     }
 
-    public PuzzleResult Part2(string input)
+    private static int CalculateCost(Coord from, Coord to)
     {
-        var numberLists = input.Split(LineBreaks.Single).Select(o => o.Split(',').Select(int.Parse).ToArray()).ToList();
-        var maxHeight = numberLists.Max(o => o[1] + o[2]);
-        var gridHeight = maxHeight + 1;
-
-        var grid = new Grid<char>(1, gridHeight, '.');
-        grid.MoveTo(0, 0);
-
-        foreach (var numberlist in numberLists)
-        {
-            var x = numberlist[0];
-
-            grid.MoveTo(x, 0);
-            grid.WriteValue('#');
-            while (grid.Coord.Y < grid.YMax)
-            {
-                grid.MoveDown();
-                grid.WriteValue('#');
-            }
-        }
-        
-        foreach (var numberlist in numberLists)
-        {
-            var x = numberlist[0];
-            var passageStart = numberlist[1];
-            var passageHeight = numberlist[2] - 1;
-
-            grid.MoveTo(x, passageStart);
-            grid.WriteValue('.');
-            
-            for (var _ = 0; _ < passageHeight; _++)
-            {
-                grid.MoveDown();
-                grid.WriteValue('.');
-            }
-        }
-
-        var targets = grid.Coords.Where(o => o.X == grid.XMax && grid.ReadValueAt(o) == '.');
-        
-        var edges = new List<GraphEdge>();
-        foreach (var coord in grid.Coords)
-        {
-            var val = grid.ReadValueAt(coord);
-            if (val == '#')
-                continue;
-        
-            var up = new Coord(coord.X + 1, coord.Y + 1);
-            var down = new Coord(coord.X + 1, coord.Y - 1);
-        
-            if (!grid.IsOutOfRange(up) && grid.ReadValueAt(up) == '.')
-            {
-                edges.Add(new GraphEdge(coord.Id, up.Id, 1));
-            }
-            
-            if (!grid.IsOutOfRange(down) && grid.ReadValueAt(down) == '.')
-            {
-                edges.Add(new GraphEdge(coord.Id, down.Id, 0));
-            }
-        }
-        
-        var cost = Dijkstra.BestCost(edges, new Coord(0, 0).Id, targets.Select(o => o.Id).ToList());
-
-        // var flippedGrid = grid.FlipVertical();
-        // Console.WriteLine(flippedGrid.Print());
-        
-        return new PuzzleResult(cost, "42fd601e0fc19354f8ef71fe2e4d3564");
-    }
-
-    public PuzzleResult Part3(string input)
-    {
-        var numberLists = input.Split(LineBreaks.Single).Select(o => o.Split(',').Select(int.Parse).ToArray()).ToList();
-        var maxHeight = numberLists.Max(o => o[1] + o[2]);
-        var gridHeight = maxHeight + 1;
-
-        var grid = new Grid<char>(1, gridHeight, '.');
-        grid.MoveTo(0, 0);
-
-        foreach (var numberlist in numberLists)
-        {
-            var x = numberlist[0];
-
-            grid.MoveTo(x, 0);
-            grid.WriteValue('#');
-            while (grid.Coord.Y < grid.YMax)
-            {
-                grid.MoveDown();
-                grid.WriteValue('#');
-            }
-        }
-        
-        foreach (var numberlist in numberLists)
-        {
-            var x = numberlist[0];
-            var passageStart = numberlist[1];
-            var passageHeight = numberlist[2] - 1;
-
-            grid.MoveTo(x, passageStart);
-            grid.WriteValue('.');
-            
-            for (var _ = 0; _ < passageHeight; _++)
-            {
-                grid.MoveDown();
-                grid.WriteValue('.');
-            }
-        }
-
-        var targets = grid.Coords.Where(o => o.X == grid.XMax && grid.ReadValueAt(o) == '.');
-        
-        var edges = new List<GraphEdge>();
-        foreach (var coord in grid.Coords)
-        {
-            var val = grid.ReadValueAt(coord);
-            if (val == '#')
-                continue;
-        
-            var up = new Coord(coord.X + 1, coord.Y + 1);
-            var down = new Coord(coord.X + 1, coord.Y - 1);
-        
-            if (!grid.IsOutOfRange(up) && grid.ReadValueAt(up) == '.')
-            {
-                edges.Add(new GraphEdge(coord.Id, up.Id, 1));
-            }
-            
-            if (!grid.IsOutOfRange(down) && grid.ReadValueAt(down) == '.')
-            {
-                edges.Add(new GraphEdge(coord.Id, down.Id, 0));
-            }
-        }
-        
-        var cost = Dijkstra.BestCost(edges, new Coord(0, 0).Id, targets.Select(o => o.Id).ToList());
-
-        // var flippedGrid = grid.FlipVertical();
-        // Console.WriteLine(flippedGrid.Print());
-        
-        return new PuzzleResult(cost);
+        var dx = to.X - from.X;
+        var dy = to.Y - from.Y;
+        var diff = dx - dy;
+        return dx - diff / 2;
     }
 }
--- Everybody/Puzzles/Ece2025/Ece202519/Ece202519Tests.cs
+++ Everybody/Puzzles/Ece2025/Ece202519/Ece202519Tests.cs
@@ -37,9 +37,18 @@ public class Ece202519Tests
     [Fact]
     public void Part3()
     {
-        const string input = "";
+        const string input = """
+                             7,7,2
+                             7,1,3
+                             12,0,4
+                             15,5,3
+                             24,1,6
+                             28,5,5
+                             40,3,3
+                             40,8,2
+                             """;
 
-        Sut.Part3(input).Answer.Should().Be("0");
+        Sut.Part3(input).Answer.Should().Be("22");
     }
 
     private static Ece202519 Sut => new();

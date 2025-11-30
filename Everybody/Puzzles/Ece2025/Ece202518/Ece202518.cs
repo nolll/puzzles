fed385c685459dca7ef28970570f1791959b04e9
Henrik Severinson <henriks@gmail.com>
2025-11-30 23:46:34 +0100

    Cleanup

--- Everybody/Puzzles/Ece2025/Ece202518/Ece202518.cs
+++ Everybody/Puzzles/Ece2025/Ece202518/Ece202518.cs
@@ -11,7 +11,54 @@ namespace Pzl.Everybody.Puzzles.Ece2025.Ece202518;
 // That is not true in the test input
 public class Ece202518 : EverybodyEventPuzzle
 {
-    public PuzzleResult Part1(string input)
+    public PuzzleResult Part1(string input) => new(ParsePlants(input).Last().Energy, "3c9317ed77a5948ad8c6ca11172055bd");
+
+    public PuzzleResult Part2(string input)
+    {
+        var (plantstr, teststr) = input.Split(LineBreaks.Triple);
+        var plants = ParsePlants(plantstr);
+        var sum = teststr.Split(LineBreaks.Single).Select(ParseTestCase).Sum(o => ActivateAndGetEnergy(plants, o.ToArray()));
+
+        return new PuzzleResult(sum, "810932c961238e78746d0d6f239e398d");
+    }
+
+    public PuzzleResult Part3(string input)
+    {
+        var (plantstr, teststr) = input.Split(LineBreaks.Triple);
+        var plants = ParsePlants(plantstr);
+        var optimal = GetOptimalEnergy(plants);
+        var sum = teststr.Split(LineBreaks.Single)
+            .Select(ParseTestCase)
+            .Select(o => ActivateAndGetEnergy(plants, o))
+            .Where(energy => energy > 0)
+            .Sum(energy => optimal - energy);
+
+        return new PuzzleResult(sum, "e784c3249103e09adf84a2ac82143826");
+    }
+
+    private static long GetOptimalEnergy(Plant[] plants)
+    {
+        var optimalActivation = plants.Where(o => o.IsFree)
+            .Select(fbPlant => plants.Select(plant => plant.Branches.FirstOrDefault(o => o.Plant != null && o.Plant.Id == fbPlant.Id))
+                .OfType<Branch>()
+                .ToList()
+                .All(o => o is { Energy: > 0 }))
+            .ToArray();
+
+        return ActivateAndGetEnergy(plants, optimalActivation);
+    }
+    
+    private static long ActivateAndGetEnergy(Plant[] plants, bool[] activations)
+    {
+        for (var i = 0; i < activations.Length; i++) 
+            plants[i].IsActive = activations[i];
+
+        return plants.Last().Energy;
+    }
+
+    private static bool[] ParseTestCase(string input) => input.Split(' ').Select(o => int.Parse(o) == 1).ToArray();
+
+    private static Plant[] ParsePlants(string input)
     {
         var sections = input.Split(LineBreaks.Double);
         List<Plant> plants = [];
@@ -28,86 +75,7 @@ public class Ece202518 : EverybodyEventPuzzle
                 var branchNums = Numbers.IntsFromString(line);
                 if (branchNums.Length == 1)
                 {
-                    plant.Branches.Add(new Branch(branchNums[0], null));
-                }
-                else
-                {
-                    var plantIndex = branchNums[0] - 1;
-                    var branchPlant = plants[plantIndex];
-                    plant.Branches.Add(new Branch(branchNums[1], branchPlant));   
-                }
-            }
-        }
-        
-        return new PuzzleResult(plants.Last().Energy, "3c9317ed77a5948ad8c6ca11172055bd");
-    }
-
-    public PuzzleResult Part2(string input)
-    {
-        var (plantstr, teststr) = input.Split(LineBreaks.Triple);
-        var sections = plantstr.Split(LineBreaks.Double);
-        List<Plant> plants = [];
-        foreach (var section in sections)
-        {
-            var lines = section.Split(LineBreaks.Single);
-            var plantNums = Numbers.IntsFromString(lines.First());
-            var id = plantNums[0];
-            var plantThickness = plantNums[1];
-            var plant = new Plant(id, plantThickness);
-            plants.Add(plant);
-            foreach (var line in lines.Skip(1))
-            {
-                var branchNums = Numbers.IntsFromString(line);
-                if (branchNums.Length == 1)
-                {
-                    plant.Branches.Add(new Branch(branchNums[0], null));
-                }
-                else
-                {
-                    var plantIndex = branchNums[0] - 1;
-                    var branchPlant = plants[plantIndex];
-                    plant.Branches.Add(new Branch(branchNums[1], branchPlant));   
-                }
-            }
-        }
-
-        var testLines = teststr.Split(LineBreaks.Single);
-        var sum = 0L;
-        foreach (var testLine in testLines)
-        {
-            var bools = testLine.Split(' ').Select(o => int.Parse(o) == 1).ToArray();
-            for (var i = 0; i < bools.Length; i++)
-            {
-                plants[i].IsActive = bools[i];
-            }
-
-            sum += plants.Last().Energy;
-        }
-        
-        return new PuzzleResult(sum, "810932c961238e78746d0d6f239e398d");
-    }
-
-    public PuzzleResult Part3(string input)
-    {
-        var (plantstr, teststr) = input.Split(LineBreaks.Triple);
-        var sections = plantstr.Split(LineBreaks.Double);
-        var freeBranchPlants = new List<Plant>();
-        List<Plant> plants = [];
-        foreach (var section in sections)
-        {
-            var lines = section.Split(LineBreaks.Single);
-            var plantNums = Numbers.IntsFromString(lines.First());
-            var id = plantNums[0];
-            var plantThickness = plantNums[1];
-            var plant = new Plant(id, plantThickness);
-            plants.Add(plant);
-            foreach (var line in lines.Skip(1))
-            {
-                var branchNums = Numbers.IntsFromString(line);
-                if (branchNums.Length == 1)
-                {
-                    freeBranchPlants.Add(plant);
-                    plant.Branches.Add(new Branch(branchNums[0], null));
+                    plant.IsFree = true;
                     break;
                 }
 
@@ -117,36 +85,7 @@ public class Ece202518 : EverybodyEventPuzzle
             }
         }
 
-        var optimalActivation = freeBranchPlants
-            .Select(fbPlant => plants.Select(plant => plant.Branches.FirstOrDefault(o => o.Plant != null && o.Plant.Id == fbPlant.Id))
-                .OfType<Branch>()
-                .ToList()
-                .All(o => o is { Energy: > 0 }))
-            .ToList();
-
-        for (var i = 0; i < optimalActivation.Count; i++)
-        {
-            plants[i].IsActive = optimalActivation[i];
-        }
-
-        var optimal = plants.Last().Energy;
-
-        var testLines = teststr.Split(LineBreaks.Single);
-        var sum = 0L;
-        foreach (var testLine in testLines)
-        {
-            var bools = testLine.Split(' ').Select(o => int.Parse(o) == 1).ToArray();
-            for (var i = 0; i < bools.Length; i++)
-            {
-                plants[i].IsActive = bools[i];
-            }
-
-            var energy = plants.Last().Energy;
-            if(energy > 0)
-                sum += optimal - energy;
-        }
-        
-        return new PuzzleResult(sum, "e784c3249103e09adf84a2ac82143826");
+        return plants.ToArray();
     }
 
     private class Plant(int id, int thickness)
@@ -154,13 +93,14 @@ public class Ece202518 : EverybodyEventPuzzle
         public int Id { get; } = id;
         public List<Branch> Branches { get; } = [];
         public bool IsActive { get; set; } = true;
+        public bool IsFree { get; set; }
         
         public int Energy
         {
             get
             {
-                if (!IsActive)
-                    return 0;
+                if (IsFree)
+                    return IsActive ? 1 : 0;
                 
                 var energy = Branches.Sum(o => o.Energy);
                 return energy < thickness ? 0 : energy;

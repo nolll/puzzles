ec5225e9127ac6e7cfc5f3f74f93d875f52054ba
Henrik Severinson <henriks@gmail.com>
2025-12-11 19:54:52 +0100

    Aoc 2025 day 10 part 2 wip

--- Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510.cs
+++ Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510.cs
@@ -10,17 +10,26 @@ public class Aoc202510 : AocPuzzle
     public PuzzleResult Part1(string input)
     {
         var items = input.Split(LineBreaks.Single).Select(Parse).ToList();
-        var results = items.Select(o => Solve(o.lights, o.buttons));
+        var results = items.Select((o, index) =>
+        {
+            return SolvePart1(o.lights, o.buttons);
+        });
         
         return new PuzzleResult(results.Sum(), "f19c91b91f25b5bde865c715075e4907");
     }
 
     public PuzzleResult Part2(string input)
     {
-        return new PuzzleResult(0);
+        var items = input.Split(LineBreaks.Single).Select(Parse).ToList();
+        var results = items.Select((o, index) =>
+        {
+            return SolvePart2(o.counters, o.buttons);
+        });
+        
+        return new PuzzleResult(results.Sum());
     }
 
-    private static int Solve(string lights, int[][] buttons)
+    private static int SolvePart1(string lights, int[][] buttons)
     {
         var initialState = lights.Replace('#', '.');
         var seen = new Dictionary<string, int>();
@@ -37,7 +46,7 @@ public class Aoc202510 : AocPuzzle
             
             foreach (var button in buttons)
             {
-                var nextState = GetNextState(state, button);
+                var nextState = GetNextStatePart1(state, button);
                 if (!seen.TryGetValue(nextState, out var s2) || s2 > steps + 1)
                     queue.Enqueue((nextState, steps + 1));
             }
@@ -45,8 +54,50 @@ public class Aoc202510 : AocPuzzle
         
         return seen[lights];
     }
+    
+    private static int SolvePart2(string counters, int[][] buttons)
+    {
+        var targetState = Numbers.IntsFromString(counters);
+        var initialState = string.Join(",", counters.Split(',').Select(_ => '0'));
+        var seen = new Dictionary<string, int>();
+        var queue = new Queue<(string state, int steps)>();
+        queue.Enqueue((initialState, 0));
+        while (queue.Count > 0)
+        {
+            var (state, steps) = queue.Dequeue();
 
-    private static string GetNextState(string state, int[] button)
+            if (seen.TryGetValue(state, out var s1) && s1 < steps)
+                continue;
+            
+            seen[state] = steps;
+            var intState = Numbers.IntsFromString(state);
+            
+            foreach (var button in buttons)
+            {
+                var newIntState = GetNextStatePart2(intState, button);
+                var newStrState = string.Join(",", newIntState);
+                if (!seen.TryGetValue(newStrState, out var s2) || s2 > steps + 1)
+                {
+                    var isValid = true;
+                    for (var i = 0; i < targetState.Length; i++)
+                    {
+                        if (newIntState[i] > targetState[i])
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    
+                    if(isValid)
+                        queue.Enqueue((newStrState, steps + 1));
+                }
+            }
+        }
+        
+        return seen[counters];
+    }
+
+    private static string GetNextStatePart1(string state, int[] button)
     {
         var arr = state.Select(o => o == '#').ToArray();
         foreach (var i in button)
@@ -56,13 +107,25 @@ public class Aoc202510 : AocPuzzle
 
         return string.Join("", arr.Select(o => o ? '#' : '.'));
     }
+    
+    private static int[] GetNextStatePart2(int[] state, int[] button)
+    {
+        var newState = state.ToArray();
+        foreach (var i in button)
+        {
+            newState[i] += 1;
+        }
 
-    private static (string lights, int[][] buttons) Parse(string s)
+        return newState;
+    }
+
+    private static (string lights, int[][] buttons, string counters) Parse(string s)
     {
         var parts = s.Replace("[", "").Replace("]", "").Replace("(", "").Replace(")", "").Replace("{", "").Replace("}", "").Split(' ');
         var lights = parts.First();
         var buttons = parts.Skip(1).SkipLast(1).Select(Numbers.IntsFromString).ToArray();
+        var counters = parts.Last();
         
-        return (lights, buttons);
+        return (lights, buttons, counters);
     }
 }
--- Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510Tests.cs
+++ Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510Tests.cs
@@ -2,33 +2,17 @@ namespace Pzl.Aoc.Puzzles.Aoc2025.Aoc202510;
 
 public class Aoc202510Tests
 {
-    [Fact]
-    public void Part1_1()
-    {
-        const string input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}";
-
-        Sut.Part1(input).Answer.Should().Be("2");
-    }
-    
-    [Fact]
-    public void Part1_2()
-    {
-        const string input = """
-                             [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-                             [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-                             [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-                             """;
-
-        Sut.Part1(input).Answer.Should().Be("7");
-    }
+    private const string Input = """
+                                 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+                                 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+                                 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+                                 """;
 
     [Fact]
-    public void Part2()
-    {
-        const string input = "";
+    public void Part1() => Sut.Part1(Input).Answer.Should().Be("7");
 
-        Sut.Part2(input).Answer.Should().Be("0");
-    }
+    [Fact]
+    public void Part2() => Sut.Part2(Input).Answer.Should().Be("33");
 
     private static Aoc202510 Sut => new();
 }

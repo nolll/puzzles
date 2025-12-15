4408575d58ace18d4ea88bdbb7f0bd2a7ac62dc4
Henrik Severinson <henriks@gmail.com>
2025-12-15 02:11:57 +0100

    Aoc 2025 day 10 part 2 solved

--- Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510.cs
+++ Aoc/Puzzles/Aoc2025/Aoc202510/Aoc202510.cs
@@ -1,3 +1,4 @@
+using Printmaster;
 using Pzl.Common;
 using Pzl.Tools.Combinatorics;
 using Pzl.Tools.Numbers;
@@ -6,6 +7,8 @@ using Pzl.Tools.Strings;
 namespace Pzl.Aoc.Puzzles.Aoc2025.Aoc202510;
 
 [Name("Factory")]
+// Thanks to Tenth Mascot
+[IsSlow]
 public class Aoc202510 : AocPuzzle
 {
     public PuzzleResult Part1(string input)
@@ -18,17 +21,23 @@ public class Aoc202510 : AocPuzzle
 
     public PuzzleResult Part2(string input)
     {
-        //var items = input.Split(LineBreaks.Single).Select(Parse).ToList();
-        //var results = items.Select(o => SolvePart2(o.counters, o.buttons));
+        var items = input.Split(LineBreaks.Single).Select(Parse).ToList();
+        var result = items.Select(o => SolvePart2(o.counters, o.buttons)).ToList();
         
-        return new PuzzleResult(0);
+        return new PuzzleResult(result.Sum(), "328efb57c96e68e01778dad1dd089540");
     }
 
-    private static int SolvePart1(string lights, int[][] buttons)
+    private static int SolvePart1(string targetLights, int[][] buttons)
     {
-        var initialState = lights.Replace('#', '.');
+        var from = targetLights.Replace('#', '.');
+        var combinations = FindCombinations(from, targetLights, buttons);
+
+        return combinations.Min(o => o.Count);
+    }
+
+    private static IEnumerable<List<int[]>> FindCombinations(string initialState, string to, int[][] buttons)
+    {
         var combinations = CombinationGenerator.GetUniqueCombinationsAnySize(buttons);
-        var validCombinations = new List<List<int[]>>();
         foreach (var combination in combinations)
         {
             var state = initialState.Select(o => o == '#').ToArray();
@@ -40,83 +49,71 @@ public class Aoc202510 : AocPuzzle
                 }
             }
             
-            if(string.Join("", state.Select(o => o ? '#' : '.')) == lights)
-                validCombinations.Add(combination);
+            if(string.Join("", state.Select(o => o ? '#' : '.')) == to)
+                yield return combination;
         }
-
-        return validCombinations.Min(o => o.Count);
     }
     
-    private static int SolvePart2(string counters, int[][] buttons)
+    private static long SolvePart2(int[] counters, int[][] buttons)
     {
-        var targetState = Numbers.IntsFromString(counters);
-        var initialState = string.Join(",", counters.Split(',').Select(_ => '0'));
-        var seen = new Dictionary<string, int>();
-        var queue = new Queue<(string state, int steps)>();
-        queue.Enqueue((initialState, 0));
-        while (queue.Count > 0)
+        var patterns = GetPatterns(buttons, counters.Length).ToList();
+        var result = SolveRecursive(patterns, counters);
+        
+        if(result < 1)
         {
-            var (state, steps) = queue.Dequeue();
+            Printr.Print(buttons, counters);
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
 
-            if (seen.TryGetValue(state, out var s1) && s1 < steps)
-                continue;
-            
-            seen[state] = steps;
-            var intState = Numbers.IntsFromString(state);
-            
-            foreach (var button in buttons)
+        return result;
+    }
+
+    private static IEnumerable<(int[] pattern, long cost)> GetPatterns(int[][] buttons, int size)
+    {
+        var combinations = CombinationGenerator.GetUniqueCombinationsAnySize(buttons);
+        combinations.Add([]);
+        foreach (var combination in combinations)
+        {
+            var a = new int[size];
+            foreach (var button in combination)
             {
-                var newIntState = GetNextStatePart2(intState, button);
-                var newStrState = string.Join(",", newIntState);
-                if (!seen.TryGetValue(newStrState, out var s2) || s2 > steps + 1)
+                foreach (var index in button)
                 {
-                    var isValid = true;
-                    for (var i = 0; i < targetState.Length; i++)
-                    {
-                        if (newIntState[i] > targetState[i])
-                        {
-                            isValid = false;
-                            break;
-                        }
-                    }
-                    
-                    if(isValid)
-                        queue.Enqueue((newStrState, steps + 1));
+                    a[index] += 1;
                 }
+            }
+            
+            yield return (a, combination.Count);
+        }
+    }
+
+    private static long SolveRecursive(List<(int[] pattern, long cost)> patterns, int[] goal)
+    {
+        if (goal.All(o => o == 0))
+            return 0;
+        
+        var result = 1_000_000L;
+        foreach (var (pattern, cost) in patterns)
+        {
+            var zip = pattern.Zip(goal).ToList();
+            if (zip.All(o => o.First <= o.Second && o.First % 2 == o.Second % 2))
+            {
+                var newGoal = zip.Select(o => (o.Second - o.First) / 2).ToArray();
+                var next = SolveRecursive(patterns, newGoal);
+                result = Math.Min(result, cost + 2 * next);
             }
         }
         
-        return seen[counters];
+        return result;
     }
 
-    private static string GetNextStatePart1(string state, int[] button)
-    {
-        var arr = state.Select(o => o == '#').ToArray();
-        foreach (var i in button)
-        {
-            arr[i] = !arr[i];
-        }
-
-        return string.Join("", arr.Select(o => o ? '#' : '.'));
-    }
-    
-    private static int[] GetNextStatePart2(int[] state, int[] button)
-    {
-        var newState = state.ToArray();
-        foreach (var i in button)
-        {
-            newState[i] += 1;
-        }
-
-        return newState;
-    }
-
-    private static (string lights, int[][] buttons, string counters) Parse(string s)
+    private static (string lights, int[][] buttons, int[] counters) Parse(string s)
     {
         var parts = s.Replace("[", "").Replace("]", "").Replace("(", "").Replace(")", "").Replace("{", "").Replace("}", "").Split(' ');
         var lights = parts.First();
         var buttons = parts.Skip(1).SkipLast(1).Select(Numbers.IntsFromString).ToArray();
-        var counters = parts.Last();
+        var counters = Numbers.IntsFromString(parts.Last());
         
         return (lights, buttons, counters);
     }

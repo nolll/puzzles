49a1c38755e766c5d9be7a1c68e3dfec55f200dc
Henrik Severinson <henriks@gmail.com>
2025-12-19 22:51:57 +0100

    FlipFlop 2025 day 6 cleanup

--- FlipFlop/Puzzles/FlipFlop2025/FlipFlop202506/FlipFlop202506.cs
+++ FlipFlop/Puzzles/FlipFlop2025/FlipFlop202506/FlipFlop202506.cs
@@ -8,25 +8,18 @@ namespace Pzl.FlipFlop.Puzzles.FlipFlop2025.FlipFlop202506;
 [Name("Bird spotters")]
 public class FlipFlop202506 : FlipFlopPuzzle
 {
+    private const long SecondsInAYear = 31556926L;
+    private const int SecondsInAnHour = 3600;
+    private const int GridSize = 1000;
+    private const int FrameStart = GridSize / 4;
+    private const int GridEnd = GridSize * 3 / 4;
+    private const int PictureCount = 1000;
+
     public PuzzleResult Part1(string input)
     {
         var speeds = input.Split(LineBreaks.Single).Select(Numbers.IntsFromString).Select(o => new Coord(o[0], o[1]));
-        var endPositions = new List<Coord>();
         const int time = 100;
-        const int size = 1000;
-        foreach (var speed in speeds)
-        {
-            var x = time * speed.X;
-            var y = time * speed.Y;
-            while (x < 0) x += size;
-            while (y < 0) y += size;
-            x %= size;
-            y %= size;
-            
-            endPositions.Add(new Coord(x, y));
-        }
-
-        var birdsInFrame = endPositions.Where(o => o.X is > 250 and <= 750 && o.Y is > 250 and <= 750);
+        var birdsInFrame = speeds.Select(o => Move(o, time)).Where(IsInFrame);
         
         return new PuzzleResult(birdsInFrame.Count(), "d0cfc435d1459e83bcc2be3046271a1a");
     }
@@ -34,23 +27,12 @@ public class FlipFlop202506 : FlipFlopPuzzle
     public PuzzleResult Part2(string input)
     {
         var speeds = input.Split(LineBreaks.Single).Select(Numbers.IntsFromString).Select(o => new Coord(o[0], o[1])).ToList();
-        const int size = 1000;
         var birdCount = 0;
     
-        for (var i = 1; i <= 1000; i++)
+        for (var i = 1; i <= PictureCount; i++)
         {
-            var endPositions = new List<Coord>();
-            //Console.WriteLine(i);
-            var time = i * 3600;
-            foreach (var speed in speeds)
-            {
-                var x = (time * speed.X % size + size) % size;
-                var y = (time * speed.Y % size + size) % size;
-            
-                endPositions.Add(new Coord(x, y));
-            }
-            
-            birdCount += endPositions.Count(o => o.X is > 250 and <= 750 && o.Y is > 250 and <= 750);
+            var time = i * SecondsInAnHour;
+            birdCount += speeds.Select(o => Move(o, time)).Count(IsInFrame);
         }
         
         return new PuzzleResult(birdCount, "6b4351b8336078bb7ea6ade68803e6d4");
@@ -59,24 +41,23 @@ public class FlipFlop202506 : FlipFlopPuzzle
     public PuzzleResult Part3(string input)
     {
         var speeds = input.Split(LineBreaks.Single).Select(Numbers.IntsFromString).Select(o => new Coord(o[0], o[1])).ToList();
-        const int size = 1000;
         var birdCount = 0L;
 
-        for (var i = 1; i <= 1000; i++)
+        for (var i = 1; i <= PictureCount; i++)
         {
-            var endPositions = new List<Coord>();
-            var time = i * 31556926L;
-            foreach (var speed in speeds)
-            {
-                var x = (int)(time * speed.X % size + size) % size;
-                var y = (int)(time * speed.Y % size + size) % size;
-            
-                endPositions.Add(new Coord(x, y));
-            }
-            
-            birdCount += endPositions.Count(o => o.X is > 250 and <= 750 && o.Y is > 250 and <= 750);
+            var time = i * SecondsInAYear;
+            birdCount += speeds.Select(o => Move(o, time)).Count(IsInFrame);
         }
         
         return new PuzzleResult(birdCount, "61dc24a9d639e326619bf87ce21094f7");
     }
+
+    private static Coord Move(Coord coord, long time)
+    {
+        var x = (int)(time * coord.X % GridSize + GridSize) % GridSize;
+        var y = (int)(time * coord.Y % GridSize + GridSize) % GridSize;
+        return new Coord(x, y);
+    }
+
+    private static bool IsInFrame(Coord o) => o.X is > FrameStart and <= GridEnd && o.Y is > FrameStart and <= GridEnd;
 }

6ed053296d9fd22c81e7945026614d1cfa108214
Henrik Severinson <henriks@gmail.com>
2025-12-19 14:12:26 +0100

    FlipFlop 2025 day 2 cleanup

--- FlipFlop/Puzzles/FlipFlop2025/FlipFlop202502/FlipFlop202502.cs
+++ FlipFlop/Puzzles/FlipFlop2025/FlipFlop202502/FlipFlop202502.cs
@@ -6,73 +6,51 @@ namespace Pzl.FlipFlop.Puzzles.FlipFlop2025.FlipFlop202502;
 [Name("Rollercoaster Heights")]
 public class FlipFlop202502 : FlipFlopPuzzle
 {
-    public PuzzleResult Part1(string input)
+    public PuzzleResult Part1(string input) => 
+        new(Solve(input, sequenceLength => sequenceLength), "a108ed87069fbb10b6d6595e8795dc16");
+
+    public PuzzleResult Part2(string input) => 
+        new(Solve(input, sequenceLength => Enumerable.Range(1, sequenceLength).Sum()), "3d520917bed77059dbbb4bf1f7433ffa");
+
+    public PuzzleResult Part3(string input) => 
+        new(Solve(input, sequenceLength => Numbers.Fibonacci(sequenceLength)), "9116cf946c0d82e0d08bffc0da078dd1");
+
+    private static long Solve(string input, Func<int, long> heightDelta)
     {
-        var best = 0;
-        var height = 0;
-        foreach (var c in input)
-        {
-            height += c == '^' ? 1 : -1;
-            best = Math.Max(best, height);
-        }
-        return new PuzzleResult(best, "a108ed87069fbb10b6d6595e8795dc16");
+        var sequenceLengths = GetSequenceLengths(input);
+        var initialDirection = input.First() == '^' ? 1 : -1;
+        return GetBestHeight(sequenceLengths, initialDirection, heightDelta);
     }
 
-    public PuzzleResult Part2(string input)
+    private static IEnumerable<int> GetSequenceLengths(string input)
     {
-        var direction = 0;
-        var best = 0;
-        var height = 0;
-        var speed = 1;
-        foreach (var c in input)
-        {
-            var newdir = c == '^' ? 1 : -1;
-            if (newdir != direction)
-            {
-                direction = newdir;
-                speed = 1;
-            }
-            else
-            {
-                speed += 1;
-            }
-                
-            height += speed * direction;
-            best = Math.Max(best, height);
-        }
-        
-        return new PuzzleResult(best, "3d520917bed77059dbbb4bf1f7433ffa");
-    }
-
-    public PuzzleResult Part3(string input)
-    {
-        var best = 0L;
-        var height = 0L;
-
         var chars = input.ToCharArray();
-        var lastchar = chars.First();
         var length = 1;
-        var sequences = new List<int>();
         for (var i = 1; i < chars.Length; i++)
         {
             if (input[i] != input[i - 1])
             {
-                sequences.Add(length);
+                yield return length;
                 length = 0;
             }
 
             length++;
         }
-        sequences.Add(length);
-        
-        var direction = lastchar == '^' ? 1 : -1;
+
+        yield return length;
+    }
+
+    private static long GetBestHeight(IEnumerable<int> sequences, int direction, Func<int, long> heightDelta)
+    {
+        var best = 0L;
+        var height = 0L;
         foreach (var sequence in sequences)
         {
-            height += direction * Numbers.Fibonacci(sequence);
+            height += direction * heightDelta(sequence);
             best = Math.Max(best, height);
             direction *= -1;
         }
-        
-        return new PuzzleResult(best, "9116cf946c0d82e0d08bffc0da078dd1");
+
+        return best;
     }
 }

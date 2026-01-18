65846424d0b86cffa7ee9e44151c586b12e7c344
Henrik Severinson <henriks@gmail.com>
2026-01-18 15:33:00 +0100

    Added flipflop to help text

--- Help/HelpPrinter.cs
+++ Help/HelpPrinter.cs
@@ -5,7 +5,7 @@ namespace Pzl.Client.Help;
 public class HelpPrinter
 {
     private const string HelpText = """
-                                    My solutions to Advent of Code, AquaQ Challenge, Project Euler, Everybody Codes and Codyssi.
+                                    My solutions to programming challenges.
                                     https://github.com/nolll/puzzles
 
                                     Usage:
@@ -19,14 +19,25 @@ public class HelpPrinter
                                                       --tags ec,s2,1 (runs everybody codes story 2 quest 1)
                                                       --tags euler,4 (runs euler puzzle 4)
                                                       --tags codyssi,2025,15 (runs codyssi challenge 15 2025)
+                                                      --tags ff,2025,3 (runs flipflop puzzle 3 2025)
 
                                     -s    --search    search query. Searches titles, class names and comments.  
                                                       Use quotes if the query contains spaces
 
                                     -h    --help      display this help text
-
-
                                     """;
 
-    public void Print() => AnsiConsole.WriteLine(HelpText);
+    public void Print()
+    {
+        AnsiConsole.WriteLine(HelpText);
+        AnsiConsole.WriteLine();
+        // AnsiConsole.WriteLine("Links:");
+        // AnsiConsole.MarkupLine("[blue underline link=https://adventofcode.com]Advent of Code[/]");
+        // AnsiConsole.WriteLine("AquaQ Challenge: https://challenges.aquaq.co.uk");
+        // AnsiConsole.WriteLine("Codyssi: https://www.codyssi.com");
+        // AnsiConsole.WriteLine("FlipFlop: https://flipflop.slome.org");
+        // AnsiConsole.WriteLine("Everybody Codes: https://everybody.codes");
+        // AnsiConsole.WriteLine("Project Euler: https://projecteuler.net");
+        // AnsiConsole.WriteLine();
+    }
 }

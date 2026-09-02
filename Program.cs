3790ca43344ad74a0ea1c8a8c5634bac2db7e627
Henrik Severinson <henriks@gmail.com>
2026-09-03 00:34:36 +0200

    Fixed warning

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", true, false)
     .Build();
 
+var inputLocation = configuration["inputLocation"];
+if (string.IsNullOrEmpty(inputLocation))
+{
+    Console.WriteLine("Error: inputLocation is not set in appsettings.json");
+    return;
+}
+
 var options = new Options(
     configuration["hashSeed"],
     configuration["timeoutSeconds"],
     configuration["debugTags"],
-    configuration["inputLocation"]);
+    inputLocation);
 
 new PuzzleProgram(options).Run(args);
